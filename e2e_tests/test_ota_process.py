d298b51294e0d0aebdde881595ba2678bc97e514
Bodong Yang <bodong.yang@tier4.jp>
2021-07-28 17:30:54 +0900

    Implement test functions

--- e2e_tests/test_ota_process.py
+++ e2e_tests/test_ota_process.py
@@ -1,15 +1,12 @@
 import pytest
-import pytest_mock
 import pathlib
-
-from bank import BankInfo
-
-from ota_client import OtaClient
-from ota_client_service import OtaClientService
 
 # TODO: set up a local http server using pytest-xprocess
 
 def _compare_files(path_l: pathlib.Path, path_r: pathlib.Path):
+    """
+    check whether files in the same relative path are the same
+    """
     l_stat, r_stat = path_l.lstat(), path_r.lstat()
     
     # check the size is the same
@@ -18,27 +15,24 @@ def _compare_files(path_l: pathlib.Path, path_r: pathlib.Path):
     # check the permission and ownership is the same
     if( l_stat.st_uid != r_stat.st_uid or 
         l_stat.st_gid != r_stat.st_gid or
-        l_stat.st_mode != r_stat.st_mode
-        ):
+        l_stat.st_mode != r_stat.st_mode):
         return False
-    # (by pass)check the file contents are the same
+    # (pass)check the file contents are the same
     return True
 
-# test the OTA process
+# step1: apply the OTA update
 def test_ota_update(
-    mocker: pytest_mock.MockerFixture,
-    tmp_path_factory: pathlib.Path,
     ota_request,
-    ota_client_service_instance: OtaClientService):
+    ota_client_service_instance):
     # start the OTA process
-    assert ota_client_service_instance._ota_update(ota_request)
+    ota_result = ota_client_service_instance._ota_update(ota_request)
 
-# test the bankB
-def test_bank_b(
-    tmp_path_factory: pathlib.Path,
-    configs,
-):
-    bank_b = tmp_path_factory / configs["BANKB_DIR"]
+    # check if ota is successful
+    assert ota_result
+
+# step2.1: test the bankB
+def test_bank_b(configs):
+    bank_b = configs["BANKB_DIR"]
     ota_source = configs["OTA_SOURCE_DIR"]
     
     # walk through bank_b to check the files are correctly applied
@@ -46,16 +40,34 @@ def test_bank_b(
         entry_source = ota_source / entry_bank_b.relative_to(bank_b)
         assert _compare_files(entry_bank_b, entry_source)
 
-# TODO: test grub files
+# step2.2: TODO: test grub files
 def test_grub(
     tmp_path_factory: pathlib.Path,
     configs,):
     pass
 
-# TODO: test ota_status
-def test_ota_status(
-    tmp_path_factory: pathlib.Path,
-    configs,
-):
-    ota_status_file: pathlib.Path = tmp_path_factory / configs["BOOT_DIR"] / "ota_status"
+# step2.3: test ota status
+def test_ota_status(configs):
+    ota_status_file = configs["BOOT_DIR"] / "ota_status"
     assert ota_status_file.read_text() == "SWITCHB"
+
+# step2.4: test updated ecuinfo
+def test_updated_ecuinfo(configs):
+    # expected
+    """
+    main_ecu:
+        ecu_name: 'autoware_ecu'
+        ecu_type: 'autoware'
+        ecu_id: '1'
+        version: '0.5.1'
+        independent: True
+    """
+    import ota_client
+
+    rd_ecuinfo = ota_client._read_ecu_info(str(configs["OTA_DIR"] / "ecuinfo.yaml"))
+    assert rd_ecuinfo["main_ecu"]["ecu_name"] == "autoware_ecu"
+    assert rd_ecuinfo["main_ecu"]["ecu_type"] == "autoware"
+    assert rd_ecuinfo["main_ecu"]["ecu_id"] == "1"
+    assert rd_ecuinfo["main_ecu"]["version"] == "0.5.1"
+    assert rd_ecuinfo["main_ecu"]["independent"] == True
+
