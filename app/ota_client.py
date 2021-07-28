23bb0130c7274c0c689c1ea0eea3fd874458c18d
Toru Sugihara <53561710+tsugi1203@users.noreply.github.com>
2021-07-28 17:58:19 +0900

    Update app/ota_client.py
    Co-authored-by: hiroyuki obinata <58019445+obi-t4@users.noreply.github.com>

--- app/ota_client.py
+++ app/ota_client.py
@@ -210,7 +210,7 @@ def _copy_persistent(src_path, target_dir):
     copy persistent dir/file
     """
     if src_path.startswith("/"):
-        dest_path = os.path.join(target_dir, "." + src_path)
+        dest_path = os.path.join(target_dir, ".", src_path)
     else:
         dest_path = os.path.join(target_dir, src_path)
     if os.path.exists(src_path):
