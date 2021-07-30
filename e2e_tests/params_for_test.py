bc2209abc296f5f1893ffabc2bb2b5566638f4ef
Bodong Yang <bodong.yang@tier4.jp>
2021-07-30 18:05:31 +0900

    Update params_for_test.py: using Template to create consts

--- e2e_tests/params_for_test.py
+++ e2e_tests/params_for_test.py
@@ -1,4 +1,29 @@
-# os status
+from string import Template
+
+############## template #############
+TEMPLATE_GRUB_CUSTOM_CFG = Template("""
+menuentry 'GNU/Linux' {{
+        linux   /vmlinuz-5.4.0-74-generic root=UUID=${uuid} ro  quiet splash $vt_handoff
+        initrd  /initrd.img-5.4.0-74-generic
+}}""")
+
+TEMPLATE_FSTAB = Template("""\
+# /etc/fstab: static file system information.
+#
+# Use 'blkid' to print the universally unique identifier for a
+# device; this may be used with UUID= as a more robust way to name devices
+# that works even if disks are added and removed. See fstab(5).
+#
+# <file system> <mount point>   <type>  <options>       <dump>  <pass>
+# / was on /dev/nvme0n1p1 during installation
+UUID=${boot_uuid} /               ext4    errors=remount-ro 0       1
+UUID=${bank_uuid} /boot           ext4    errors=remount-ro 0       1
+/swapfile                                 none            swap    sw              0       0
+""")
+
+############## consts ##############
+
+### os status ###
 BANKA_UUID, BANKA_DEV = "3a1c99e7-46d9-41b1-8b0a-b07bceef1d02", "/dev/sda3"
 BANKB_UUID, BANKB_DEV = "ad0cd79a-1752-47bb-9274-f9aa4e289cb9", "/dev/sda4"
 BOOT_UUID, BOOT_DEV = "cc59073d-9e5b-41e1-b724-576259341132", "/dev/sda2"
@@ -8,7 +33,7 @@ banka: {BANKA_DEV}
 bankb: {BANKB_DEV}
 """
 
-# initial status
+### initial status ###
 ECUID = "1\n"
 ECUINFO_YAML = """\
 main_ecu:
@@ -29,27 +54,10 @@ GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"
 GRUB_CMDLINE_LINUX=""
 """
 
-GRUB_CUSTOM_CFG = f"""
-menuentry 'GNU/Linux' {{
-        linux   /vmlinuz-5.4.0-74-generic root=UUID={UUID_A} ro  quiet splash $vt_handoff
-        initrd  /initrd.img-5.4.0-74-generic
-}}"""
+GRUB_CUSTOM_CFG = TEMPLATE_GRUB_CUSTOM_CFG.substitute(uuid=BANKA_UUID)
+FSTAB_BY_UUID_BANKA = TEMPLATE_FSTAB.substitute(boot_uuid=BOOT_UUID, bank_uuid=BANKA_UUID)
 
-FSTAB_BY_UUID_BANKA = f"""\
-# /etc/fstab: static file system information.
-#
-# Use 'blkid' to print the universally unique identifier for a
-# device; this may be used with UUID= as a more robust way to name devices
-# that works even if disks are added and removed. See fstab(5).
-#
-# <file system> <mount point>   <type>  <options>       <dump>  <pass>
-# / was on /dev/nvme0n1p1 during installation
-UUID={BOOT_UUID} /               ext4    errors=remount-ro 0       1
-UUID={BANKA_UUID} /boot           ext4    errors=remount-ro 0       1
-/swapfile                                 none            swap    sw              0       0
-"""
-
-# updated status
+### updated status ###
 # status switched from NORMAL to SWITCHB
 UPDATED_OTA_STATUS="SWITCHB"
 
@@ -62,22 +70,5 @@ main_ecu:
   independent: True
 """
 
-FSTAB_BY_UUID_BANKB = """\
-# /etc/fstab: static file system information.
-#
-# Use 'blkid' to print the universally unique identifier for a
-# device; this may be used with UUID= as a more robust way to name devices
-# that works even if disks are added and removed. See fstab(5).
-#
-# <file system> <mount point>   <type>  <options>       <dump>  <pass>
-# / was on /dev/nvme0n1p1 during installation
-UUID={BOOT_UUID} /               ext4    errors=remount-ro 0       1
-UUID={BANKB_UUID} /boot           ext4    errors=remount-ro 0       1
-/swapfile                                 none            swap    sw              0       0
-"""
-
-GRUB_CUSTOM_CFG_BANKB = f"""
-menuentry 'GNU/Linux' {{
-        linux   /vmlinuz-5.4.0-74-generic root=UUID={UUID_B} ro  quiet splash $vt_handoff
-        initrd  /initrd.img-5.4.0-74-generic
-}}"""
+FSTAB_BY_UUID_BANKB = TEMPLATE_FSTAB.substitute(boot_uuid=BOOT_UUID, bank_uuid=BANKB_UUID)
+GRUB_CUSTOM_CFG_BANKB = TEMPLATE_GRUB_CUSTOM_CFG.substitute(uuid=BANKB_UUID)
