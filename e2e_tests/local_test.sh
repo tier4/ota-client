eafc6dbcf05695a3bddb847533612e2f1cadf42b
Bodong Yang <bodong.yang@tier4.jp>
2021-08-04 13:59:34 +0900

    local_test.sh: coloring log output; minor fix

--- e2e_tests/local_test.sh
+++ e2e_tests/local_test.sh
@@ -4,6 +4,7 @@ set -e # exit if any command failed
 # configs
 DEPENDENCIES=(python3 docker)
 TIMESTAMP_FORMAT='%Y-%m-%d %H:%M:%S'
+CONTAINER_NAME=base-image-`date +%s`
 # flags
 SETUP_ENVIRONMENT=0
 DO_TEST=0
@@ -21,56 +22,67 @@ _print_usage() {
 
 _echo() {
     printf "[%(${TIMESTAMP_FORMAT})T] "
-    printf "$1\n"
+    case $1 in
+    "Error")
+        printf "\033[0;31m$2\n\033[0m";;
+    "Warn")
+        printf "\033[1;33m$2\n\033[0m";;
+    *)
+        printf "\033[0;32m$1\n\033[0m";;
+    esac
+    
 }
 
 _clean_up() {
     [ $? == 0 ] && \
         _echo "Test finished!" || \
-        _echo "Test failed!"
-    read -rp "$(_echo "Cleanup the working_dir $WORKING_DIR? [Y/N]:")" reply
-    echo
-    if [ "$reply" == "Y" ]
-    then
-        rm -rf "$WORKING_DIR"
-        _echo "Finished cleaning up working dir!"
-    fi
+        _echo "Error" "Test failed!"
+
+    ([ $SETUP_ENVIRONMENT == 1 ] && [ $DO_TEST == 0 ]) || {
+        read -rp "$(_echo "Warn" "Cleanup the working_dir $WORKING_DIR? [Y/N]:")" reply
+        echo
+        if [ "$reply" == "Y" ]
+        then
+            rm -rf "$WORKING_DIR"
+            _echo "Finished cleaning up working dir!"
+        fi
+    }
 }
 
 setup_test_environment() {
     # check dependencies
-    _echo "Checking dependencies..."
+    _echo "Warn" "Checking dependencies..."
     for cmd in ${DEPENDENCIES[@]}
     do
         _echo "Check for $cmd presents..."
-        which cmd
+        which $cmd > /dev/null
     done
 
     # cp the repo to the working dir
     _echo "Copying the $REPO source codes to $WORKING_DIR..."
-    cp -a "$REPO_LOCATION" "$WORKING_DIR"
-    cd "$WORKING_DIR"
-    _echo "Switch to working dir. Current working_dir is `$pwd`."
+    sudo cp -a "$REPO_LOCATION" "$WORKING_DIR"
+    cd "$WORKING_DIR" && _echo "Warn" "Switch to working dir. Current working_dir is `$WORKING_DIR`."
 
     # build & prepare ota baseimage
-    _echo "Building OTA baseimage..."
-    docker build \
+    _echo "Warn" "Building OTA baseimage..."
+    sudo docker build \
         -t base-image \
-        --build-arg KERNEL_VERSION=5.8.0-53-generic \
-        ./"$REPO"/e2e_tests/Dockerfile_OTA-baseimage
-    docker create --name base-image base-image
-    docker export base-image > ./base-image.tgz
+        --build-arg KERNEL_VERSION=5.8.0-53-generic - < ./"$REPO"/e2e_tests/Dockerfile_OTA-baseimage
+    sudo docker create --name $CONTAINER_NAME base-image
+    sudo docker export $CONTAINER_NAME > ./base-image.tgz
+    sudo docker rm -f $CONTAINER_NAME
+
     _echo "Finished building OTA baseimage."
 
-    _echo "Extracting OTA baseimage..."
-    mkdir -p ./data
+    _echo "Warn" "Extracting OTA baseimage..."
+    sudo mkdir -p ./data
     sudo tar xf ./base-image.tgz -C ./data
 
     # sign the ota baseimage
-    _echo "Setup ota-metadata signtools..."
+    _echo "Warn" "Setup ota-metadata signtools..."
     git clone https://github.com/tier4/ota-metadata
     python3 -m pip install -r ./ota-metadata/metadata/ota_metadata/requirements.txt
-    _echo "Signing the OTA-metadata..."
+    _echo "Warn" "Signing the OTA-metadata..."
     sudo -E python3 ./ota-metadata/metadata/ota_metadata/metadata_gen.py \
         --target-dir ./data --ignore-file ./ota-metadata/metadata/ignore.txt
     sudo ./ota-metadata/metadata/key-gen.sh
@@ -88,16 +100,16 @@ setup_test_environment() {
 
 do_e2e_test() {
     # switch to the working dir
-    cd "$WORKING_DIR"
-    _echo "Switch to working dir. Current working_dir is `$pwd`."
+    cd "$WORKING_DIR" && \
+    _echo "Warn" "Switch to working dir. Current working_dir is `$WORKING_DIR`."
 
     # install test dependencies
-    _echo "Install test dependencies..."
+    _echo "Warn" "Install test dependencies..."
     python3 -m pip install --upgrade pip
     python3 -m pip install -r ./"$REPO"/app/requirements.txt
-    python3 -m pip install -r ./"$REPO"/tests/requirements.txt
+    python3 -m pip install -r ./"$REPO"/e2e-tests/requirements.txt
 
-    _echo "Start OTA E2E test..."
+    _echo "Warn" "Start OTA E2E test..."
     export WORKING_DIR="$WORKING_DIR"
     sudo -E python3 -m pytest --cov-report term-missing --cov=app ./"$REPO"/e2e_tests
     _echo "Finished OTA E2E test!"
@@ -130,21 +142,22 @@ then
     _print_usage
     exit -1
 else
-    _echo "Get absolute paths for WORKING_DIR and REPO_LOCATION..."
+    _echo "Warn" "Get absolute paths for WORKING_DIR and REPO_LOCATION..."
     WORKING_DIR=`readlink -f $WORKING_DIR`
     REPO_LOCATION=`readlink -f $REPO_LOCATION`
 fi
 
 ############# start the script ################
+trap '_clean_up' SIGINT SIGKILL SIGTERM EXIT
+
 if [ $SETUP_ENVIRONMENT == 1 ] && [ $DO_TEST == 0 ];then
-    _echo "Setup test environment only..."
+    _echo "Warn" "Setup test environment only..."
     setup_test_environment
 elif [ $SETUP_ENVIRONMENT == 0 ] && [ $DO_TEST == 1 ];then
-    trap '_clean_up' SIGINT SIGKILL SIGTERM EXIT
-    _echo "Do the e2e test only..."
+    _echo "Warn" "Do the e2e test only..."
     do_e2e_test
 else
-    trap '_clean_up' SIGINT SIGKILL SIGTERM EXIT
+    _echo "Warn" "Do the whole e2e test ..."
     setup_test_environment
     do_e2e_test
 fi
