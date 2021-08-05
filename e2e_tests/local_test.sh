b81316c8a88d80067ec70de3b48895b8a3031b75
Bodong Yang <bodong.yang@tier4.jp>
2021-08-05 13:53:52 +0900

    local_test.sh: require root priviledge to execute the script

--- e2e_tests/local_test.sh
+++ e2e_tests/local_test.sh
@@ -10,12 +10,13 @@ SETUP_ENVIRONMENT=0
 DO_TEST=0
 
 _print_usage() {
-    echo "Usage: local_test.sh -w <workding_dir> -r <repo_location> [-s, -e]"
+    echo "Usage: local_test.sh -w <workding_dir> -r <repo_location> [-s -e -c]"
     echo "options: "
     echo "  -w <working_dir>       E2e test will executed under this folder."
     echo "  -r <repo_location>     The location of the to be tested repository."
     echo "  -s                     Setup the test environment."
     echo "  -e                     Do the e2e test."
+    echo "  -c                     Cleanup the working dir after the test."
     echo ""
     echo "If neither -s or -e are set, the whole test will be carried out."
 }
@@ -38,7 +39,7 @@ _clean_up() {
         _echo "Test finished!" || \
         _echo "Error" "Test failed!"
 
-    ([ $SETUP_ENVIRONMENT == 1 ] && [ $DO_TEST == 0 ]) || {
+    ([ $SETUP_ENVIRONMENT == 1 ] && [ $DO_TEST == 0 ] ) || [ $CLEANUP == 1 ] && {
         read -rp "$(_echo "Warn" "Cleanup the working_dir $WORKING_DIR? [Y/N]:")" reply
         echo
         if [ "$reply" == "Y" ]
@@ -60,33 +61,33 @@ setup_test_environment() {
 
     # cp the repo to the working dir
     _echo "Copying the $REPO source codes to $WORKING_DIR..."
-    sudo cp -a "$REPO_LOCATION" "$WORKING_DIR"
+    cp -a "$REPO_LOCATION" "$WORKING_DIR"
     cd "$WORKING_DIR" && _echo "Warn" "Switch to working dir. Current working_dir is `$WORKING_DIR`."
 
     # build & prepare ota baseimage
     _echo "Warn" "Building OTA baseimage..."
-    sudo docker build \
+    docker build \
         -t base-image \
         --build-arg KERNEL_VERSION=5.8.0-53-generic - < ./"$REPO"/e2e_tests/Dockerfile_OTA-baseimage
-    sudo docker create --name $CONTAINER_NAME base-image
-    sudo docker export $CONTAINER_NAME > ./base-image.tgz
-    sudo docker rm -f $CONTAINER_NAME
+    docker create --name $CONTAINER_NAME base-image
+    docker export $CONTAINER_NAME > ./base-image.tgz
+    docker rm -f $CONTAINER_NAME
 
     _echo "Finished building OTA baseimage."
 
     _echo "Warn" "Extracting OTA baseimage..."
-    sudo mkdir -p ./data
-    sudo tar xf ./base-image.tgz -C ./data
+    mkdir -p ./data
+    tar xf ./base-image.tgz -C ./data
 
     # sign the ota baseimage
     _echo "Warn" "Setup ota-metadata signtools..."
     git clone https://github.com/tier4/ota-metadata
     python3 -m pip install -r ./ota-metadata/metadata/ota_metadata/requirements.txt
     _echo "Warn" "Signing the OTA-metadata..."
-    sudo -E python3 ./ota-metadata/metadata/ota_metadata/metadata_gen.py \
+    python3 ./ota-metadata/metadata/ota_metadata/metadata_gen.py \
         --target-dir ./data --ignore-file ./ota-metadata/metadata/ignore.txt
-    sudo ./ota-metadata/metadata/key-gen.sh
-    sudo -E python3 ./ota-metadata/metadata/ota_metadata/metadata_sign.py \
+    ./ota-metadata/metadata/key-gen.sh
+    python3 ./ota-metadata/metadata/ota_metadata/metadata_sign.py \
         --sign-key privatekey.pem \
         --cert-file certificate.pem \
         --directory-file dirs.txt \
@@ -94,29 +95,29 @@ setup_test_environment() {
         --regular-file regulars.txt \
         --rootfs-directory data \
         --persistent-file ./"$REPO"/e2e_tests/persistents-x1.txt
-    sudo cp ./"$REPO"/e2e_tests/persistents-x1.txt .
+    cp ./"$REPO"/e2e_tests/persistents-x1.txt .
     _echo "Finished preparing the OTA baseimage!"
 }
 
 do_e2e_test() {
     # switch to the working dir
     cd "$WORKING_DIR" && \
-    _echo "Warn" "Switch to working dir. Current working_dir is `$WORKING_DIR`."
+    _echo "Warn" "Switch to working dir. Current working_dir is $PWD."
 
     # install test dependencies
     _echo "Warn" "Install test dependencies..."
     python3 -m pip install --upgrade pip
     python3 -m pip install -r ./"$REPO"/app/requirements.txt
-    python3 -m pip install -r ./"$REPO"/e2e-tests/requirements.txt
+    python3 -m pip install -r ./"$REPO"/e2e_tests/requirements.txt
 
     _echo "Warn" "Start OTA E2E test..."
     export WORKING_DIR="$WORKING_DIR"
-    sudo -E python3 -m pytest --cov-report term-missing --cov=app ./"$REPO"/e2e_tests
+    python3 -m pytest --cov-report term-missing --cov=app ./"$REPO"/e2e_tests
     _echo "Finished OTA E2E test!"
 }
 
 # parse options
-while getopts ":hr:w:se" option
+while getopts ":hr:w:sec" option
 do
     case $option in
     h)
@@ -131,6 +132,8 @@ do
         SETUP_ENVIRONMENT=1;;
     e)
         DO_TEST=1;;
+    c)
+        CLEANUP=1
     *)
         _print_usage
         exit;;
@@ -146,6 +149,8 @@ else
     WORKING_DIR=`readlink -f $WORKING_DIR`
     REPO_LOCATION=`readlink -f $REPO_LOCATION`
 fi
+# check priviledge
+[ `whoami` != "root" ] && _echo "Error" "Please run the script under root priviledge!" && exit -1
 
 ############# start the script ################
 trap '_clean_up' SIGINT SIGKILL SIGTERM EXIT
