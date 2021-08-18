10916dd319ef464386d941124916d41e5e884961
Bodong Yang <bodong.yang@tier4.jp>
2021-08-18 15:07:05 +0900

    configs.py:  add a independent config files for ota_client

---
+++ app/configs.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+_configs_dir_list = (
+    'OTA_DIR',
+    'GRUB_DIR',
+    'ETC_DIR',
+    'MOUNT_POINT'
+)
+_configs_file_list = (
+    'OTA_STATUS_FILE',
+    'BANK_INFO_FILE',
+    'ECUID_FILE',
+    'ECUID_YAML_FILE',
+    'CUSTOM_CONFIG_FILE',
+    'GRUB_CFG_FILE',
+    'GRUB_DEFAUT_FILE',
+    'FSTAB_FILE'
+)
+
+__all__ = _configs_dir_list + _configs_file_list
+
+#
+# dirs
+#
+OTA_DIR=Path("/boot/ota")
+GRUB_DIR=Path("/boot/grub")
+ETC_DIR=Path("/etc")
+MOUNT_POINT=Path("/mnt/bank")
+
+#
+# files
+#
+# ota_client
+OTA_STATUS_FILE=OTA_DIR / "ota_status"
+BANK_INFO_FILE=OTA_DIR / "bankinfo.yaml"
+ECUID_FILE=OTA_DIR / "ecuid"
+ECUID_YAML_FILE=OTA_DIR / "ecuinfo.yaml"
+CUSTOM_CONFIG_FILE=GRUB_DIR / "custom.cfg"
+# system file
+GRUB_CFG_FILE=GRUB_DIR / "grub.conf"
+GRUB_DEFAUT_FILE=ETC_DIR / "default/grub"
+FSTAB_FILE=ETC_DIR / "fstab"
