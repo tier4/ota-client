cba76e6c6b83bdd4a4cf6a1cd65b44f3db44cfdb
hiroyuki.obinata <hiroyuki.obinata@tier4.jp>
2021-09-10 21:50:57 +0900

    add each skelton class

--- design.py
+++ design.py
@@ -1,9 +1,4 @@
 """
---> OtaClientServer -> OtaClientStub -> OtaClient
-                       (callback)
-"""
-
-"""
 terminology:
     partition (NG: bank)
     create, update, delete (NG: generate-update-delete, make-update-delete, create-update-remove)
@@ -12,6 +7,8 @@ terminology:
     start, stop
     initialize, finalize,
     load, store (for file)
+    request, response (NG: reply)
+    EcuInfo, EcuId (NG: Ecuinfo, ecuinfo, Ecuid, ecuid)
 """
 
 """ main.py """
@@ -19,9 +16,7 @@ import ota_client_stub
 import ota_client_service
 
 if __name__ == "__main__":
-    ota_satus = OtaStatus()
-    ota_client = OtaClient(ota_status)
-    ota_client_stub = OtaClientStub(ota_client)
+    ota_client_stub = OtaClientStub()
     ota_client_service = OtaClientService(ota_client_stub)
 
 """ OtaClientService """
@@ -36,81 +31,87 @@ class OtaClientService(otaclient_pb2_grpc.OtaClientServiceServicer):
 
     def update(self, request, context):
         result = self._stub.update(request)
-        reply = otaclient_pb2.OtaUpdateReply()
-        reply.result = result
-        return reply
+        response = otaclient_pb2.UpdateResponse()
+        response.result = result
+        return response
 
     def rollback(self, request, context):
         result = self._stub.rollback(request)
-        reply = otaclient_pb2.OtaRollbackReply()
-        reply.result = result
-        return reply
+        response = otaclient_pb2.RollbackResponse()
+        response.result = result
+        return response
 
     def status(self, request, context):
         result = self._stub.status(request)
-        reply = otaclient_pb2.OtaStatusReply()
-        reply.result = result
-        return reply
+        response = otaclient_pb2.StatusResponse()
+        response.result = result
+        return response
 
 
 """ OtaClientStub """
+import ota_client
 import ota_client_call
+import ecu_info
 
 
 class OtaClientStub:
-    def __init__(self, ota_client):
-        self._ota_client = ota_client
+    def __init__(self):
+        self._ota_client = OtaClient()
+        self._ecu_info = EcuInfo()
         self._ota_client_call = OtaClientCall("50051")
 
     def update(self, request):
-        reply = []
+        response = []
 
         # secondary ecus
-        secondary_ecu_ids = self._ota_client.get_secondary_ecu_ids()
-        for secondary in secondary_ecu_ids:
+        secondary_ecus = self._ecu_info.get_secondary_ecus()
+        for secondary in secondary_ecus:
             entry = OtaClientStub._find_request(request, secondary)
             if entry:
-                r = self._ota_client_call.update(request, entry.ip_addr)
-                reply.append(r)
+                r = self._ota_client_call.update(request, secondary["ip_addr"])
+                response.append(r)
 
         # my ecu
-        ecu_id = self._ota_client.get_ecu_id()  # my ecu id
+        ecu_id = self._ecu_info.get_ecu_id()  # my ecu id
         entry = OtaClientStub._find_request(request, ecu_id)
         if entry:
             r = self._ota_client.update(entry.version, entry.url, entry.signed_cookies)
-            reply.append(r)
+            response.append(r)
 
-        return reply
+        return response
 
     def rollback(self, request):
-        reply = []
+        response = []
 
         # secondary ecus
-        secondary_ecu_ids = self._ota_client.get_secondary_ecu_ids()
-        for secondary in secondary_ecu_ids:
+        secondary_ecus = self._ecu_info.get_secondary_ecus()
+        for secondary in secondary_ecus:
             entry = OtaClientStub._find_request(request, secondary)
             if entry:
-                r = self._ota_client_call.rollback(request, entry.ip_addr)
-                reply.append(r)
+                r = self._ota_client_call.rollback(request, secondary["ip_addr"])
+                response.append(r)
 
         # my ecu
-        ecu_id = self._ota_client.get_ecu_id()  # my ecu id
+        ecu_id = self._ecu_info.get_ecu_id()  # my ecu id
         entry = OtaClientStub._find_request(request, ecu_id)
         if entry:
             r = self._ota_client.rollback()
-            reply.append(r)
-        return reply
+            response.append(r)
+        return response
 
     def status(self, request):
+        response = []
+
         # secondary ecus
-        for secondary in secondary_ecu_ids:
-            r = self._ota_client_call.status(request, entry.ip_addr)
-            reply.append(r)
+        secondary_ecus = self._ecu_info.get_secondary_ecus()
+        for secondary in secondary_ecus:
+            r = self._ota_client_call.status(request, secondary["ip_addr"])
+            response.append(r)
 
         # my ecu
         r = self._ota_client.status()
-        reply.append(r)
-        return reply
+        response.append(r)
+        return response
 
     @staticmethod
     def _find_request(update_request, ecu_id):
@@ -211,10 +212,25 @@ class OtaStatusControl:
         self._grub_control = GrubControl()
         self._ota_status = self._get_initial_ota_status()
 
-    def enter_updating(self, mount_path):
+    def get_boot_standby_path(self):
+        standby_boot = self._ota_partition.get_standby_boot_partition()
+        return f"/boot/ota-partition.{standby_boot}/"
+
+    def get_ota_status(self):
+        return self._ota_status
+
+    def enter_updating(self, version, mount_path):
+        if self.ota_status not in [
+            OtaStatus.INITIALIZED,
+            OtaStatus.SUCCESS,
+            OtaStatus.FAILURE,
+            OtaStatus.ROLLBACK_FAILURE,
+        ]:
+            raise ValueError(f"status={self.status} is illegal for update")
         standby_boot = self._ota_partition.get_standby_boot_partition()
         standby_status_path = f"/boot/ota-partition.{standby_boot}/status"
-        # TODO: lock if grpc is using thread
+        standby_version_path = f"/boot/ota-partition.{standby_boot}/version"
+        self._store_ota_version(standby_version_path, version)
         self._store_ota_status(standby_status_path, OtaStatus.UPDATING.name)
         self._ota_status = OtaStatus.UPDATING
         # TODO: mount standby partition
@@ -222,18 +238,26 @@ class OtaStatusControl:
 
     def leave_updating(self, mounted_path):
         # TODO: umount mounted_path
-        self._ota_partition.switch_boot_partition()
+        standby_boot = self._ota_partition.get_standby_boot_partition()
+        self._ota_partition.update_fstab(standby_boot)
+        self._ota_partition.update_boot_partition(standby_boot)
         self._grub_control.create_custom_cfg_and_reboot()
 
     def enter_rollbacking(self):
+        if self.ota_status not in [
+            OtaStatus.SUCCESS,
+            OtaStatus.ROLLBACK_FAILURE,
+        ]:
+            raise ValueError(f"status={self.status} is illegal for rollback")
         standby_boot = self._ota_partition.get_standby_boot_partition()
         standby_status_path = f"/boot/ota-partition.{standby_boot}/status"
-        # TODO: lock if grpc is using thread
         self._store_ota_status(standby_status_path, OtaStatus.ROLLBACKING.name)
         self._ota_status = OtaStatus.ROLLBACKING
 
     def leave_rollbacking(self):
-        self._ota_partition.switch_boot_partition()
+        standby_boot = self._ota_partition.get_standby_boot_partition()
+        self._ota_partition.update_fstab(standby_boot)
+        self._ota_partition.update_boot_partition(standby_boot)
         self._grub_control.create_custom_cfg_and_reboot()
 
     def _get_initial_ota_status(self):
@@ -243,7 +267,7 @@ class OtaStatusControl:
         standby_root = self._ota_partition.get_standby_root_partition()
 
         if active_boot != active_root:
-            self._ota_partition.switch_boot_partition()
+            self._ota_partition.update_boot_partition(active_root)
             self._grub_control.reboot()
 
         active_status_path = f"/boot/ota-partition.{active_boot}/status"
@@ -285,20 +309,35 @@ class OtaStatusControl:
         # and move to path
         pass
 
+    def _load_ota_version(self, path):
+        try:
+            with open(path) as f:
+                version = f.read().strip()  # if it contains whitespace
+                return version
+        except FileNotFoundError as e:
+            return ""
+
+    def _store_ota_version(self, path, version):
+        # TODO:
+        # create temp file
+        # and write ota_status to it
+        # and move to path
+        pass
+
 
 """ GrubControl """
 
 
 class GrubControl:
-    def __init__(self):
-        pass
-
+    @staticmethod
     def create_custom_cfg_and_reboot():
         # custom.cfg
+        # count custom.cfg menuentry number
         # grub-reboot
         # reboot
         pass
 
+    @staticmethod
     def update_grub_cfg():
         # update /etc/default/grub w/ GRUB_DISABLE_SUBMENU
         # grub-mkconfig temporally
@@ -306,6 +345,7 @@ class GrubControl:
         # grub-mkconfig w/ the number counted
         pass
 
+    @staticmethod
     def reboot():
         # reboot
         pass
@@ -315,20 +355,52 @@ class GrubControl:
 
 
 class OtaPrtition:
-    def __init__():
-        pass
-
+    @classmethod
     def get_active_boot_partition():
         pass
 
+    @classmethod
     def get_standby_boot_partition():
         pass
 
+    @classmethod
     def get_active_root_partition():
         pass
 
+    @classmethod
     def get_standby_root_partition():
         pass
 
-    def switch_boot_partition():
+    @staticmethod
+    def update_boot_partition(partition):
         pass
+
+    @staticmethod
+    def update_fstab_root_partition(partition):
+        # retrieve uuid from the partion
+        # retrieve device file from the partion
+        pass
+
+
+""" EcuInfo """
+import yaml
+
+
+class EcuInfo:
+    def __init__(self):
+        ecu_info_path = "/boot/ota/ecu_info.yaml"
+        self._ecu_info = self._load_ecu_info(ecu_info_path)
+
+    def get_secondary_ecus():
+        return self._ecu_info["secondaries"]
+
+    def get_ecu_id():
+        return self._ecu_info["ecu_id"]
+
+    def _load_ecu_info(self):
+        with open(self._ecu_info_path) as f:
+            ecu_info = yaml.load(f, Loader=yaml.SafeLoader)
+            format_version = ecu_info["format_version"]
+            if format_version != 1:
+                raise ValueError(f"format_version={format_version} is illegal")
+            return ecu_info
