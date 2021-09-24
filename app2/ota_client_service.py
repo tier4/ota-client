66e94cf32904c4b13eada9b3e4afb6ee914229e0
hiroyuki.obinata <hiroyuki.obinata@tier4.jp>
2021-09-24 19:13:23 +0900

    add v1 service

--- app2/ota_client_service.py
+++ app2/ota_client_service.py
@@ -2,17 +2,17 @@ from concurrent import futures
 from logging import getLogger
 
 import grpc
-import otaclient_pb2 as v1  # to keep backword compatibility
-import otaclient_pb2_grpc as grpc_v1  # to keep backword compatibility
+import otaclient_pb2 as v1  # to keep backward compatibility
+import otaclient_pb2_grpc as v1_grpc  # to keep backward compatibility
 import otaclient_v2_pb2 as v2
-import otaclient_v2_pb2_grpc as grpc_v2
+import otaclient_v2_pb2_grpc as v2_grpc
 import configs as cfg
 
 logger = getLogger(__name__)
 logger.setLevel(cfg.LOG_LEVEL_TABLE.get(__name__, cfg.DEFAULT_LOG_LEVEL))
 
 
-class OtaClientServiceV2(grpc_v2.OtaClientServiceServicer):
+class OtaClientServiceV2(v2_grpc.OtaClientServiceServicer):
     def __init__(self, ota_client_stub):
         self._stub = ota_client_stub
         self._server = None
@@ -46,7 +46,7 @@ class OtaClientServiceV2(grpc_v2.OtaClientServiceServicer):
 
     def service_start(self, port):
         server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
-        grpc_v2.add_OtaClientServiceServicer_to_server(self, server)
+        v2_grpc.add_OtaClientServiceServicer_to_server(self, server)
         server.add_insecure_port(port)
         server.start()
         self._server = server
@@ -56,3 +56,71 @@ class OtaClientServiceV2(grpc_v2.OtaClientServiceServicer):
 
     def service_stop(self):
         self._server.stop(None)
+
+
+# DEPRECATED
+class OtaClientService(v1_grpc.OtaClientServiceServicer):
+    def __init__(self, ota_client_stub):
+        self._stub = ota_client_stub
+        self._server = None
+
+    def OtaUpdate(self, request, context):
+        # TODO: convert v1 request to v2 request
+        results = self._stub.update(request)
+        response = v1.OtaUpdateReply()
+        # TODO: convert v2 result to v1 response
+        """
+        for result in results:
+            res_ecu = response.ecu.add()
+            res_ecu.ecu_id = result["ecu_id"]
+            res_ecu.result = result["result"]
+        """
+        return response
+
+    def OtaRollback(self, request, context):
+        return OtaRollbackReply()
+
+    def EcuStatus(self, request, context):
+        # TODO: convert v1 request to v2 request
+        results = self._stub.status(request)
+        response = v1.EcuStatusReply()
+        # TODO: convert v2 result to v1 response
+        """
+        for result in results:
+            res_ecu = response.ecu.add()
+            res_ecu.ecu_id = result["ecu_id"]
+            res_ecu.result = result["result"]
+        """
+        return response
+
+    def EcuVersion(self, request, context):
+        # TODO: convert v1 request to v2 request
+        results = self._stub.status(request)
+        response = v1.EcuVersionReply()
+        # TODO: convert v2 result to v1 response
+        """
+        for result in results:
+            res_ecu = response.ecu.add()
+            res_ecu.ecu_id = result["ecu_id"]
+            res_ecu.result = result["result"]
+        """
+        return response
+
+
+def service_start(port, service_list):
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
+    for service in service_list:
+        service["grpc"].add_OtaClientServiceServicer_to_server(
+            service["instance"], server
+        )
+    server.add_insecure_port(port)
+    server.start()
+    return server
+
+
+def service_wait_for_termination(server):
+    server.wait_for_termination()
+
+
+def service_stop(server):
+    server.stop(None)
--- tests2/test_ota_client_service.py
+++ tests2/test_ota_client_service.py
@@ -5,22 +5,38 @@ import grpc
 
 @pytest.fixture
 def start_service_with_ota_client_mock(mocker):
-    from ota_client_service import OtaClientServiceV2
+    from ota_client_service import (
+        OtaClientServiceV2,
+        OtaClientService,
+        service_start,
+        service_stop,
+    )
     from ota_client_stub import OtaClientStub
     from ota_client import OtaClient
     import otaclient_v2_pb2 as v2
     import otaclient_v2_pb2_grpc as v2_grpc
+    import otaclient_pb2 as v1
+    import otaclient_pb2_grpc as v1_grpc
 
     ota_client_mock = mocker.Mock(spec=OtaClient)
     mocker.patch("ota_client_stub.OtaClient", return_value=ota_client_mock)
 
     ota_client_stub = OtaClientStub()
-    ota_client_service = OtaClientServiceV2(ota_client_stub)
-    ota_client_service.service_start("localhost:50051")
+
+    ota_client_service_v2 = OtaClientServiceV2(ota_client_stub)
+    ota_client_service = OtaClientService(ota_client_stub)
+
+    server = service_start(
+        "localhost:50051",
+        [
+            {"grpc": v1_grpc, "instance": ota_client_service},
+            {"grpc": v2_grpc, "instance": ota_client_service_v2},
+        ],
+    )
 
     yield ota_client_mock
 
-    ota_client_service.service_stop()
+    service_stop(server)
 
 
 def test_ota_client_service_update(mocker, start_service_with_ota_client_mock):
