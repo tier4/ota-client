631bc48adaf16709856bb1bc6bf5e0b4f2109c40
Yoshifumi Hayashi <yoshifumi.hayashi@tier4.jp>
2021-10-04 10:33:00 +0900

    add testcases

--- tests/aws_gglog/test_boto3_session.py
+++ tests/aws_gglog/test_boto3_session.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 
 
 class TestBoto3Session:
@@ -13,6 +14,24 @@ class TestBoto3Session:
         assert config.get("cert") == "/greengrass/certs/gg.cert.pem"
         assert config.get("region") == "ap-northeast-1"
         assert config.get("thing_name") == "foo-bar"
+
+    def test_parse_config_no_file(self):
+        from aws_gglog.boto3_session import Boto3Session
+
+        with pytest.raises(Exception) as e:
+            Boto3Session.parse_config("no_file")
+
+        assert str(e.value) == "[Errno 2] No such file or directory: 'no_file'"
+
+    def test_parse_config_invalid_thing_arn(self, shared_datadir):
+        from aws_gglog.boto3_session import Boto3Session
+
+        with pytest.raises(Exception) as e:
+            Boto3Session.parse_config(
+                os.path.join(shared_datadir, "greengrass/config_invalid_thingArn.json")
+            )
+
+        assert str(e.value) == "invalid thing arn: thing_arn=thing/foo-bar"
 
     def test__refresh_credentials(self, mocker, shared_datadir):
         import requests
