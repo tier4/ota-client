0f20dbf2a48dff9b4c16d8b1bd5b83d21012b7ca
Yoshifumi Hayashi <yoshifumi.hayashi@tier4.jp>
2021-10-23 16:17:03 +0900

    add to config root logger

--- app/log_util.py
+++ app/log_util.py
@@ -8,6 +8,9 @@ def get_logger(name: str, level: int) -> logging.Logger:
     if os.environ.get("AWS_CLOUDWATCH_LOG_ENABLE") == "true":
         return aws_gglog.logger.Logger(name, level).get_logger()
     else:
+        logging.basicConfig(
+            format="[%(asctime)s][%(levelname)s]-%(filename)s:%(lineno)d,%(message)s",
+        )
         logger = logging.getLogger(name)
         logger.setLevel(level)
         return logger
