54951c871a01f81ea01fb771c7549e8382581072
Bodong Yang <bodong.yang@tier4.jp>
2021-11-18 15:52:52 +0900

    enable logger

--- app/log_util.py
+++ app/log_util.py
@@ -2,7 +2,8 @@ import os
 import logging
 
 import aws_gglog.logger
-
+logging.basicConfig()
+logging.root.setLevel(logging.INFO)
 
 def get_logger(name: str, level: int) -> logging.Logger:
     if os.environ.get("AWS_CLOUDWATCH_LOG_ENABLE") == "true":
