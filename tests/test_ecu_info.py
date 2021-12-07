b4eb0f39efb4bd16c9f9d0acad852f18d4909ef8
hiroyuki obinata <58019445+obi-t4@users.noreply.github.com>
2021-12-07 09:47:25 +0900

    [T4PB-12973] remove format version check (#85)
    https://tier4.atlassian.net/browse/T4PB-12973

--- tests/test_ecu_info.py
+++ tests/test_ecu_info.py
@@ -8,6 +8,7 @@ import yaml
     (
         (None, [], "autoware"),
         ({"format_version": 1, "ecu_id": "autoware"}, [], "autoware"),
+        ({"format_version": 2, "ecu_id": "autoware"}, [], "autoware"),
         (
             {
                 "format_version": 1,
@@ -39,26 +40,3 @@ def test_ecu_info(mocker, tmp_path, ecu_info_dict, secondary_ecus, ecu_id):
     ecu_info = EcuInfo()
     assert ecu_info.get_secondary_ecus() == secondary_ecus
     assert ecu_info.get_ecu_id() == ecu_id
-
-
-@pytest.mark.parametrize(
-    "ecu_info_dict",
-    (
-        ({"format_version": 2, "ecu_id": "autoware"}),
-        ({"ecu_id": "autoware"}),
-    ),
-)
-def test_ecu_info_exception(mocker, tmp_path, ecu_info_dict):
-    from ecu_info import EcuInfo
-    from ota_error import OtaErrorUnrecoverable
-
-    boot_dir = tmp_path / "boot"
-    boot_dir.mkdir()
-    (boot_dir / "ota").mkdir()
-    ecu_info_file = boot_dir / "ota" / "ecu_info.yaml"
-    if ecu_info_dict is not None:
-        ecu_info_file.write_text(yaml.dump(ecu_info_dict))
-
-    mocker.patch.object(EcuInfo, "ECU_INFO_FILE", ecu_info_file)
-    with pytest.raises(OtaErrorUnrecoverable):
-        ecu_info = EcuInfo()
