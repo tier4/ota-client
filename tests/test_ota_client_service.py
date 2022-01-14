a205d5c795e8c7a869154db0f2e9e26bf52b7dc1
hiroyuki.obinata <hiroyuki.obinata@tier4.jp>
2022-01-14 15:54:24 +0900

    add test

--- tests/test_ota_client_service.py
+++ tests/test_ota_client_service.py
@@ -14,7 +14,6 @@ def start_service_with_ota_client_mock(mocker):
     )
     from ota_client_stub import OtaClientStub
     from ota_client import OtaClient
-    import otaclient_v2_pb2 as v2
     import otaclient_v2_pb2_grpc as v2_grpc
 
     ota_client_mock = mocker.Mock(spec=OtaClient)
@@ -31,7 +30,7 @@ def start_service_with_ota_client_mock(mocker):
         ],
     )
 
-    yield ota_client_mock
+    yield ota_client_mock, ota_client_stub
 
     service_stop(server)
 
@@ -41,7 +40,7 @@ def test_ota_client_service_update(mocker, start_service_with_ota_client_mock):
     import otaclient_v2_pb2_grpc as v2_grpc
     from ota_client import OtaClientFailureType
 
-    ota_client_mock = start_service_with_ota_client_mock
+    ota_client_mock, _ = start_service_with_ota_client_mock
 
     def mock_update(version, url, cookies, pre_update_event, post_update_event):
         pre_update_event.set()
@@ -79,7 +78,7 @@ def test_ota_client_service_rollback(mocker, start_service_with_ota_client_mock)
     import otaclient_v2_pb2_grpc as v2_grpc
     from ota_client import OtaClientFailureType
 
-    ota_client_mock = start_service_with_ota_client_mock
+    ota_client_mock, _ = start_service_with_ota_client_mock
     ota_client_mock.rollback.return_value = OtaClientFailureType.NO_FAILURE
 
     with grpc.insecure_channel("localhost:50051") as channel:
@@ -103,7 +102,7 @@ def test_ota_client_service_status(mocker, start_service_with_ota_client_mock):
     import otaclient_v2_pb2_grpc as v2_grpc
     from ota_client import OtaClientFailureType
 
-    ota_client_mock = start_service_with_ota_client_mock
+    ota_client_mock, _ = start_service_with_ota_client_mock
     status = {
         "status": "SUCCESS",
         "failure_type": "NO_FAILURE",
@@ -134,6 +133,222 @@ def test_ota_client_service_status(mocker, start_service_with_ota_client_mock):
         response = service.Status(request)
 
         response_exp = v2.StatusResponse()
+        res_ecu = response_exp.ecu.add()
+        res_ecu.ecu_id = "autoware"
+        res_ecu.result = v2.FailureType.NO_FAILURE
+
+        res_ecu.status.status = v2.StatusOta.SUCCESS
+        res_ecu.status.failure = v2.FailureType.NO_FAILURE
+        res_ecu.status.failure_reason = ""
+        res_ecu.status.version = "1.2.3"
+        res_ecu.status.progress.phase = v2.StatusProgressPhase.REGULAR
+        res_ecu.status.progress.total_regular_files = 99
+        res_ecu.status.progress.regular_files_processed = 10
+
+        res_ecu.status.progress.files_processed_copy = 50
+        res_ecu.status.progress.files_processed_link = 9
+        res_ecu.status.progress.files_processed_download = 40
+        res_ecu.status.progress.file_size_processed_copy = 1000
+        res_ecu.status.progress.file_size_processed_link = 100
+        res_ecu.status.progress.file_size_processed_download = 1000
+
+        res_ecu.status.progress.elapsed_time_copy.FromMilliseconds(1230)
+        res_ecu.status.progress.elapsed_time_link.FromMilliseconds(120)
+        res_ecu.status.progress.elapsed_time_download.FromMilliseconds(9870)
+        res_ecu.status.progress.errors_download = 10
+
+        assert response == response_exp
+
+    ota_client_mock.status.assert_called_once()
+
+
+def test_ota_client_service_update_with_secondary(
+    mocker, start_service_with_ota_client_mock
+):
+    import otaclient_v2_pb2 as v2
+    import otaclient_v2_pb2_grpc as v2_grpc
+    from ota_client import OtaClientFailureType
+
+    ota_client_mock, ota_client_stub = start_service_with_ota_client_mock
+
+    def mock_update(version, url, cookies, pre_update_event, post_update_event):
+        pre_update_event.set()
+        return OtaClientFailureType.NO_FAILURE
+
+    ota_client_mock.update.side_effect = mock_update
+
+    mocker.patch.object(
+        ota_client_stub._ecu_info,
+        "get_secondary_ecus",
+        return_value=[{"ecu_id": "subecu", "ip_addr": "192.168.1.2"}],
+    )
+
+    sub = v2.UpdateResponse()
+    sub_ecu = sub.ecu.add()
+    sub_ecu.ecu_id = "subecu"
+    sub_ecu.result = v2.FailureType.NO_FAILURE
+    mocker.patch.object(ota_client_stub._ota_client_call, "update", return_value=sub)
+
+    with grpc.insecure_channel("localhost:50051") as channel:
+        request = v2.UpdateRequest()
+        # "autoware" ecu
+        req_ecu = request.ecu.add()
+        req_ecu.ecu_id = "autoware"
+        req_ecu.version = "1.2.3.a"
+        req_ecu.url = "https://foo.bar.com/ota-data"
+        req_ecu.cookies = json.dumps({"test": "my data"})
+        # "subecu" ecu
+        req_ecu = request.ecu.add()
+        req_ecu.ecu_id = "subecu"
+        req_ecu.version = "4.5.6.a"
+        req_ecu.url = "https://foo.bar.com/ota-data-subecu"
+        req_ecu.cookies = json.dumps({"test": "subecu data"})
+
+        service = v2_grpc.OtaClientServiceStub(channel)
+        response = service.Update(request)
+
+        response_exp = v2.UpdateResponse()
+        # "autoware" ecu
+        res_ecu = response_exp.ecu.add()
+        res_ecu.ecu_id = "autoware"
+        res_ecu.result = v2.FailureType.NO_FAILURE
+        # "subecu" ecu
+        res_ecu = response_exp.ecu.add()
+        res_ecu.ecu_id = "subecu"
+        res_ecu.result = v2.FailureType.NO_FAILURE
+        assert response == response_exp
+
+    ota_client_mock.update.assert_called_once_with(
+        "1.2.3.a",
+        "https://foo.bar.com/ota-data",
+        '{"test": "my data"}',
+        pre_update_event=ANY,
+        post_update_event=ANY,
+    )
+
+
+def test_ota_client_service_rollback_with_secondary(
+    mocker, start_service_with_ota_client_mock
+):
+    import otaclient_v2_pb2 as v2
+    import otaclient_v2_pb2_grpc as v2_grpc
+    from ota_client import OtaClientFailureType
+
+    ota_client_mock, ota_client_stub = start_service_with_ota_client_mock
+    ota_client_mock.rollback.return_value = OtaClientFailureType.NO_FAILURE
+
+    mocker.patch.object(
+        ota_client_stub._ecu_info,
+        "get_secondary_ecus",
+        return_value=[{"ecu_id": "subecu", "ip_addr": "192.168.1.2"}],
+    )
+
+    sub = v2.RollbackResponse()
+    sub_ecu = sub.ecu.add()
+    sub_ecu.ecu_id = "subecu"
+    sub_ecu.result = v2.FailureType.NO_FAILURE
+    mocker.patch.object(ota_client_stub._ota_client_call, "rollback", return_value=sub)
+
+    with grpc.insecure_channel("localhost:50051") as channel:
+        request = v2.RollbackRequest()
+        # "autoware" ecu
+        req_ecu = request.ecu.add()
+        req_ecu.ecu_id = "autoware"
+        # "subecu" ecu
+        req_ecu = request.ecu.add()
+        req_ecu.ecu_id = "subecu"
+
+        service = v2_grpc.OtaClientServiceStub(channel)
+        response = service.Rollback(request)
+
+        response_exp = v2.RollbackResponse()
+        # "subecu" ecu
+        res_ecu = response_exp.ecu.add()
+        res_ecu.ecu_id = "subecu"
+        res_ecu.result = v2.FailureType.NO_FAILURE
+        # "autoware" ecu
+        res_ecu = response_exp.ecu.add()
+        res_ecu.ecu_id = "autoware"
+        res_ecu.result = v2.FailureType.NO_FAILURE
+        assert response == response_exp
+
+    ota_client_mock.rollback.assert_called_once()
+
+
+def test_ota_client_service_status_with_secondary(
+    mocker, start_service_with_ota_client_mock
+):
+    import otaclient_v2_pb2 as v2
+    import otaclient_v2_pb2_grpc as v2_grpc
+    from ota_client import OtaClientFailureType
+
+    ota_client_mock, ota_client_stub = start_service_with_ota_client_mock
+    status = {
+        "status": "SUCCESS",
+        "failure_type": "NO_FAILURE",
+        "failure_reason": "",
+        "version": "1.2.3",
+        "update_progress": {
+            "phase": "REGULAR",
+            "total_regular_files": 99,
+            "regular_files_processed": 10,
+            "files_processed_copy": 50,
+            "files_processed_link": 9,
+            "files_processed_download": 40,
+            "file_size_processed_copy": 1000,
+            "file_size_processed_link": 100,
+            "file_size_processed_download": 1000,
+            "elapsed_time_copy": 1230,
+            "elapsed_time_link": 120,
+            "elapsed_time_download": 9870,
+            "errors_download": 10,
+        },
+    }
+
+    ota_client_mock.status.return_value = OtaClientFailureType.NO_FAILURE, status
+    mocker.patch.object(
+        ota_client_stub._ecu_info,
+        "get_secondary_ecus",
+        return_value=[{"ecu_id": "subecu", "ip_addr": "192.168.1.2"}],
+    )
+    sub = v2.StatusResponse()
+    sub_ecu = sub.ecu.add()
+    sub_ecu.ecu_id = "subecu"
+    sub_ecu.result = v2.FailureType.NO_FAILURE
+
+    sub_ecu.status.status = v2.StatusOta.SUCCESS
+    sub_ecu.status.failure = v2.FailureType.NO_FAILURE
+    sub_ecu.status.failure_reason = ""
+    sub_ecu.status.version = "4.5.6"
+    sub_ecu.status.progress.phase = v2.StatusProgressPhase.REGULAR
+    sub_ecu.status.progress.total_regular_files = 999
+    sub_ecu.status.progress.regular_files_processed = 109
+
+    sub_ecu.status.progress.files_processed_copy = 509
+    sub_ecu.status.progress.files_processed_link = 99
+    sub_ecu.status.progress.files_processed_download = 409
+    sub_ecu.status.progress.file_size_processed_copy = 10009
+    sub_ecu.status.progress.file_size_processed_link = 1009
+    sub_ecu.status.progress.file_size_processed_download = 10009
+
+    sub_ecu.status.progress.elapsed_time_copy.FromMilliseconds(12309)
+    sub_ecu.status.progress.elapsed_time_link.FromMilliseconds(1209)
+    sub_ecu.status.progress.elapsed_time_download.FromMilliseconds(98709)
+    sub_ecu.status.progress.errors_download = 109
+
+    mocker.patch.object(ota_client_stub._ota_client_call, "status", return_value=sub)
+
+    with grpc.insecure_channel("localhost:50051") as channel:
+        request = v2.StatusRequest()
+        service = v2_grpc.OtaClientServiceStub(channel)
+        response = service.Status(request)
+
+        response_exp = v2.StatusResponse()
+        # "subecu" ecu
+        res_ecu = response_exp.ecu.add()
+        res_ecu.CopyFrom(sub_ecu)
+
+        # "autoware" ecu
         res_ecu = response_exp.ecu.add()
         res_ecu.ecu_id = "autoware"
         res_ecu.result = v2.FailureType.NO_FAILURE
