8aaf099cb994df9d3a16c841208ad07c164ae095
hiroyuki obinata <58019445+obi-t4@users.noreply.github.com>
2022-01-19 15:21:33 +0900

    use py38 (#98)
    * use python3.8, minor fix to call main()
    https://tier4.atlassian.net/browse/T4PB-14182

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,17 +3,17 @@ import pytest
 
 @pytest.fixture
 def patch(mocker):
+    import random
     from ota_client import OtaClient
-    from ecu_info import EcuInfo
     from ota_client_call import OtaClientCall
     from ota_client_service import OtaClientServiceV2
 
     mocker.patch.object(OtaClient, "__init__", return_value=None)
-    mocker.patch.object(EcuInfo, "__init__", return_value=None)
     mocker.patch.object(OtaClientCall, "__init__", return_value=None)
     mocker.patch.object(OtaClientServiceV2, "__init__", return_value=None)
     mocker.patch("main.service_start", return_value=None)
     mocker.patch("main.service_wait_for_termination", return_value=None)
+    mocker.patch("main.os.getpid", lambda: random.getrandbits(64))
 
 
 def test_main(patch, mocker):
@@ -30,7 +30,7 @@ def test_main_with_version(patch, mocker, tmp_path, caplog):
     mocker.patch("main.VERSION_FILE", version)
 
     main()
-    assert len(caplog.records) == 2
+    assert caplog.records[0].msg == "started"
     assert (
         caplog.records[1].msg == "d3b6bdb | 2021-10-27 09:36:48 +0900 | Initial commit"
     )
