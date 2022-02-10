44e838187d23130e8dd66ea90442cd291735ff87
Bodong Yang <bodong.yang@tier4.jp>
2022-02-10 15:25:30 +0900

    proxy_info.py: prepare default proxy_info if proxy_info.yaml is not presented

--- app/proxy_info.py
+++ app/proxy_info.py
@@ -1,4 +1,5 @@
 import yaml
+from pathlib import Path
 
 from configs import config as cfg
 import log_util
@@ -17,11 +18,19 @@ generic_proxy: str
 local_server: Tuple[str, int]
 """
 
+DEFUALT_PROXY_INFO = """
+gateway: false
+enable_ota_proxy: false
+"""
+
 class ProxyInfo:
 
     def __init__(self, proxy_info_file: str=cfg.PROXY_INFO_FILE):
-        with open(proxy_info_file, 'r') as f:
-            proxy_info: dict = yaml.safe_load(f)
+        proxy_info_file_path = Path(proxy_info_file)
+        if proxy_info_file_path.is_file():
+            proxy_info: dict = yaml.safe_load(proxy_info_file_path.read_text())
+        else:
+            proxy_info: dict = yaml.safe_load(DEFUALT_PROXY_INFO)
 
         self.enable_local_ota_proxy: bool = proxy_info.get("enable_ota_proxy", False)
         self.upper_ota_proxy: str = proxy_info.get("upper_ota_proxy")
@@ -43,9 +52,12 @@ class ProxyInfo:
             return self.generic_proxy
         else:
             # default not using proxy
-            return
+            return ""
 
     def get_generic_proxy(self) -> str:
-        return self.generic_proxy
+        if self.generic_proxy:
+            return self.generic_proxy
+        else:
+            return ""
 
 proxy_cfg = ProxyInfo()
