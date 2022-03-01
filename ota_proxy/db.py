39225deb9f2b013cacd586d302ff13f6b22fef87
Bodong Yang <bodong.yang@tier4.jp>
2022-03-02 00:36:42 +0900

    db: refactor

--- ota_proxy/db.py
+++ ota_proxy/db.py
@@ -1,9 +1,11 @@
 import sqlite3
+from contextlib import contextmanager
 from dataclasses import dataclass
 from pathlib import Path
 from threading import Lock
 
 import logging
+from typing import Any, List
 
 from .config import config as cfg
 
@@ -13,11 +15,11 @@ logger.setLevel(cfg.LOG_LEVEL)
 
 @dataclass
 class CacheMeta:
-    url: str
-    hash: str
-    size: int
-    content_type: str
-    content_encoding: str
+    url: str = ""
+    hash: str = ""
+    size: int = 0
+    content_type: str = ""
+    content_encoding: str = ""
 
     def to_tuple(self) -> tuple:
         return (
@@ -27,6 +29,14 @@ class CacheMeta:
             self.content_type,
             self.content_encoding,
         )
+
+    @classmethod
+    def row_to_meta(cls, row: sqlite3.Row):
+        res = cls()
+        for k in OTACacheDB.COLUMNS:
+            setattr(res, k, row[k])
+
+        return res
 
 
 class OTACacheDB:
@@ -38,6 +48,14 @@ class OTACacheDB:
         "content_type": 3,
         "content_encoding": 4,
     }
+    INIT_DB: str = (
+        f"CREATE TABLE {TABLE_NAME}("
+        "url text UNIQUE PRIMARY KEY,"
+        "hash text NOT NULL, "
+        "size real NOT NULL,"
+        "content_type text, "
+        "content_encoding text)"
+    )
 
     def __init__(self, db_file: str, init: bool = False):
         logger.debug("init database...")
@@ -47,6 +65,29 @@ class OTACacheDB:
 
         self._connect_db(init)
 
+    @contextmanager
+    def _general_query(
+        self,
+        query: str,
+        query_param: List[Any],
+        multiple_query: bool = True,
+        init: bool = False,
+    ):
+        if not init and self._closed:
+            raise sqlite3.OperationalError("connect is closed")
+
+        cur = self._con.cursor()
+        if multiple_query:
+            query_param = [(p,) for p in query_param]
+            cur.executemany(query, query_param)
+        else:
+            cur.execute(query, query_param)
+
+        try:
+            yield cur
+        finally:
+            cur.close()
+
     def close(self):
         logger.debug("closing db...")
         if not self._closed:
@@ -55,91 +96,56 @@ class OTACacheDB:
 
     def _init_table(self):
         logger.debug("init sqlite database...")
-        cur = self._con.cursor()
-
-        # create the table
-        cur.execute(
-            f"""CREATE TABLE {self.TABLE_NAME}(
-                    url text UNIQUE PRIMARY KEY, 
-                    hash text NOT NULL, 
-                    size real NOT NULL,
-                    content_type text, 
-                    content_encoding text)"""
-        )
-
-        self._con.commit()
-        cur.close()
+        with self._general_query(self.INIT_DB, (), multiple_query=False, init=True):
+            self._con.commit()
 
     def _connect_db(self, init: bool):
         if init:
-            # remove the old db file if needed
             Path(self._db_file).unlink(missing_ok=True)
 
         self._con = sqlite3.connect(self._db_file, check_same_thread=False)
+        self._con.row_factory = sqlite3.Row
 
         # check if the table exists
-        cur = self._con.cursor()
-        cur.execute(
+        with self._general_query(
             "SELECT name FROM sqlite_master WHERE type='table' AND name=?",
             (self.TABLE_NAME,),
-        )
-        if cur.fetchone() is None:
-            self._init_table()
+            multiple_query=False,
+            init=True,
+        ) as cur:
+            if cur.fetchone() is None:
+                self._init_table()
 
     def remove_url_by_hash(self, *hash: str):
-        if self._closed:
-            raise sqlite3.OperationalError("connect is closed")
-
-        hash_list = [(h,) for h in hash]
-        with self._wlock:
-            cur = self._con.cursor()
-            cur.executemany(f"DELETE FROM {self.TABLE_NAME} WHERE hash=?", hash_list)
-
+        with self._wlock, self._general_query(
+            f"DELETE FROM {self.TABLE_NAME} WHERE hash=?", hash
+        ):
             self._con.commit()
-            cur.close()
-        return
 
     def remove_urls(self, *urls: str):
-        if self._closed:
-            raise sqlite3.OperationalError("connect is closed")
-
-        url_list = [(u,) for u in urls]
-        with self._wlock:
-            cur = self._con.cursor()
-            cur.executemany(f"DELETE FROM {self.TABLE_NAME} WHERE url=?", url_list)
-
+        with self._wlock, self._general_query(
+            f"DELETE FROM {self.TABLE_NAME} WHERE url=?", urls
+        ):
             self._con.commit()
-            cur.close()
 
     def insert_urls(self, *cache_meta: CacheMeta):
-        if self._closed:
-            raise sqlite3.OperationalError("connect is closed")
-
         rows = [m.to_tuple() for m in cache_meta]
-        with self._wlock:
-            cur = self._con.cursor()
-            cur.executemany(
-                f"INSERT OR REPLACE INTO {self.TABLE_NAME} VALUES (?,?,?,?,?)", rows
-            )
-
+        with self._wlock, self._general_query(
+            f"INSERT OR REPLACE INTO {self.TABLE_NAME} VALUES (?,?,?,?,?)",
+            rows,
+            multiple_query=False,
+        ):
             self._con.commit()
-            cur.close()
 
     def lookup_url(self, url: str) -> CacheMeta:
-        if self._closed:
-            raise sqlite3.OperationalError("connect is closed")
+        with self._general_query(
+            f"SELECT * FROM {self.TABLE_NAME} WHERE url=?", (url,), multiple_query=False
+        ) as cur:
+            return CacheMeta.row_to_meta(cur.fetchone())
 
-        cur = self._con.cursor()
-        cur.execute(f"SELECT * FROM {self.TABLE_NAME} WHERE url=?", (url,))
-        row = cur.fetchone()
-        if not row:
-            return
-
-        res = CacheMeta(
-            url=row[self.COLUMNS["url"]],
-            hash=row[self.COLUMNS["hash"]],
-            size=row[self.COLUMNS["size"]],
-            content_type=row[self.COLUMNS["content_type"]],
-            content_encoding=row[self.COLUMNS["content_encoding"]],
-        )
-        return res
+    def lookup_all(self) -> CacheMeta:
+        with self._general_query(
+            f"SELECT * FROM {self.TABLE_NAME}", (), multiple_query=False
+        ) as cur:
+            for row in cur.fetchall():
+                yield CacheMeta.row_to_meta(row)
