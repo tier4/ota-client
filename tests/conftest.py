8322ae081b52b50c12edaa69527bc9b66fd1336d
Bodong Yang <bodong.yang@tier4.jp>
2022-03-03 13:07:36 +0900

    lint the code

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,8 @@ import sys
 
 from pathlib import Path
 
+
 @pytest.fixture(autouse=True, scope="session")
 def pythonpath():
     _base_dir = Path(__file__).absolute().parent.parent
-    sys.path.extend(
-        [str(_base_dir), str(_base_dir / "app")]
-    )
+    sys.path.extend([str(_base_dir), str(_base_dir / "app")])
