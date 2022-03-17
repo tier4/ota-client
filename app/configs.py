0119aee6cb1b162fb899bcbb9d30c7f8ead3f01f
Bodong Yang <bodong.yang@tier4.jp>
2022-03-17 23:25:29 +0900

    add definition of OTAFileCacheControl to app.configs

--- app/configs.py
+++ app/configs.py
@@ -1,8 +1,53 @@
+import enum
 import platform
 from dataclasses import dataclass, field
 from logging import INFO
 
 from typing import Tuple
+
+
+class OTAFileCacheControl(enum.Enum):
+    """Custom header for ota file caching control policies.
+
+    format:
+        Ota-File-Cache-Control: <directive>
+    directives:
+        retry_cache: indicates that ota_proxy should clear cache entry for <URL>
+            and retry caching
+        no_cache: indicates that ota_proxy should not use cache for <URL>
+        use_cache: implicitly applied default value, conflicts with no_cache directive
+            no need(and no effect) to add this directive into the list
+
+    NOTE: using retry_cache and no_cache together will not work as expected,
+        only no_cache will be respected, already cached file will not be deleted as retry_cache indicates.
+    """
+
+    use_cache = "use_cache"
+    no_cache = "no_cache"
+    retry_caching = "retry_caching"
+
+    header = "Ota-File-Cache-Control"
+    header_lower = "ota-file-cache-control"
+
+    @classmethod
+    def parse_to_value_set(cls, input: str) -> "set[str]":
+        return set(input.split(","))
+
+    @classmethod
+    def parse_to_enum_set(cls, input: str) -> "set[OTAFileCacheControl]":
+        _policies_set = cls.parse_to_value_set(input)
+        res = set()
+        for p in _policies_set:
+            res.add(OTAFileCacheControl[p])
+
+        return res
+
+    @classmethod
+    def add_to(cls, target: str, input: "OTAFileCacheControl") -> str:
+        _policies_set = cls.parse_to_value_set(target)
+        _policies_set.add(input.value)
+        return ",".join(_policies_set)
+
 
 # fmt: off
 @dataclass(frozen=True)
@@ -12,7 +57,6 @@ class OtaClientServerConfig:
     WAITING_SUBECU_ACK_UPDATE_REQ_TIMEOUT: float = 300  # 5mins
     WAITING_GET_SUBECU_STATUS: float = 300  # 5mins
     WAITING_SUBECU_READY_TIMEOUT: float = 3600  # 1h
-    LOCAL_OTA_UPDATE_TIMEOUT: float = 3600  # 1h
     QUERYING_SUBECU_STATUS_TIMEOUT: float = 120  # 2mins
     LOOP_QUERYING_SUBECU_STATUS_INTERVAL: float = 10
 
--- ota_proxy/config.py
+++ ota_proxy/config.py
@@ -7,12 +7,16 @@ class OTAFileCacheControl(enum.Enum):
     """Custom header for ota file caching control policies.
 
     format:
-        Ota-File-Cache-Control: <d1>[, <d2>[, ...]]
+        Ota-File-Cache-Control: <directive>
     directives:
         retry_cache: indicates that ota_proxy should clear cache entry for <URL>
             and retry caching
         no_cache: indicates that ota_proxy should not use cache for <URL>
         use_cache: implicitly applied default value, conflicts with no_cache directive
+            no need(and no effect) to add this directive into the list
+
+    NOTE: using retry_cache and no_cache together will not work as expected,
+        only no_cache will be respected, already cached file will not be deleted as retry_cache indicates.
     """
 
     use_cache = "use_cache"
