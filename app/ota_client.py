ff931e7127578d85a79e020ac483464e0af581d8
Bodong Yang <bodong.yang@tier4.jp>
2022-05-10 19:12:38 +0900

    ota_client: special treatment for empty file

--- app/ota_client.py
+++ app/ota_client.py
@@ -207,6 +207,11 @@ class Downloader:
         if not headers:
             headers = dict()
 
+        # specially deal with empty file
+        if digest == "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855":
+            dst.write_bytes(b"")
+            return 0
+
         try:
             error_count = 0
             response = self._session.get(
