6d44ebbdff605b0b7de55eaa03bd82ba08f69ba7
Bodong Yang <bodong.yang@tier4.jp>
2022-06-09 00:15:49 +0900

    split ota_status' live_ota_status part as mixin

--- app/ota_status.py
+++ app/ota_status.py
@@ -1,8 +1,8 @@
+from enum import Enum, unique, auto
 from pathlib import Path
 
 from app.common import read_from_file, write_to_file
 from app.configs import config as cfg
-from app.interface import OTAStatusHandlerProtocol, OtaStatus
 from app.ota_error import OtaErrorBusy
 from app import log_util
 
@@ -11,43 +11,37 @@ logger = log_util.get_logger(
 )
 
 
-class OTAStatusHandler(OTAStatusHandlerProtocol):
-    def __init__(
-        self, *, standby_slot_status_file: Path, current_slot_status_file: Path
-    ) -> None:
-        self.standby_slot_status_file: Path = standby_slot_status_file
-        self.current_slot_status_file: Path = current_slot_status_file
-        self._live_ota_status: OtaStatus = None
+@unique
+class OTAStatusEnum(Enum):
+    INITIALIZED = 0
+    SUCCESS = auto()
+    FAILURE = auto()
+    UPDATING = auto()
+    ROLLBACKING = auto()
+    ROLLBACK_FAILURE = auto()
 
-    def init_ota_status(self):
-        """Apply INITIALIZED status if ota_status is not configured yet."""
-        write_to_file(self.current_slot_status_file, OtaStatus.INITIALIZED.name)
 
-    def get_current_ota_status(self) -> OtaStatus:
-        _status = read_from_file(self.current_slot_status_file)
-        return OtaStatus[_status.upper()]
+class OTAStatusMixin:
+    live_ota_status: OTAStatusEnum
 
-    def get_live_ota_status(self) -> OtaStatus:
-        return self._live_ota_status
+    def get_ota_status(self) -> OTAStatusEnum:
+        return self.live_ota_status
 
-    def set_live_ota_status(self, _status: OtaStatus):
-        self._ota_status = _status
-
-    def store_standby_ota_status(self, _status: OtaStatus):
-        write_to_file(self.standby_slot_status_file, _status.name)
+    def set_ota_status(self, _status: OTAStatusEnum):
+        self.live_ota_status = _status
 
     def request_update(self) -> None:
-        if self._ota_status not in [
-            OtaStatus.INITIALIZED,
-            OtaStatus.SUCCESS,
-            OtaStatus.FAILURE,
-            OtaStatus.ROLLBACK_FAILURE,
+        if self.live_ota_status not in [
+            OTAStatusEnum.INITIALIZED,
+            OTAStatusEnum.SUCCESS,
+            OTAStatusEnum.FAILURE,
+            OTAStatusEnum.ROLLBACK_FAILURE,
         ]:
-            raise OtaErrorBusy(f"status={self._ota_status} is illegal for update")
+            raise OtaErrorBusy(f"{self.live_ota_status=} is illegal for update")
 
     def request_rollback(self) -> None:
-        if self._ota_status not in [
-            OtaStatus.SUCCESS,
-            OtaStatus.ROLLBACK_FAILURE,
+        if self.live_ota_status not in [
+            OTAStatusEnum.SUCCESS,
+            OTAStatusEnum.ROLLBACK_FAILURE,
         ]:
-            raise OtaErrorBusy(f"status={self._ota_status} is illegal for rollback")
+            raise OtaErrorBusy(f"{self.live_ota_status=} is illegal for rollback")
