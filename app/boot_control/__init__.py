ecbc955d4bd53b5d3e6870555429daab88c272b9
Bodong Yang <bodong.yang@tier4.jp>
2022-06-13 10:33:01 +0900

    boot_control: implement boot_controller selection logic

---
+++ app/boot_control/__init__.py
@@ -0,0 +1,24 @@
+from typing import Type
+from app.boot_control.interface import BootControllerProtocol
+from app.configs import config as cfg
+
+_bootloader = cfg.BOOTLOADER
+
+
+def get_boot_controller(bootloader: str) -> Type[BootControllerProtocol]:
+    if bootloader == "grub":
+        from app.boot_control.grub import GrubController
+
+        return GrubController
+
+    elif bootloader == "cboot":
+        from app.boot_control.cboot import CBootController
+
+        return CBootController
+    else:
+        raise NotImplementedError(
+            f"boot controller is not implemented for {_bootloader=}"
+        )
+
+
+BootController: Type[BootControllerProtocol] = get_boot_controller(_bootloader)
