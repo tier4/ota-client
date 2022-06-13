23c7cad095277dc0a66b05958a791fb762306203
Bodong Yang <bodong.yang@tier4.jp>
2022-06-13 10:02:47 +0900

    make update_phase definition as a standalone module

---
+++ app/update_phase.py
@@ -0,0 +1,12 @@
+from enum import unique, Enum
+
+
+@unique
+class OtaClientUpdatePhase(Enum):
+    INITIAL = 0
+    METADATA = 1
+    DIRECTORY = 2
+    SYMLINK = 3
+    REGULAR = 4
+    PERSISTENT = 5
+    POST_PROCESSING = 6
