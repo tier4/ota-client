b8260adcc505a994b07885bcdb82f39bbbdba918
Bodong Yang <bodong.yang@tier4.jp>
2022-07-04 12:46:49 +0900

    add logging in boot_control.__init__ to indicates which boot controller is in use now

--- app/boot_control/__init__.py
+++ app/boot_control/__init__.py
@@ -1,9 +1,16 @@
 from typing import Type
 from app.boot_control.interface import BootControllerProtocol
-from app.configs import BOOT_LOADER
+
+from app.configs import BOOT_LOADER, config as cfg
+from app import log_util
+
+logger = log_util.get_logger(
+    __name__, cfg.LOG_LEVEL_TABLE.get(__name__, cfg.DEFAULT_LOG_LEVEL)
+)
 
 
 def get_boot_controller(bootloader: str = BOOT_LOADER) -> Type[BootControllerProtocol]:
+    logger.info(f"use boot_controller {bootloader=}")
     if bootloader == "grub":
         from app.boot_control.grub import GrubController
 
