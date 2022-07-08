d7751d99834feb9268d368a8261a3531b536b47d
Bodong Yang <bodong.yang@tier4.jp>
2022-07-08 12:12:40 +0900

    update main

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ import _pathloader
 assert _pathloader
 
 from app import log_util
+from app.common import read_from_file, write_to_file_sync
 from app.configs import config as cfg
 from app.ota_client_service import launch_otaclient_grpc_server
 
@@ -18,27 +19,26 @@ VERSION_FILE = Path(__file__).parent.parent / "version.txt"
 
 
 def _check_other_otaclient():
+    """Check if there is another otaclient instance running."""
     # create a lock file to prevent multiple ota-client instances start
     lock_file = Path("/var/run/ota-client.lock")
     our_pid = os.getpid()
-    if lock_file.is_file():
-        pid = lock_file.read_text()
+    if pid := read_from_file(lock_file):
         # running process will have a folder under /proc
         if Path(f"/proc/{pid}").is_dir():
             msg = f"another instance of ota-client(pid: {pid}) is running, abort"
             sys.exit(msg)
     # write our pid to the lock file
-    lock_file.write_text(f"{our_pid}")
+    write_to_file_sync(lock_file, f"{our_pid}")
 
 
 def main():
+    logger.info("started")
+
     version_file = VERSION_FILE
     if version_file.is_file():
         version = open(version_file).read()
         logger.info(version)
-
-    if cfg is None:
-        sys.exit("unsupported platform, abort")
 
     _check_other_otaclient()
 
