9daf2389a5d065000610d1541942dbab4210a29f
Bodong Yang <bodong.yang@tier4.jp>
2022-07-15 16:39:55 +0900

    create_standby: call os.sync after applying the slot update

--- app/create_standby/rebuild_mode.py
+++ app/create_standby/rebuild_mode.py
@@ -1,3 +1,4 @@
+import os
 import shutil
 import time
 from concurrent.futures import ThreadPoolExecutor
@@ -320,6 +321,9 @@ class RebuildMode(StandbySlotCreatorProtocol):
             self._process_symlinks()
             self._process_persistents()
             self._save_meta()
+
+            # do a os.sync after the standby slot creation finished
+            os.sync()
         except OTAError:
             raise  # if the error is already specified and wrapped, just raise again
         except DownloadFailedSpaceNotEnough:
