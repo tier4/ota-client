a408f90f5c040cc80672e27d665ea417289a9fa3
Bodong Yang <bodong.yang@tier4.jp>
2022-08-04 17:29:48 +0900

    tests: implement fixtures for running ota-image server and prepare ab slots

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,22 @@
+import logging
 import pytest
+import shutil
+from multiprocessing import Process
 from pathlib import Path
+from typing import Tuple
 
+from tests.utils import run_http_server
+
+logger = logging.getLogger(__name__)
 
 # not enable proxy when doing test
 DEFUALT_PROXY_INFO = """
 enable_local_ota_proxy: false
 """
+
+OTA_IMAGE_DIR = "/ota-image"
+OTA_SERVER_ADDR = "127.0.0.1"
+OTA_SERVER_PORT = 8080
 
 
 @pytest.fixture(scope="session")
@@ -16,3 +27,48 @@ def proxy_cfg():
     with tempfile.NamedTemporaryFile() as f:
         Path(f.name).write_text(DEFUALT_PROXY_INFO)
         return proxy_info.parse_proxy_info(proxy_info_file=f.name)
+
+
+@pytest.fixture(autouse=True, scope="session")
+def run_http_server_subprocess():
+    _server_p = Process(
+        target=run_http_server,
+        args=[OTA_SERVER_ADDR, OTA_SERVER_PORT],
+        kwargs={"directory": OTA_IMAGE_DIR},
+    )
+    logger.info(
+        f"start background ota-image server at http://{OTA_SERVER_ADDR}:{OTA_SERVER_PORT}"
+    )
+    try:
+        yield _server_p.start()
+    finally:
+        logger.info("shutdown background ota-image server")
+        _server_p.kill()
+
+
+@pytest.fixture(scope="session")
+def ab_slots(tmp_path_factory: Path) -> Tuple[str, str]:
+    """Prepare AB slots for the whole test session.
+
+    The slot_a will be the active slot, it will be populated
+    with the contents from /ota-image dir, with some of the dirs
+    renamed to simulate version update.
+
+    Structure:
+        tmp_path_factory:
+            slot_a/ (active, populated with ota-image)
+            slot_b/ (standby)
+
+    Return:
+        A tuple includes the path to A/B slots respectly.
+    """
+    # prepare slot_a
+    slot_a = tmp_path_factory / "slot_a"
+    shutil.copytree(OTA_IMAGE_DIR, slot_a, dirs_exist_ok=True, symlinks=True)
+    (slot_a / "var").rename("var_old")
+    (slot_a / "usr").rename("lib_old")
+    # prepare slot_b
+    slot_b = tmp_path_factory / "slot_b"
+    slot_b.mkdir()
+
+    return str(slot_a), str(slot_b)
--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,7 @@
 import asyncio
+import http.server as http_server
 from contextlib import asynccontextmanager
+from functools import partial
 
 import grpc
 from app.proto import otaclient_v2_pb2_grpc as v2_grpc
@@ -20,3 +22,9 @@ async def run_otaclient_server(otaclient_service_v2, listen_addr):
     finally:
         await server.stop(None)
         background_task.cancel()  # ensure the task termination
+
+
+def run_http_server(addr: str, port: int, *, directory: str):
+    handler_class = partial(http_server.SimpleHTTPRequestHandler, directory=directory)
+    with http_server.ThreadingHTTPServer((addr, port), handler_class) as httpd:
+        httpd.serve_forever()
