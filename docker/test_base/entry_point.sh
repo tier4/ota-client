92a372f125003e4da69f6d05ee5b45cf9bb576ca
Bodong Yang <bodong.yang@tier4.jp>
2022-08-17 11:36:02 +0900

    update how ota-test_base image is built * introduce virtualenv for preparing python dependencies * use entrypoint.sh to allow pre-configuring test certs,   setup/activate python virtualenv and install python dependencies * suppress log output when building image * refinment over Dockerfile

--- docker/test_base/entry_point.sh
+++ docker/test_base/entry_point.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 set -eu
 
+OTA_CLIENT_DIR="/ota-client"
+VENV="${OTA_CLIENT_DIR}/.venv"
+
 # setup certs
 echo "setup certificates for testing..."
-mkdir -p /ota-client/certs && 
-    cp -av /ota-client/tests/keys/root.pem /ota-client/certs/1.root.pem && 
-    cp -av /ota-client/tests/keys/interm.pem /ota-client/certs/1.interm.pem
+mkdir -p ${OTA_CLIENT_DIR}/certs && 
+    cp -av ${OTA_CLIENT_DIR}/tests/keys/root.pem ${OTA_CLIENT_DIR}/certs/1.root.pem && 
+    cp -av ${OTA_CLIENT_DIR}/tests/keys/interm.pem ${OTA_CLIENT_DIR}/certs/1.interm.pem
+
+# activate virtual env
+source ${VENV}/bin/activate
+
+# setup dependencies unconditionally
+# NOTE: if we only do minor modified to the requirements.txt,
+#       we don't have to rebuild the image, just call pip here
+APP_DEPENDENCIES="${OTA_CLIENT_DIR}/app/requirements.txt"
+[ -f "$APP_DEPENDENCIES" ] && echo "setup app dependencies..." && \
+    python3 -m pip install --no-cache-dir -q -r $APP_DEPENDENCIES
+TESTS_DEPENDENCIES="${OTA_CLIENT_DIR}/tests/requirements.txt"
+[ -f "$TESTS_DEPENDENCIES" ] && echo "setup tests dependencies..." && \
+    python3 -m pip install --no-cache-dir -q -r $TESTS_DEPENDENCIES
 
 # exec the input params
 echo "execute command..."
