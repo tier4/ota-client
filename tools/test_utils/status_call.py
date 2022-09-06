98667fdc95b18f5cbd28db3c9304aa1d13ac7a29
Bodong Yang <bodong.yang@tier4.jp>
2022-09-06 16:41:51 +0900

    update/fix api_caller

--- tools/test_utils/status_call.py
+++ tools/test_utils/status_call.py
@@ -1,45 +1,39 @@
 import time
 import asyncio
-import grpc
+from typing import Optional
 
-from app import otaclient_v2_pb2_grpc as v2_grpc
-from app.otaclient_v2_pb2 import StatusRequest
+from otaclient.app.ota_client_call import OtaClientCall
 
-import logutil
-import logging
+from . import logutil
 
-logger = logutil.get_logger(__name__, logging.DEBUG)
-
-
-async def _status(request, ip_addr, port="50051"):
-    target_addr = f"{ip_addr}:{port}"
-    async with grpc.aio.insecure_channel(target_addr) as channel:
-        stub = v2_grpc.OtaClientServiceStub(channel)
-        response = await stub.Status(request)
-        return response
+logger = logutil.get_logger(__name__)
 
 
 def call_status(
-    ecu_ip: str = "127.0.0.1",
-    ecu_port: str = "50051",
+    ecu_id: str,
+    ecu_ip: str,
+    ecu_port: int,
+    *,
     interval: float = 1,
-    count: int = 1024,
+    count: Optional[float] = None,
 ):
-    request = StatusRequest()
-    logger.debug(f"request status API on ecu at {ecu_ip}:{ecu_port}")
+    logger.debug(f"request status API on ecu(@{ecu_id}) at {ecu_ip}:{ecu_port}")
+    if count is None:
+        count = float("inf")
 
-    for i in range(count):
-        logger.debug(f"status request#{i}")
-        time.sleep(interval)
+    _count = 0
+    while _count < count:
+        logger.debug(f"status request#{_count}")
         try:
-            response = asyncio.run(
-                _status(
-                    request=request,
-                    ip_addr=ecu_ip,
-                    port=ecu_port,
+            if response := asyncio.run(
+                OtaClientCall.status_call(
+                    ecu_id,
+                    ecu_ip,
+                    ecu_port,
                 )
-            )
+            ):
+                logger.debug(f"{response.data=}")
         except Exception as e:
             logger.debug(f"API request failed: {e!r}")
             continue
-        logger.debug(f"{response=}")
+        time.sleep(interval)
--- tools/test_utils/update_call.py
+++ tools/test_utils/update_call.py
@@ -1,51 +1,44 @@
 import asyncio
-import json
-import grpc
+import yaml
 
-from app import otaclient_v2_pb2_grpc as v2_grpc
-from app.otaclient_v2_pb2 import UpdateRequest, UpdateRequestEcu
+from otaclient.app.ota_client_call import OtaClientCall
+from otaclient.app.proto import wrapper
 
-import logutil
-import logging
+from . import logutil
 
-logger = logutil.get_logger(__name__, logging.DEBUG)
-
-_default_request = UpdateRequest(
-    ecu=[
-        UpdateRequestEcu(
-            ecu_id="autoware",
-            version="123.x",
-            url="http://10.6.65.3:8080",
-            cookies=json.dumps({"test": "my-cookie"}),
-        )
-    ],
-)
+logger = logutil.get_logger(__name__)
 
 
-async def _update(request, ip_addr, port="50051"):
-    target_addr = f"{ip_addr}:{port}"
-    async with grpc.aio.insecure_channel(target_addr) as channel:
-        stub = v2_grpc.OtaClientServiceStub(channel)
-        response = await stub.Update(request)
-    return response
+def load_external_update_request(request_yaml_file: str) -> wrapper.UpdateRequest:
+    with open(request_yaml_file, "r") as f:
+        request_yaml: dict = yaml.safe_load(f)
+        logger.debug(f"load external request: {request_yaml!r}")
+
+        request = wrapper.UpdateRequest()
+        for request_ecu in request_yaml:
+            request.ecu.append(wrapper.UpdateRequestEcu(**request_ecu))
+    return request
 
 
 def call_update(
-    ecu_ip: str = "127.0.0.1",
-    ecu_port: str = "50051",
-    request: UpdateRequest = _default_request,
+    ecu_id: str,
+    ecu_ip: str,
+    ecu_port: int,
+    *,
+    request_file: str,
 ):
-    logger.debug(f"request update on ecu at {ecu_ip}:{ecu_port}")
-    logger.debug(f"{request=}")
+    logger.debug(f"request update on ecu(@{ecu_id}) at {ecu_ip}:{ecu_port}")
+    update_request = load_external_update_request(request_file)
 
     try:
         result = asyncio.run(
-            _update(
-                request=request,
-                ip_addr=ecu_ip,
-                port=ecu_port,
+            OtaClientCall.update_call(
+                ecu_id,
+                ecu_ip,
+                ecu_port,
+                request=update_request,
             )
         )
-        logger.debug(f"{result=}")
+        logger.debug(f"{result.data=}")
     except Exception as e:
         logger.debug(f"error occured: {e!r}")
