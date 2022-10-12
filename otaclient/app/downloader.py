56d70b2473ebdafa4ed14a0a92cec5a87c07bf37
Bodong Yang <bodong.yang@tier4.jp>
2022-10-13 02:11:56 +0900

    re-implement downloader module * now Downloader is backed by a threadpool, each work thread in   the pool will have its own session/zstd_dctx, all requests will   be dispatched to the threadpool, * re-implement retrying on/handling errors flows, * download method now directly take a URL, its the caller's duty   to calculate URL from RegularInf.path, * introduce zstd decompression support on downloaded files.

--- otaclient/app/downloader.py
+++ otaclient/app/downloader.py
@@ -12,20 +12,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-import requests
-import time
 import errno
+import os
+import requests
+import threading
+import time
+import zstandard
+from concurrent.futures import ThreadPoolExecutor
 from functools import partial
 from hashlib import sha256
 from pathlib import Path
 from typing import Any, Callable, Dict, Optional, Union
-from urllib.parse import quote_from_bytes, urljoin, urlparse
+from requests.adapters import HTTPAdapter
 from requests.exceptions import (
-    ConnectionError,
+    HTTPError,
+    RequestException,
     ChunkedEncodingError,
-    StreamConsumedError,
+    RetryError,
 )
+from urllib3.util.retry import Retry
 
 from .configs import config as cfg
 from .common import OTAFileCacheControl
@@ -47,6 +52,15 @@ class DownloadError(Exception):
         return f"{_inject}{super().__str__()}"
 
     __repr__ = __str__
+
+
+class UnhandledHTTPError(DownloadError):
+    """HTTPErrors that cannot be handled by us.
+
+    Currently include 403 and 404.
+    """
+
+    pass
 
 
 class DestinationNotAvailableError(DownloadError):
@@ -78,9 +92,9 @@ REQUEST_RECACHE_HEADER: Dict[str, str] = {
 
 def _retry(retry: int, backoff_factor: float, backoff_max: int, func: Callable):
     """
-    NOTE: this retry decorator expects the input func to
-    have 'headers' kwarg.
-    NOTE 2: only retry on ChunkStreamingError and HashVerificationError
+    NOTE: this retry decorator expects the input func to have 'headers' kwarg.
+    NOTE: only retry on errors that doesn't handled by the urllib3.Retry module,
+          which are ChunkStreamingError and HashVerificationError.
     """
     from functools import wraps
 
@@ -90,6 +104,10 @@ def _retry(retry: int, backoff_factor: float, backoff_max: int, func: Callable):
         while True:
             try:
                 return func(*args, **kwargs)
+            except (ExceedMaxRetryError, UnhandledHTTPError):
+                # if urllib3.Retry has already handled the retry,
+                # or we hit an UnhandledHTTPError, just re-raise
+                raise
             except (HashVerificaitonError, ChunkStreamingError):
                 _retry_count += 1
                 _backoff = min(backoff_max, backoff_factor * (2 ** (_retry_count - 1)))
@@ -110,34 +128,35 @@ def _retry(retry: int, backoff_factor: float, backoff_max: int, func: Callable):
 
 
 class Downloader:
+    EMPTY_STR_SHA256 = (
+        r"e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    )
+    MAX_DOWNLOAD_THREADS = cfg.MAX_DOWNLOAD_THREAD
+    MAX_CONCURRENT_DOWNLOAD = cfg.MAX_CONCURRENT_DOWNLOAD_PER_THREAD
     CHUNK_SIZE = cfg.CHUNK_SIZE
     RETRY_COUNT = cfg.DOWNLOAD_RETRY
     BACKOFF_FACTOR = 1
     OUTER_BACKOFF_FACTOR = 0.01
     BACKOFF_MAX = cfg.DOWNLOAD_BACKOFF_MAX
-    MAX_CONCURRENT_DOWNLOAD = cfg.MAX_CONCURRENT_DOWNLOAD
-    EMPTY_STR_SHA256 = (
-        r"e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-    )
+    RETRY_ON_STATUS_CODE = {413, 429, 500, 502, 503, 504}
 
-    def __init__(self):
-        from requests.adapters import HTTPAdapter
-        from urllib3.util.retry import Retry
+    def _thread_initializer(self):
+        self._local = threading.local()
 
-        # base session
+        ### setup the requests.Session ###
         session = requests.Session()
-
         # init retry mechanism
-        # NOTE: for urllib3 version below 2.0, we have to change Retry class' DEFAULT_BACKOFF_MAX,
-        # to configure the backoff max, set the value to the instance will not work as increment() method
-        # will create a new instance of Retry on every try without inherit the change to instance's DEFAULT_BACKOFF_MAX
+        # NOTE: for urllib3 version below 2.0, we have to change Retry class'
+        # DEFAULT_BACKOFF_MAX, to configure the backoff max, set the value to
+        # the instance will not work as increment() method will create a new
+        # instance of Retry on every try without inherit the change to
+        # instance's DEFAULT_BACKOFF_MAX
         Retry.DEFAULT_BACKOFF_MAX = self.BACKOFF_MAX
         retry_strategy = Retry(
             total=self.RETRY_COUNT,
-            raise_on_status=True,
             backoff_factor=self.BACKOFF_FACTOR,
-            # retry on common server side errors and non-critical client side errors
-            status_forcelist={413, 429, 500, 502, 503, 504},
+            # retry on common serverside errors and clientside errors
+            status_forcelist=self.RETRY_ON_STATUS_CODE,
             allowed_methods=["GET"],
         )
         adapter = HTTPAdapter(
@@ -147,115 +166,110 @@ class Downloader:
         )
         session.mount("https://", adapter)
         session.mount("http://", adapter)
+        self._local.session = session
 
-        # cleanup proxy if any
-        proxies = {"http": "", "https": ""}
-        session.proxies.update(proxies)
+        ### setup zstd decompressor ###
+        self._local._zstd_dctx = zstandard.ZstdDecompressor()
 
-        # register the connection pool
-        self.session = session
-        self._proxy_set = False
+    @property
+    def _session(self) -> requests.Session:
+        """A thread-local private session."""
+        return self._local.session
+
+    @property
+    def _zstd_dctx(self) -> zstandard.ZstdDecompressor:
+        """A thread-local private zstd decompressor."""
+        return self._local._zstd_dctx
+
+    def __init__(self) -> None:
+        self._executor = ThreadPoolExecutor(
+            max_workers=min(self.MAX_DOWNLOAD_THREADS, (os.cpu_count() or 1) + 4),
+            thread_name_prefix="downloader",
+            initializer=self._thread_initializer,
+        )
+        self._hash_func = sha256
 
     def shutdown(self):
-        self.session.close()
-
-    def configure_proxy(self, proxy: str):
-        # configure proxy
-        self._proxy_set = True
-        proxies = {"http": proxy, "https": ""}
-        self.session.proxies.update(proxies)
-
-    def cleanup_proxy(self):
-        self._proxy_set = False
-        self.configure_proxy("")
-
-    def _path_to_url(self, base: str, path: Union[Path, str]) -> str:
-        # regulate base url, add suffix / to it if not existed
-        base = f"{base}/" if not base.endswith("/") else base
-        # convert to Path if path is str
-        path = Path(path) if isinstance(path, str) else path
-
-        relative_path = path
-        # if the path is relative to /
-        try:
-            relative_path = path.relative_to("/")
-        except ValueError:
-            pass
-
-        quoted_path = quote_from_bytes(bytes(relative_path))
-
-        # switch scheme if needed
-        _url_parsed = urlparse(urljoin(base, quoted_path))
-        # unconditionally set scheme to HTTP if proxy is applied
-        if self._proxy_set:
-            _url_parsed = _url_parsed._replace(scheme="http")
-
-        return _url_parsed.geturl()
+        self._executor.shutdown()
 
     @partial(_retry, RETRY_COUNT, OUTER_BACKOFF_FACTOR, BACKOFF_MAX)
-    def download(
+    def _download_task(
         self,
-        path: Union[Path, str],
-        dst: Union[Path, str],
-        digest: Optional[str] = None,
-        size: Optional[int] = None,
+        url: str,
+        dst: Union[str, Path],
         *,
-        url_base: str,
+        size: Optional[int] = None,
+        digest: Optional[str] = None,
+        proxies: Optional[Dict[str, str]] = None,
         cookies: Optional[Dict[str, str]] = None,
         headers: Optional[Dict[str, str]] = None,
+        zstd_decompressed=False,
     ) -> int:
-        url = self._path_to_url(url_base, path)
-        cookies = cookies if cookies else {}
-        headers = headers if headers else {}
-        dst = Path(dst)
-
-        # specially deal with empty file
-        if digest == self.EMPTY_STR_SHA256:
-            dst.write_bytes(b"")
+        # special treatment for empty file
+        if dst == self.EMPTY_STR_SHA256 and not (dst_p := Path(dst)).is_file():
+            dst_p.write_bytes(b"")
             return 0
 
+        _hash_inst, _downloaded_bytes = self._hash_func(), 0
+        _err_count = 0
         try:
-            error_count = 0
-            response = self.session.get(
-                url, stream=True, cookies=cookies, headers=headers
-            )
-
-            raw_r = response.raw
-            if raw_r.retries:
-                error_count = len(raw_r.retries.history)
-        except Exception as e:
-            raise ExceedMaxRetryError(url, dst) from e
-
-        hash_f = sha256()
-        _downloaded_size = 0
-        try:
-            with open(dst, "wb") as f:
-                for chunk in response.iter_content(chunk_size=self.CHUNK_SIZE):
-                    hash_f.update(chunk)
-                    f.write(chunk)
-                    _downloaded_size += len(chunk)
-        except (ChunkedEncodingError, ConnectionError, StreamConsumedError) as e:
+            with self._session.get(
+                url,
+                stream=True,
+                proxies=proxies,
+                cookies=cookies,
+                headers=headers,
+            ) as resp, open(dst, "wb") as f:
+                resp.raise_for_status()
+                raw_resp = resp.raw
+                if raw_resp.retries:
+                    _err_count = len(raw_resp.retries.history)
+                if zstd_decompressed:
+                    raw_resp = self._zstd_dctx.stream_reader(raw_resp)
+                while data := raw_resp.read(self.CHUNK_SIZE):
+                    _hash_inst.update(data)
+                    f.write(data)
+                    _downloaded_bytes += len(data)
+        except RetryError as e:
+            raise ExceedMaxRetryError(url, dst, f"{e!r}")
+        except ChunkedEncodingError as e:
+            # streaming interrupted
             raise ChunkStreamingError(url, dst) from e
+        except HTTPError as e:
+            # HTTPErrors that cannot be handled by retry,
+            # include 403 and 404
+            raise UnhandledHTTPError(url, dst, e.strerror)
+        except RequestException as e:
+            # any errors that happens during handling request
+            # and are not the above exceptions
+            raise ExceedMaxRetryError(url, dst) from e
         except FileNotFoundError as e:
+            # dst is not available
             raise DestinationNotAvailableError(url, dst) from e
         except OSError as e:
-            # space not enough error
+            # only handle disk out-of-space error
             if e.errno == errno.ENOSPC:
                 raise DownloadFailedSpaceNotEnough(url, dst) from None
 
-        # throw a network error if the file is not fully downloaded
-        if size is not None and size != _downloaded_size:
-            raise ChunkStreamingError(url, dst) from ValueError(
-                "partial download detected"
-            )
-
-        calc_digest = hash_f.hexdigest()
-        if digest and calc_digest != digest:
+        # checking the download result
+        if size is not None and size != _downloaded_bytes:
+            msg = f"partial download detected: {size=},{_downloaded_bytes=}"
+            logger.error(msg)
+            raise ChunkStreamingError(url, dst, msg)
+        if digest and ((calc_digest := _hash_inst.hexdigest()) != digest):
             msg = (
-                f"hash check failed detected: "
+                "sha256hash check failed detected: "
                 f"act={calc_digest}, exp={digest}, {url=}"
             )
             logger.error(msg)
             raise HashVerificaitonError(url, dst, msg)
 
-        return error_count
+        return _err_count
+
+    def download(self, *args, **kwargs):
+        """Dispatcher for download tasks."""
+        return self._executor.submit(
+            self._download_task,
+            *args,
+            **kwargs,
+        ).result()
