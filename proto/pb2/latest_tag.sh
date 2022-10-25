19e5a3e6718e32649ea5a6878cb153073fa9d0a1
hiroyuki.obinata <hiroyuki.obinata@tier4.jp>
2022-10-25 13:56:47 +0900

    add conditional proto whl build

--- proto/pb2/latest_tag.sh
+++ proto/pb2/latest_tag.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 current_branch=$(git rev-parse --abbrev-ref HEAD)
-latest_tag=$(git tag -l --sort=-creatordate --merged ${current_branch} | head -n 1)
+if [ "$1" = "second_latest" ]; then
+  latest_tag=$(git tag -l --sort=-creatordate --merged ${current_branch} | head -n 2 | tail -n 1)
+else
+  latest_tag=$(git tag -l --sort=-creatordate --merged ${current_branch} | head -n 1)
+fi
 echo ${latest_tag}
