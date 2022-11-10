0b2fbb5587c4602bd3c96365703c59bc0dad97e8
Bodong Yang <bodong.yang@tier4.jp>
2022-11-10 17:01:21 +0900

    refactor ECUInfo with dataclass

--- otaclient/app/ecu_info.py
+++ otaclient/app/ecu_info.py
@@ -11,25 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-r"""ECU metadatas definition.
-
-Version 1 scheme example:
-format_vesrion: 1
-ecu_id: "autoware"
-ip_addr: "0.0.0.0"
-# available options: grub, cboot, rpi_boot
-bootloader: "grub"
-secondaries:
-    - ecu_id: "p1"
-      ip_addr: "0.0.0.0"
-available_ecu_ids:
-    - "autoware"
-    - "p1"
-"""
+r"""ECU metadatas definition."""
 
 import yaml
+from dataclasses import dataclass, field, fields, MISSING
 from pathlib import Path
-from typing import Iterator, Union, Dict, List, Tuple
+from typing import Iterator, Union, Dict, List, Tuple, Any
 
 from . import log_util
 from .configs import config as cfg
@@ -46,26 +33,66 @@ DEFAULT_ECU_INFO = {
 }
 
 
+@dataclass
 class ECUInfo:
-    def __init__(self, ecu_info_file: Union[str, Path]):
-        ecu_info = DEFAULT_ECU_INFO
+    """
+    Version 1 scheme example:
+        format_vesrion: 1
+        ecu_id: "autoware"
+        ip_addr: "0.0.0.0"
+        bootloader: "grub"
+        secondaries:
+            - ecu_id: "p1"
+              ip_addr: "0.0.0.0"
+        available_ecu_ids:
+            - "autoware"
+            - "p1
+    """
+
+    ecu_id: str
+    ip_addr: str = "127.0.0.1"
+    bootloader: str = BootloaderType.UNSPECIFIED.value
+    available_ecu_id: List[str] = field(default_factory=list)
+    secondaries: List[Dict[str, str]] = field(default_factory=list)
+    format_version: int = 1
+
+    @classmethod
+    def parse_ecu_info(cls, ecu_info_file: Union[str, Path]) -> "ECUInfo":
+        ecu_info = DEFAULT_ECU_INFO.copy()
         try:
-            with open(ecu_info_file) as f:
-                ecu_info = yaml.safe_load(f)
+            ecu_info = yaml.safe_load(Path(ecu_info_file).read_text())
+            assert isinstance(ecu_info, Dict)
         except Exception:
-            pass
+            logger.warning(
+                f"failed to load {ecu_info_file=} or config file corrupted, use default config"
+            )
         logger.info(f"ecu_info={ecu_info}")
-        # required field
-        self.ecu_id = ecu_info["ecu_id"]
-        # optional fields
-        self.ip_addr = ecu_info.get("ip_addr", "127.0.0.1")
-        self.bootloader = BootloaderType.parse_str(
-            ecu_info.get("bootloader", "unspecified")
-        )
-        self.secondaries: List[Dict[str, str]] = ecu_info.get("secondaries", [])
-        self.available_ecu_id: List[str] = ecu_info.get(
-            "available_ecu_ids", [self.ecu_id]
-        )
+
+        # load options
+        # NOTE: if option is not presented,
+        #       this option will be set to the default value
+        _ecu_info_dict: Dict[str, Any] = dict()
+        for _field in fields(cls):
+            _option = ecu_info.get(_field.name)
+            if not isinstance(_option, _field.type):
+                if _option is not None:
+                    logger.warning(
+                        f"{_field.name} contains invalid value={_option}, "
+                        "ignored and set to default={_field.default}"
+                    )
+                if _field.default is MISSING:
+                    raise ValueError(
+                        f"required field {_field.name} is not presented, abort"
+                    )
+                _ecu_info_dict[_field.name] = _field.default
+                continue
+            if isinstance(_option, (list, dict)):
+                _ecu_info_dict[_field.name] = _option.copy()
+            else:
+                _ecu_info_dict[_field.name] = _option
+
+        # initialize ECUInfo inst
+        return cls(**_ecu_info_dict)
 
     def iter_secondary_ecus(self) -> Iterator[Tuple[str, str]]:
         """
@@ -79,6 +106,9 @@ class ECUInfo:
 
     def get_ecu_ip_addr(self) -> str:
         return self.ip_addr
+
+    def get_bootloader(self) -> BootloaderType:
+        return BootloaderType.parse_str(self.bootloader)
 
     def get_available_ecu_ids(self) -> List[str]:
         return self.available_ecu_id.copy()
