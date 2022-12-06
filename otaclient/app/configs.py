46d34a29be38259177f7c30e9c188633158755d4
Bodong Yang <bodong.yang@tier4.jp>
2022-12-06 12:57:53 +0900

    reduce MAX_DOWNLOAD_THREAD to 3

--- otaclient/app/configs.py
+++ otaclient/app/configs.py
@@ -94,7 +94,7 @@ class BaseConfig:
     DOWNLOAD_RETRY: int = 10
     DOWNLOAD_BACKOFF_MAX: int = 3  # seconds
     MAX_CONCURRENT_TASKS: int = 128
-    MAX_DOWNLOAD_THREAD: int = 8
+    MAX_DOWNLOAD_THREAD = 3
     DOWNLOADER_CONNPOOL_SIZE_PER_THREAD: int = 8
     STATS_COLLECT_INTERVAL: int = 1  # second
     ## standby creation mode, default to rebuild now
