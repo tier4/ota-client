2d5ce8cc78d3134b048b7868d5c9aa3ac485871a
Bodong Yang <bodong.yang@tier4.jp>
2022-12-08 18:34:21 +0900

    boot_control.common: mkfs_ext4 now also take fslabel param

--- otaclient/app/boot_control/_common.py
+++ otaclient/app/boot_control/_common.py
@@ -286,12 +286,16 @@ class CMDHelperFuncs:
                 raise BootControlError(_failure_msg) from None
 
     @classmethod
-    def mkfs_ext4(cls, dev: str):
+    def mkfs_ext4(cls, dev: str, *, fslabel: Optional[str] = None):
         _specify_uuid, _specify_fslabel = "", ""
         try:
             # NOTE: preserve the UUID and FSLABEL(if set)
             _specify_uuid = f"-U {cls.get_uuid_by_dev(dev)}"
-            if _fslabel := cls.get_fslabel_by_dev(dev):
+            # if fslabel is specified, then use it,
+            # otherwise try to detect the previously set one
+            if fslabel:
+                _specify_fslabel = f"-L {fslabel}"
+            elif _fslabel := cls.get_fslabel_by_dev(dev):
                 _specify_fslabel = f"-L {_fslabel}"
         except ValueError:
             pass
