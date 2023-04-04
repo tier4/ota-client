e56097732009806951e0e1ac0bb5d5d6f5bcb358
bodong.yang <bodong.yang@tier4.jp>
2023-04-04 02:57:17 +0000

    merge main.py into __main__.py

--- otaclient/app/__main__.py
+++ otaclient/app/__main__.py
@@ -13,7 +13,53 @@
 # limitations under the License.
 
 
-from .main import main
+import asyncio
+import logging
+import os
+import sys
+from pathlib import Path
+
+from otaclient import __version__  # type: ignore
+from .proto import wrapper, v2, v2_grpc, ota_metafiles  # noqa: F401
+from .common import read_str_from_file, write_str_to_file_sync
+from .configs import config as cfg, EXTRA_VERSION_FILE
+from .log_setting import configure_logging, get_ecu_id
+from .ota_client_service import run_otaclient_grpc_server
+
+# configure logging before any code being executed
+configure_logging(loglevel=cfg.DEFAULT_LOG_LEVEL, http_logging_url=get_ecu_id())
+logger = logging.getLogger(__name__)
+
+
+def _check_other_otaclient():
+    """Check if there is another otaclient instance running."""
+    # create a lock file to prevent multiple ota-client instances start
+    if pid := read_str_from_file(cfg.OTACLIENT_PID_FILE):
+        # running process will have a folder under /proc
+        if Path(f"/proc/{pid}").is_dir():
+            logger.error(f"another instance of ota-client({pid=}) is running, abort")
+            sys.exit()
+        else:
+            logger.warning(f"dangling otaclient lock file({pid=}) detected, cleanup")
+            Path(cfg.OTACLIENT_PID_FILE).unlink(missing_ok=True)
+    # create run dir
+    _run_dir = Path(cfg.RUN_DIR)
+    _run_dir.mkdir(parents=True, exist_ok=True)
+    os.chmod(_run_dir, 0o550)
+    # write our pid to the lock file
+    write_str_to_file_sync(cfg.OTACLIENT_PID_FILE, f"{os.getpid()}")
+
+
+def main():
+    logger.info("started")
+    if Path(EXTRA_VERSION_FILE).is_file():
+        logger.info(read_str_from_file(EXTRA_VERSION_FILE))
+    logger.info(f"otaclient version: {__version__}")
+
+    # start the otaclient grpc server
+    _check_other_otaclient()
+    asyncio.run(run_otaclient_grpc_server())
+
 
 if __name__ == "__main__":
     main()
