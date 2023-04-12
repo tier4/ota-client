tier4/ota-client

commit 69d880fce170187f8cf0c59da4ab79635e00652c

otaproxy.retrieve_file: directly raise connection err when open_remote

otaclient/ota_proxy/ota_cache.py
@@ -1049,20 +1049,14 @@ class OTACache:
             raw_url, executor=self._executor
         )
         if is_writer:
-            try:
-                _remote_fd, _meta = await _FileDescriptorHelper.open_remote(
-                    url=self._process_raw_url(raw_url),
-                    raw_url=raw_url,
-                    cookies=cookies,
-                    headers=extra_headers,
-                    session=self._session,
-                    upper_proxy=self._upper_proxy,
-                )
-            except Exception as e:
-                _err_msg = f"failed to open remote connection for {raw_url=}: {e!r}"
-                logger.debug(_err_msg)
-                await _tracker.provider_on_failed()
-                raise CacheStreamingFailed(_err_msg) from e
+            _remote_fd, _meta = await _FileDescriptorHelper.open_remote(
+                url=self._process_raw_url(raw_url),
+                raw_url=raw_url,
+                cookies=cookies,
+                headers=extra_headers,
+                session=self._session,
+                upper_proxy=self._upper_proxy,
+            )
 
             return await RemoteOTAFile(
                 fd=_remote_fd,

commit 988639888fe5870e7138419d2ed0c6192ad84b9d

otaproxy.ota_cache: fix broken retrieve_file method caused by 69d880fce17

otaclient/ota_proxy/ota_cache.py
@@ -1049,14 +1049,18 @@ class OTACache:
             raw_url, executor=self._executor
         )
         if is_writer:
-            _remote_fd, _meta = await _FileDescriptorHelper.open_remote(
-                url=self._process_raw_url(raw_url),
-                raw_url=raw_url,
-                cookies=cookies,
-                headers=extra_headers,
-                session=self._session,
-                upper_proxy=self._upper_proxy,
-            )
+            try:
+                _remote_fd, _meta = await _FileDescriptorHelper.open_remote(
+                    url=self._process_raw_url(raw_url),
+                    raw_url=raw_url,
+                    cookies=cookies,
+                    headers=extra_headers,
+                    session=self._session,
+                    upper_proxy=self._upper_proxy,
+                )
+            except Exception:
+                await _tracker.provider_on_failed()
+                raise
 
             return await RemoteOTAFile(
                 fd=_remote_fd,