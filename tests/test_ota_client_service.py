c71baa43c1279ae14561f4b053d0383a43840d73
Bodong Yang <86948717+Bodong-Yang@users.noreply.github.com>
2023-05-11 16:32:46 +0900

    Update copyright message in test_ota_client_service.py

--- tests/test_ota_client_service.py
+++ tests/test_ota_client_service.py
@@ -1,4 +1,4 @@
-# Copyright 2022 TIER IV, INC. All rights reserved.
+# Copyright 2023 TIER IV, INC. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
