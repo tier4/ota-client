cd6656c0693d8059faf6a5ba11a03fca4dc22bd6
Bodong Yang <86948717+Bodong-Yang@users.noreply.github.com>
2023-06-12 10:35:43 +0900

    [Refinement] refine downloader and align its behavior as doc (#228)
    This PR aligns the downloader's behavior to the implementation document, and introduce many internal refinements for improved implementation/code readability, better error handlings, minor mechanism/workflow refinement, etc.
    
    Internal behavior changes:
    1. empty file special treatment is now fully handled in otaclient module, downloader no-longer handles this,
    2. use_http_if_http_proxy_is_set flag is now Downloader instance scope, only configured once via configure_proxies,
    3. downloading exception catching group is refined according to document, check implementation document for more details,
    4. no extra mechanism is implemented for active detection, now downloader active tracking is done by checking if downloaded_bytes increased or not,
    5. outer retrier now only retries on invalid data transfer(HashVerificaitonError, ChunkStreamingError) and unhandled exceptions from requests (UnhandledRequestException, as last resort).

--- otaclient/_utils/__init__.py
+++ otaclient/_utils/__init__.py
@@ -11,3 +11,22 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from typing import Any, Callable
+from typing_extensions import ParamSpec
+
+P = ParamSpec("P")
+
+
+def copy_callable_typehint(_source: Callable[P, Any]):
+    """This helper function return a decorator that can type hint the target
+    function as the _source function.
+
+    At runtime, this decorator actually does nothing, but just return the input function as it.
+    But the returned function will have the same type hint as the source function in ide.
+    It will not impact the runtime behavior of the decorated function.
+    """
+
+    def _decorator(target) -> Callable[P, Any]:
+        return target
+
+    return _decorator
