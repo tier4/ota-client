9e15647f95474e7761f2863a84d4c565885f4c17
bodong.yang <bodong.yang@tier4.jp>
2023-07-12 01:16:14 +0000

    otaproxy: define _consts.py

---
+++ otaclient/ota_proxy/_consts.py
@@ -0,0 +1,18 @@
+from multidict import istr
+from .cache_control import OTAFileCacheControl
+
+# uvicorn
+REQ_TYPE_LIFESPAN = "lifespan"
+REQ_TYPE_HTTP = "http"
+RESP_TYPE_BODY = "http.response.body"
+RESP_TYPE_START = "http.response.start"
+
+METHOD_GET = "GET"
+
+# headers
+# for implementation convienience, we use lowercase for all headers.
+HEADER_OTA_FILE_CACHE_CONTROL = istr(OTAFileCacheControl.HEADER_LOWERCASE)
+HEADER_AUTHORIZATION = istr("authorization")
+HEADER_COOKIE = istr("cookie")
+HEADER_CONTENT_TYPE = istr("content-type")
+HEADER_CONTENT_ENCODING = istr("content-encoding")
