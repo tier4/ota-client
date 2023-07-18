fa55a26f73ee57e5a1a3677a7733ec3ae4d8ee92
bodong.yang <bodong.yang@tier4.jp>
2023-07-18 10:15:07 +0000

    otaproxy: update and refine server_app

--- otaclient/ota_proxy/server_app.py
+++ otaclient/ota_proxy/server_app.py
@@ -15,11 +15,28 @@
 
 import asyncio
 import aiohttp
+from contextlib import asynccontextmanager
 from http import HTTPStatus
-from typing import Any, Dict, List, Union
+from typing import Dict, List, Mapping, Tuple, Union
+from urllib.parse import urlparse
 
 from otaclient._utils.logging import BurstSuppressFilter
 from .cache_control import OTAFileCacheControl
+from ._consts import (
+    METHOD_GET,
+    REQ_TYPE_HTTP,
+    REQ_TYPE_LIFESPAN,
+    RESP_TYPE_BODY,
+    RESP_TYPE_START,
+    BHEADER_AUTHORIZATION,
+    BHEADER_COOKIE,
+    BHEADER_OTA_FILE_CACHE_CONTROL,
+    BHEADER_CONTENT_ENCODING,
+    HEADER_AUTHORIZATION,
+    HEADER_COOKIE,
+    HEADER_OTA_FILE_CACHE_CONTROL,
+    HEADER_CONTENT_ENCODING,
+)
 from .errors import BaseOTACacheError
 from .ota_cache import OTACache
 
@@ -41,6 +58,57 @@ connection_err_logger.addFilter(
 __all__ = ("App",)
 
 
+# helper methods
+
+
+def parse_raw_headers(raw_headers: List[Tuple[bytes, bytes]]) -> Dict[str, str]:
+    """Picks and decode raw headers from client's request.
+
+    Uvicorn sends headers from client's request to application as list of bytes tuple.
+    Currently we only need authorization, cookie and ota-file-cache-control header.
+
+    Returns:
+        An inst of header dict.
+    """
+    headers = {}
+    for bheader_pair in raw_headers:
+        if len(bheader_pair) != 2:
+            continue
+        bname, bvalue = bheader_pair
+        if bname == BHEADER_AUTHORIZATION:
+            headers[HEADER_AUTHORIZATION] = bvalue.strip().decode(
+                "utf-8", "surrogateescape"
+            )
+        elif bname == BHEADER_COOKIE:
+            headers[HEADER_COOKIE] = bvalue.strip().decode("utf-8", "surrogateescape")
+        elif bname == BHEADER_OTA_FILE_CACHE_CONTROL:
+            headers[HEADER_OTA_FILE_CACHE_CONTROL] = bvalue.decode(
+                "utf-8", "surrogateescape"
+            )
+    return headers
+
+
+def encode_headers(headers: Mapping[str, str]) -> List[Tuple[bytes, bytes]]:
+    """Encode headers dict to list of bytes tuples for sending back to client.
+
+    Uvicorn requests application to pre-process headers to bytes.
+    Currently we only need to send content-encoding and ota-file-cache-control header
+        back to client.
+    """
+    bytes_headers: List[Tuple[bytes, bytes]] = []
+    for name, value in headers.items():
+        if name == HEADER_CONTENT_ENCODING and value:
+            bytes_headers.append((BHEADER_CONTENT_ENCODING, value.encode("utf-8")))
+        elif name == HEADER_OTA_FILE_CACHE_CONTROL and value:
+            bytes_headers.append(
+                (BHEADER_OTA_FILE_CACHE_CONTROL, value.encode("utf-8"))
+            )
+    return bytes_headers
+
+
+# uvicorn APP
+
+
 class App:
     """The ASGI application for ota_proxy server passed to unvicorn.
 
@@ -54,16 +122,18 @@ class App:
         b. It seems that uvicorn will not interrupt the App running even the client closes connection.
 
     Attributes:
-        ota_cache: initialized but not yet launched ota_cache instance
+        ota_cache: initialized but not yet launched ota_cache instance.
 
     Example usage:
 
         # initialize an instance of the App:
-        _ota_cache = OTACache(cache_enabled=True, init_cache=False, enable_https=False)
+
+        _ota_cache = OTACache(...)
         app = App(_ota_cache)
 
         # load the app with uvicorn, and start uvicorn
         # NOTE: lifespan must be set to "on" for properly launching/closing ota_cache instance
+
         uvicorn.run(app, host="0.0.0.0", port=8082, log_level="debug", lifespan="on")
     """
 
@@ -87,69 +157,112 @@ class App:
                 await self._ota_cache.close()
 
     @staticmethod
-    def parse_raw_cookies(cookies_bytes: bytes) -> Dict[str, str]:
-        """Parses raw cookies bytes into dict.
-
-        Converts an input raw cookies string in bytes into a dict.
-
-        Args:
-            cookies_bytes bytes: raw cookies string in bytes
-                i.e.: b"cpk0=cpv0;cpk1=cpv1;cpk2=cpv2"
-
-        Returns:
-            Dict[str, str]: dict represented cookies
-        """
-        cookie_pairs: List[str] = cookies_bytes.decode().split(";")
-        res = dict()
-        for p in cookie_pairs:
-            k, v = p.strip().split("=")
-            res[k] = v
-
-        return res
-
-    async def _respond_with_error(self, status: Union[HTTPStatus, int], msg: str, send):
-        """Helper method for sending errors back to client"""
+    async def _respond_with_error(status: Union[HTTPStatus, int], msg: str, send):
+        """Helper method for sending errors back to client."""
         await send(
             {
-                "type": "http.response.start",
+                "type": RESP_TYPE_START,
                 "status": status,
                 "headers": [
                     [b"content-type", b"text/html;charset=UTF-8"],
                 ],
             }
         )
-        await send({"type": "http.response.body", "body": msg.encode("utf8")})
+        await send({"type": RESP_TYPE_BODY, "body": msg.encode("utf8")})
 
-    async def _send_chunk(self, data: bytes, more: bool, send):
-        """Helper method for sending data chunks to client
+    @staticmethod
+    async def _send_chunk(data: bytes, more: bool, send):
+        """Helper method for sending data chunks to client.
 
         Args:
-            data bytes
-            more bool: whether there will be a next chunk or not
-            send: ASGI send method
+            data: bytes to send to client.
+            more bool: whether there will be a next chunk or not.
+            send: ASGI send method.
         """
         if more:
-            await send({"type": "http.response.body", "body": data, "more_body": True})
+            await send({"type": RESP_TYPE_BODY, "body": data, "more_body": True})
         else:
-            await send({"type": "http.response.body", "body": b""})
+            await send({"type": RESP_TYPE_BODY, "body": b""})
 
+    @staticmethod
     async def _init_response(
-        self, status: HTTPStatus, headers: List[Dict[str, Any]], send
+        status: Union[HTTPStatus, int], headers: List[Tuple[bytes, bytes]], send
     ):
-        """Helper method for constructing and sending HTTP response back to client
+        """Helper method for constructing and sending HTTP response back to client.
 
         Args:
-            status HTTPStatus
-            headers dict: headers in the response
-            send: ASGI send method
+            status: HTTP status code for this response.
+            headers dict: headers to be sent in this response.
+            send: ASGI send method.
         """
         await send(
             {
-                "type": "http.response.start",
+                "type": RESP_TYPE_START,
                 "status": status,
                 "headers": headers,
             }
         )
+
+    @asynccontextmanager
+    async def _error_handling_for_cache_retrieving(self, url: str, send):
+        _common_err_msg = f"request for {url=} failed"
+        try:
+            yield
+        except aiohttp.ClientResponseError as e:
+            logger.error(f"{_common_err_msg} due to HTTP error: {e!r}")
+            # passthrough 4xx(currently 403 and 404) to otaclient
+            await self._respond_with_error(e.status, e.message, send)
+        except aiohttp.ClientConnectionError as e:
+            connection_err_logger.error(
+                f"{_common_err_msg} due to connection error: {e!r}"
+            )
+            await self._respond_with_error(
+                HTTPStatus.BAD_GATEWAY,
+                "failed to connect to remote server",
+                send,
+            )
+        except aiohttp.ClientError as e:
+            logger.error(f"{_common_err_msg} due to aiohttp client error: {e!r}")
+            await self._respond_with_error(
+                HTTPStatus.SERVICE_UNAVAILABLE, f"client error: {e!r}", send
+            )
+        except (BaseOTACacheError, StopAsyncIteration) as e:
+            logger.error(
+                f"{_common_err_msg} due to handled ota_cache internal error: {e!r}"
+            )
+            await self._respond_with_error(
+                HTTPStatus.INTERNAL_SERVER_ERROR, f"internal error: {e!r}", send
+            )
+        except Exception as e:
+            # exceptions rather than aiohttp error indicates
+            # internal errors of ota_cache
+            logger.exception(
+                f"{_common_err_msg} due to unhandled ota_cache internal error: {e!r}"
+            )
+            await self._respond_with_error(
+                HTTPStatus.INTERNAL_SERVER_ERROR, f"internal error: {e!r}", send
+            )
+
+    @asynccontextmanager
+    async def _error_handling_during_transferring(self, url: str, send):
+        """
+        NOTE: for exeception during transferring, only thing we can do is to
+              terminate the transfer by sending empty chunk back to otaclient.
+        """
+        _common_err_msg = f"request for {url=} failed"
+        try:
+            yield
+        except (BaseOTACacheError, StopAsyncIteration) as e:
+            logger.error(
+                f"{_common_err_msg=} due to handled ota_cache internal error: {e!r}"
+            )
+            await self._send_chunk(b"", False, send)
+        except Exception as e:
+            # unexpected internal errors of ota_cache
+            logger.exception(
+                f"{_common_err_msg=} due to unhandled ota_cache internal error: {e!r}"
+            )
+            await self._send_chunk(b"", False, send)
 
     async def _pull_data_and_send(self, url: str, scope, send):
         """Streaming data between OTACache instance and ota_client
@@ -158,120 +271,42 @@ class App:
         yields chunks from file descriptor and streams chunks back to ota_client.
 
         Args:
-            url str: URL requested by ota_client
-            scope: ASGI scope for current request
-            send: ASGI send method
+            url str: URL requested by ota_client.
+            scope: ASGI scope for current request.
+            send: ASGI send method.
         """
-        # pass cookies and other headers needed for proxy into the ota_cache module
-        cookies_dict: Dict[str, str] = dict()
-        extra_headers: Dict[str, str] = dict()
-        # currently we only need cookie and/or authorization header
-        # also parse OTA-File-Cache-Control header
-        ota_cache_control_policies = OTAFileCacheControl()
-        for header in scope["headers"]:
-            if header[0] == b"cookie":
-                cookies_dict = self.parse_raw_cookies(header[1])
-            elif header[0] == b"authorization":
-                extra_headers["Authorization"] = header[1].decode()
-            # custome header for ota_file, see retrieve_file and OTACache for details
-            elif (
-                header[0] == OTAFileCacheControl.HEADER_LOWERCASE.encode()
-                and len(header) == 2
-            ):
-                ota_cache_control_policies = OTAFileCacheControl.parse_header(
-                    header[1].decode()
-                )
+        headers_from_client = parse_raw_headers(scope["headers"])
 
-        respond_started = False
-        try:
-            _bundle = await self._ota_cache.retrieve_file(
-                url, cookies_dict, extra_headers, ota_cache_control_policies
+        # try to get a cache entry for this URL or for file_sha256 indicated by cache_policy
+        retrieved_ota_cache, retrieve_file_executed = None, False
+        async with self._error_handling_for_cache_retrieving(url, send):
+            retrieved_ota_cache = await self._ota_cache.retrieve_file(
+                url, headers_from_client
             )
-            if _bundle is None:
+            retrieve_file_executed = True
+
+        if retrieved_ota_cache is None:
+            # retrieve_file executed successfully, but return nothing
+            if retrieve_file_executed:
+                _msg = f"failed to retrieve fd for {url} from otacache"
+                logger.warning(_msg)
                 await self._respond_with_error(
-                    HTTPStatus.INTERNAL_SERVER_ERROR,
-                    f"failed to retrieve fp for {url}",
-                    send,
+                    HTTPStatus.INTERNAL_SERVER_ERROR, _msg, send
                 )
-                return
-            fp, meta = _bundle
+            return
+        fp, headers_to_client = retrieved_ota_cache
 
-            # NOTE: currently only record content_type and content_encoding
-            headers = []
-            if meta.content_type:
-                headers.append([b"Content-Type", meta.content_type.encode()])
-            if meta.content_encoding:
-                headers.append([b"Content-Encoding", meta.content_encoding.encode()])
-
-            # prepare the response to the client
-            await self._init_response(HTTPStatus.OK, headers, send)
-            respond_started = True
-
-            # stream the response to the client
+        async with self._error_handling_during_transferring(url, send):
+            await self._init_response(
+                HTTPStatus.OK, encode_headers(headers_to_client), send
+            )
             async for chunk in fp:
                 await self._send_chunk(chunk, True, send)
-            # finish the streaming by send a 0 len payload
             await self._send_chunk(b"", False, send)
 
-        except aiohttp.ClientResponseError as e:
-            logger.error(f"request for {url=} failed due to HTTP error: {e!r}")
-            # passthrough 4xx(currently 403 and 404) to otaclient
-            await self._respond_with_error(e.status, e.message, send)
-        except aiohttp.ClientConnectionError as e:
-            connection_err_logger.error(
-                f"request for {url=} failed due to connection error: {e!r}"
-            )
-            if respond_started:
-                await send({"type": "http.response.body", "body": b""})
-            else:
-                await self._respond_with_error(
-                    HTTPStatus.BAD_GATEWAY,
-                    "failed to connect to remote server",
-                    send,
-                )
-        except aiohttp.ClientError as e:
-            logger.error(
-                f"request for {url=} failed due to aiohttp client error: {e!r}"
-            )
-            # terminate the transmission
-            if respond_started:
-                await send({"type": "http.response.body", "body": b""})
-            else:
-                await self._respond_with_error(
-                    HTTPStatus.SERVICE_UNAVAILABLE, f"client error: {e!r}", send
-                )
-        except (BaseOTACacheError, StopAsyncIteration) as e:
-            logger.error(
-                f"request for {url=} failed due to handled ota_cache internal error: {e!r}"
-            )
-            # terminate the transmission
-            if respond_started:
-                await send({"type": "http.response.body", "body": b""})
-            else:
-                await self._respond_with_error(
-                    HTTPStatus.INTERNAL_SERVER_ERROR, f"internal error: {e!r}", send
-                )
-        except Exception as e:
-            # exceptions rather than aiohttp error indicates
-            # internal errors of ota_cache
-            logger.exception(
-                f"request for {url=} failed due to unhandled ota_cache internal error: {e!r}"
-            )
-            # terminate the transmission
-            if respond_started:
-                await send({"type": "http.response.body", "body": b""})
-            else:
-                await self._respond_with_error(
-                    HTTPStatus.INTERNAL_SERVER_ERROR, f"internal error: {e!r}", send
-                )
-
-    async def app(self, scope, send):
+    async def http_handler(self, scope, send):
         """The real entry for the ota_proxy."""
-        from urllib.parse import urlparse
-
-        assert scope["type"] == "http"
-        # check method, currently only support GET method
-        if scope["method"] != "GET":
+        if scope["method"] != METHOD_GET:
             msg = "ONLY SUPPORT GET METHOD."
             await self._respond_with_error(HTTPStatus.BAD_REQUEST, msg, send)
             return
@@ -294,7 +329,7 @@ class App:
         It filters requests, hands valid requests over to the app entry,
         and handles lifespan protocol to start/stop server properly.
         """
-        if scope["type"] == "lifespan":
+        if scope["type"] == REQ_TYPE_LIFESPAN:
             # handling lifespan protocol
             while True:
                 message = await receive()
@@ -305,6 +340,6 @@ class App:
                     await self.stop()
                     await send({"type": "lifespan.shutdown.complete"})
                     return
-        else:
-            # real app entry
-            await self.app(scope, send)
+        elif scope["type"] == REQ_TYPE_HTTP:
+            await self.http_handler(scope, send)
+        # ignore unknown request type
