629bafa8e1bccc05bd7778aaa47b670c53f7a878
bodong.yang <bodong.yang@tier4.jp>
2023-11-16 03:55:12 +0000

    configs: add docstrings for most of the configs and consts

--- otaclient/app/configs.py
+++ otaclient/app/configs.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Runtime configs and consts for otaclient."""
 
 
 from __future__ import annotations
@@ -54,20 +55,22 @@ class _FixedInternalConfigs(BaseModel):
     OTACLIENT_PID_FPATH: str = "/run/otaclient.pid"
     SUPPORTED_COMPRESS_ALG: ClassVar[Tuple[str, ...]] = ("zst", "zstd")
 
+    # filesystem label of external cache source
     EXTERNAL_CACHE_DEV_FSLABEL: ClassVar[str] = "ota_cache_src"
 
 
 class _DynamicRootedPathsConfig(BaseModel):
     """Dynamic generated internal paths config.
 
-    Paths configured in this class are dynamically adjusted with specified
-    ACTIVE_ROOTFS.
+    Paths configured in this class are dynamically adjusted and rooted with
+    specified ACTIVE_ROOTFS, by default ACTIVE_ROOTFS is "/".
     """
 
     #
-    # --- active_rootfs & container mode ---
+    # ------ active_rootfs & container mode ------ #
     #
     DEFAULT_ACTIVE_ROOTFS: ClassVar[str] = "/"
+
     ACTIVE_ROOTFS: Annotated[
         str,
         AfterValidator(validator_wrapper(isabs)),
@@ -78,18 +81,22 @@ class _DynamicRootedPathsConfig(BaseModel):
     def IS_CONTAINER(self) -> bool:
         """Whether otaclient is running as container.
 
-        If active rootfs is specified and not /, otaclient will
+        If active rootfs is specified and not "/", otaclient will
         activate the container running mode.
         """
         return self.ACTIVE_ROOTFS != self.DEFAULT_ACTIVE_ROOTFS
 
     #
-    # --- mount point placement ---
+    # ------ mount point placement ------ #
     #
     DEFAULT_OTACLIENT_MOUNT_SPACE: ClassVar[str] = "/mnt/otaclient"
 
     @cached_computed_field
     def OTACLIENT_MOUNT_SPACE_DPATH(self) -> str:
+        """The dynamically rooted location to hold mount points created by otaclient.
+
+        Default: /mnt/otaclient
+        """
         return replace_root(
             self.DEFAULT_OTACLIENT_MOUNT_SPACE,
             self.DEFAULT_ACTIVE_ROOTFS,
@@ -98,65 +105,113 @@ class _DynamicRootedPathsConfig(BaseModel):
 
     @cached_computed_field
     def STANDBY_SLOT_MP(self) -> str:
+        """The dynamically rooted location to mount standby slot partition.
+
+        Default: /mnt/otaclient/standby_slot
+        """
         return os.path.join(self.OTACLIENT_MOUNT_SPACE_DPATH, "standby_slot")
 
     @cached_computed_field
     def ACTIVE_SLOT_MP(self) -> str:
+        """The dynamically rooted location to mount active slot partition.
+
+        Default: /mnt/otaclient/active_slot
+        """
         return os.path.join(self.OTACLIENT_MOUNT_SPACE_DPATH, "active_slot")
 
     #
-    # --- /boot related ---
+    # ------ /boot related ------ #
     #
     @cached_computed_field
     def BOOT_DPATH(self) -> str:
+        """The dynamically rooted location of /boot dir.
+
+        Default: /boot
+        """
         return os.path.join(self.ACTIVE_ROOTFS, "boot")
 
     # /boot/ota and its files
 
     @cached_computed_field
     def BOOT_OTA_DPATH(self) -> str:
+        """The dynamically rooted location holding proxy_info.yaml and ecu_info.yaml.
+
+        Default: /boot/ota
+        """
         return os.path.join(self.BOOT_DPATH, "ota")
 
     @cached_computed_field
     def ECU_INFO_FPATH(self) -> str:
+        """The dynamically rooted location of ecu_info.yaml.
+
+        Default: /boot/ota/ecu_info.yaml
+        """
         return os.path.join(self.BOOT_OTA_DPATH, "ecu_info.yaml")
 
     @cached_computed_field
     def PROXY_INFO_FPATH(self) -> str:
+        """The dynamically rooted location of proxy_info.yaml.
+
+        Default: /boot/ota/proxy_info.yaml
+        """
         return os.path.join(self.BOOT_OTA_DPATH, "proxy_info.yaml")
 
-    # /boot/ota-status and its files
+    # --- /boot/ota-status and its files --- #
+    # NOTE: the actual location of these files depends on each boot controller
+    #       implementation, please refer to boot_control.configs.
 
     OTA_STATUS_FNAME: ClassVar[str] = "status"
     OTA_VERSION_FNAME: ClassVar[str] = "version"
     SLOT_IN_USE_FNAME: ClassVar[str] = "slot_in_use"
 
-    # some files under /etc
+    # --- some files under /etc --- #
 
     @cached_computed_field
     def ETC_DPATH(self) -> str:
+        """The dynamically rooted location of /etc folder.
+
+        Default: /etc
+        """
         return os.path.join(self.ACTIVE_ROOTFS, "etc")
 
     @cached_computed_field
     def PASSWD_FPATH(self) -> str:
+        """The dynamically rooted location of /etc/passwd file.
+
+        Default: /etc/passwd
+        """
         return os.path.join(self.ETC_DPATH, "passwd")
 
     @cached_computed_field
     def GROUP_FPATH(self) -> str:
+        """The dynamically rooted location of /etc/group file.
+
+        Default: /etc/group
+        """
         return os.path.join(self.ETC_DPATH, "group")
 
     @cached_computed_field
     def FSTAB_FPATH(self) -> str:
+        """The dynamically rooted location of /etc/fstab file.
+
+        Default: /etc/fstab
+        """
         return os.path.join(self.ETC_DPATH, "fstab")
 
     #
-    # ------ /opt/ota paths ------
+    # ------ /opt/ota paths ------ #
     #
+    # This folder holds files and packages related to OTA functionality.
+
     DEFAULT_OTA_CERTS_DPATHS: ClassVar[str] = "/opt/ota/client/certs"
     DEFAULT_OTA_INSTALLATION_PATH: ClassVar[str] = "/opt/ota"
 
     @cached_computed_field
     def OTA_INSTALLATION_PATH(self) -> str:
+        """The dynamically rooted OTA installation path.
+
+        Default: /opt/ota
+        """
         return replace_root(
             self.DEFAULT_OTA_INSTALLATION_PATH,
             self.DEFAULT_ACTIVE_ROOTFS,
@@ -165,6 +220,10 @@ class _DynamicRootedPathsConfig(BaseModel):
 
     @cached_computed_field
     def OTA_CERTS_DPATH(self) -> str:
+        """The dynamically rooted location of certs for OTA metadata validation.
+
+        Default: /opt/ota/client/certs
+        """
         return replace_root(
             self.DEFAULT_OTA_CERTS_DPATHS,
             self.DEFAULT_ACTIVE_ROOTFS,
@@ -173,14 +232,28 @@ class _DynamicRootedPathsConfig(BaseModel):
 
     @cached_computed_field
     def OTACLIENT_INSTALLATION_PATH(self) -> str:
+        """The dynamically rooted location of otaclient installation path.
+
+        Default: /opt/ota/client
+        """
         return os.path.join(self.OTA_INSTALLATION_PATH, "client")
 
     @cached_computed_field
     def ACTIVE_IMAGE_META_DPATH(self) -> str:
+        """The dynamically rooted location of the image-meta of active slot.
+
+        Default: /opt/ota/image-meta
+        """
         return os.path.join(self.OTA_INSTALLATION_PATH, "image-meta")
 
     @cached_computed_field
     def STANDBY_IMAGE_META_DPATH(self) -> str:
+        """The location of save destination of standby slot.
+
+        NOTE: this location is relatived to the standby slot's mount point.
+
+        Default: /mnt/otaclient/standby_slot/opt/ota/image-meta
+        """
         return replace_root(
             self.ACTIVE_IMAGE_META_DPATH, self.ACTIVE_ROOTFS, self.STANDBY_SLOT_MP
         )
@@ -190,10 +263,20 @@ class _DynamicRootedPathsConfig(BaseModel):
     #
     @cached_computed_field
     def EXTERNAL_CACHE_DEV_MOUNTPOINT(self) -> str:
+        """The mount point for external OTA cache source partition.
+
+        Default: /mnt/otaclient/external_cache_src
+        """
         return os.path.join(self.OTACLIENT_MOUNT_SPACE_DPATH, "external_cache_src")
 
     @cached_computed_field
     def EXTERNAL_CACHE_SRC_PATH(self) -> str:
+        """The data folder of the external cache source filesystem.
+
+        NOTE: this path is relatived to the external cache source mount point.
+
+        Default: /mnt/otaclient/external_cache_src/data
+        """
         return os.path.join(self.EXTERNAL_CACHE_DEV_MOUNTPOINT, "data")
 
 
@@ -201,7 +284,7 @@ class _InternalConfigs(_FixedInternalConfigs, _DynamicRootedPathsConfig):
     """Internal configs for otaclient.
 
     User should not change these settings, except ACTIVE_ROOTFS if running as container,
-        otherwise otaclient might not work properly or backward-compatibility breaks.
+    otherwise otaclient might not work properly or backward-compatibility breaks.
     """
 
 
@@ -209,26 +292,28 @@ class _NormalConfigs(BaseModel):
     """User configurable otaclient settings.
 
     These settings can tune the runtime performance and behavior of otaclient,
-        configurable via environment variables, with prefix OTA.
+    configurable via environment variables, with prefix OTA.
     For example, to set SERVER_ADDRESS, set env OTA_SERVER_ADDRESS=10.0.1.1 .
     """
 
-    # enable internal debug feature
+    #
+    # ------ enable internal debug feature ------ #
+    #
     # currently available feature:
     #   1. enable failure_traceback in status API response
     DEBUG_MODE: bool = False
 
-    # OTA used tmp folder on standby slot
-    OTA_TMP_DPATH: str = "/ota-tmp"
+    # name of OTA used temp folder
+    OTA_TMP_DNAME: str = "ota_tmp"
 
     #
-    # ------ otaclient grpc server config ------
+    # ------ otaclient grpc server config ------ #
     #
     SERVER_ADDRESS: IPvAnyAddress = IPvAnyAddress("0.0.0.0")
     SERVER_PORT: int = Field(default=50051, ge=0, le=65535)
 
     #
-    # ------ otaproxy server config ------
+    # ------ otaproxy server config ------ #
     #
     OTA_PROXY_LISTEN_ADDRESS: IPvAnyAddress = IPvAnyAddress("0.0.0.0")
     OTA_PROXY_LISTEN_PORT: int = Field(default=8082, ge=0, le=65535)
@@ -255,16 +340,12 @@ class _NormalConfigs(BaseModel):
     # ------ otaclient runtime behavior setting ------ #
     #
 
-    #
-    # --- request dispatch settings ---
-    #
+    # --- request dispatch settings --- #
     WAITING_SUBECU_ACK_REQ_TIMEOUT: int = 6
     QUERYING_SUBECU_STATUS_TIMEOUT: int = 30
     LOOP_QUERYING_SUBECU_STATUS_INTERVAL: int = 10
 
-    #
     # --- file I/O settings --- #
-    #
     CHUNK_SIZE: int = 1 * 1024 * 1024  # 1MB
     LOCAL_CHUNK_SIZE: int = 4 * 1024 * 1024  # 4MB
 
@@ -339,7 +420,10 @@ class _NormalConfigs(BaseModel):
     # Idle status polling interval, when ther is no active OTA updaste in the cluster.
     IDLE_INTERVAL: int = 10  # seconds
 
-    # --- default version str ---
+    #
+    # --- default version str --- #
+    #
+    # The string return in status API firmware_version field if version is unknown.
     DEFAULT_VERSION_STR: str = ""
 
 
@@ -348,14 +432,21 @@ class Config(_InternalConfigs, _NormalConfigs):
 
     @cached_computed_field
     def STANDBY_OTA_TMP_DPATH(self) -> str:
-        return replace_root(
-            self.OTA_TMP_DPATH, self.DEFAULT_ACTIVE_ROOTFS, self.STANDBY_SLOT_MP
-        )
+        """The location for holding OTA runtime files during OTA.
+
+        NOTE: this location is relatived to standby slot's mount point.
+
+        Default: /mnt/otaclient/standby_slot/ota_tmp
+        """
+        return os.path.join(self.STANDBY_SLOT_MP, self.OTA_TMP_DNAME)
 
 
-# init config
+#
+# ------ init config ------ #
+#
 
 ENV_PREFIX = "OTA_"
+# NOTE: ACTIVE_ROOTFS is specially treated and retrieved via HOST_ROOTFS_ENV.
 HOST_ROOTFS_ENV = f"{ENV_PREFIX}HOST_ROOTFS"
 
 
@@ -363,9 +454,11 @@ def _init_config() -> Config:
     class _ConfigurableNormalConfigs(BaseSettings, _NormalConfigs):
         """one-time class that parse configs from environment vars."""
 
+        # retrieve user configurable configs from environmental variables
         model_config = SettingsConfigDict(env_prefix=ENV_PREFIX)
 
     return Config(
+        # especially get ACTIVE_ROOTFS via HOST_ROOTFS_ENV.
         ACTIVE_ROOTFS=os.getenv(
             HOST_ROOTFS_ENV, _DynamicRootedPathsConfig.DEFAULT_ACTIVE_ROOTFS
         ),
