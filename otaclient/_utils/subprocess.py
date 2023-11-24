9545e5023b983fd1d4763b1a3fbaad1bfaa405b1
bodong.yang <bodong.yang@tier4.jp>
2023-11-24 02:23:57 +0000

    utils.subprocess: refine and cleanup

--- otaclient/_utils/subprocess.py
+++ otaclient/_utils/subprocess.py
@@ -23,13 +23,39 @@ from typing import Callable, Optional
 
 from . import truncate_str_or_bytes
 
-
-class SubProcessCalledFailed(Exception):
-    ...
-
-
 # prevent too-long stdout/stderr in err when handling exception
 _ERR_MAX_LEN = 2048
+
+
+class SubProcessCalledFailed(Exception):
+    def __init__(
+        self,
+        cmd: str = "",
+        *,
+        return_code: int = -1,
+        stdout: str = "",
+        stderr: str = "",
+        new_root: Optional[str] = None,
+        msg: str = "",
+    ) -> None:
+        self.cmd = cmd
+        self.return_code = return_code
+        self.stdout = truncate_str_or_bytes(stdout, _ERR_MAX_LEN)
+        self.stderr = truncate_str_or_bytes(stderr, _ERR_MAX_LEN)
+        self.new_root = new_root
+        self.msg = msg
+        super().__init__(f"{cmd=} execution failed({new_root=}): {msg}")
+
+    @property
+    def err_report(self):
+        if self.cmd:
+            return (
+                f"cmd({self.cmd}) execution failed(new_root={self.new_root}): {self.msg}\n"
+                f"return_code=({self.return_code}, \n"
+                f"stderr={truncate_str_or_bytes(self.stderr, _ERR_MAX_LEN)}, \n"
+                f"stdout={truncate_str_or_bytes(self.stdout, _ERR_MAX_LEN)})\n"
+            )
+        return f"other failure: {self.msg}"
 
 
 def subprocess_call(
@@ -38,27 +64,28 @@ def subprocess_call(
     new_root: Optional[str] = None,
     raise_exception: bool = False,
     timeout: Optional[float] = None,
-) -> None:
+    capture_output: bool = False,
+    default: Optional[str] = None,
+) -> str | None:
     """Run <cmd> in subprocess without returnning the output.
 
     Args:
         cmd (str | list[str]): the <cmd> to be execute.
-        new_root (str): if this command is required to run with chroot, specifying
+        new_root (str = None): if this command is required to run with chroot, specifying
             which root to chroot to.
-        raise_exception (bool): if true, exception before/during subprocess execution
+        raise_exception (bool = False): if true, exception before/during subprocess execution
             will be raised, otherwise exception will be handled.
             Note that exception raised due to <new_root> is invalid will always be raised.
-        timeout (floats): subprocess execution timeout.
-        default (str): if subprocess execution failed but <raise_exception> is False,
+        timeout (floats = None): subprocess execution timeout.
+
+        capture_output (bool = False): whether to capture stdout and return it.
+        default (str = None): (only valid when <capture_output> is True) if subprocess execution failed but <raise_exception> is False,
             use <default> as return value.
 
     Raises:
         SubprocessCalledFailed exception if subprocess call failed or <new_root> is specified
             but invalid(not found or not a dir).
     """
-
-    # NOTE: we need to check the stderr and stdout when error occurs,
-    # so use subprocess.run here instead of subprocess.check_call
     if isinstance(cmd, str):
         cmd = shlex.split(cmd)
 
@@ -66,93 +93,36 @@ def subprocess_call(
     if new_root:
         if not os.path.isdir(new_root):
             raise SubProcessCalledFailed(
-                f"try to run {cmd} with chroot, but {new_root} is not a dir"
+                msg=f"try to run cmd={' '.join(cmd)} with chroot, but {new_root=} is not a dir"
             )
         _preexec_fn = functools.partial(os.chroot, new_root)
 
     try:
-        subprocess.run(
+        _res = subprocess.run(
             cmd,
             check=True,
             capture_output=True,
             preexec_fn=_preexec_fn,
             timeout=timeout,
         )
+        if capture_output:
+            return _res.stdout.decode()
+
     except subprocess.CalledProcessError as e:
-        msg = (
-            f"{cmd=} failed: \n"
-            f"return_code=({e.returncode}, \n"
-            f"stderr={truncate_str_or_bytes(e.stderr, _ERR_MAX_LEN)}, \n"
-            f"stdout={truncate_str_or_bytes(e.stdout, _ERR_MAX_LEN)})\n"
-        )
         if raise_exception:
-            raise SubProcessCalledFailed(msg) from e
-    except Exception as e:
-        if raise_exception:
-            raise SubProcessCalledFailed(f"unexpected exception: {e!r}") from e
-
-
-def subprocess_check_output(
-    cmd: str | list[str],
-    *,
-    new_root: Optional[str] = None,
-    raise_exception: bool = False,
-    timeout: Optional[float] = None,
-    default: Optional[str] = None,
-) -> str | None:
-    """Run <cmd> in subprocess, return the execution result.
-
-    Args:
-        cmd (str | list[str]): the <cmd> to be execute.
-        new_root (str): if this command is required to run with chroot, specifying
-            which root to chroot to.
-        raise_exception (bool): if true, exception before/during subprocess execution
-            will be raised, otherwise exception will be handled.
-            Note that exception raised due to new_root is invalid will always be raised.
-        timeout (floats): subprocess execution timeout.
-        default (str): if subprocess execution failed but <raise_exception> is False,
-            use <default> as return value.
-
-    Return:
-        The UTF-8 decoded result from subprocess stdout, or <default> if execution failed
-            but <raise_exception> is False.
-
-    Raises:
-        SubprocessCalledFailed exception if subprocess call failed or <new_root> is specified
-            but invalid(not found or not a dir).
-    """
-    # NOTE: we need to check the stderr and stdout when error occurs,
-    # so use subprocess.run here instead of subprocess.check_call
-    if isinstance(cmd, str):
-        cmd = shlex.split(cmd)
-
-    _preexec_fn: Optional[Callable[[], None]] = None
-    if new_root:
-        if not os.path.isdir(new_root):
             raise SubProcessCalledFailed(
-                f"try to run {cmd} with chroot, but {new_root} is not a dir"
-            )
-        _preexec_fn = functools.partial(os.chroot, new_root)
+                " ".join(cmd),
+                return_code=e.returncode,
+                new_root=new_root,
+                stderr=str(e.stderr),
+            ) from e
+        if capture_output:
+            return default
 
-    try:
-        return subprocess.run(
-            cmd,
-            check=True,
-            capture_output=True,
-            preexec_fn=_preexec_fn,
-            timeout=timeout,
-        ).stdout.decode()
-    except subprocess.CalledProcessError as e:
-        msg = (
-            f"{cmd=} failed: \n"
-            f"return_code=({e.returncode}, \n"
-            f"stderr={truncate_str_or_bytes(e.stderr, _ERR_MAX_LEN)}, \n"
-            f"stdout={truncate_str_or_bytes(e.stdout, _ERR_MAX_LEN)})\n"
-        )
-        if raise_exception:
-            raise SubProcessCalledFailed(msg) from e
-        return default
     except Exception as e:
         if raise_exception:
-            raise SubProcessCalledFailed(f"unexpected exception: {e!r}") from e
-        return default
+            raise SubProcessCalledFailed(
+                msg=f"unexpected exception:{cmd=}, {e!r}"
+            ) from e
+        if capture_output:
+            return default
