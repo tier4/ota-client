00e27c8d930c75e44a71bed208397f639c73981b
bodong.yang <bodong.yang@tier4.jp>
2023-12-13 02:05:04 +0000

    cmdhelper: still add more specific helper methods for getting each attrs from dev

--- otaclient/app/_cmdhelpers.py
+++ otaclient/app/_cmdhelpers.py
@@ -1,3 +1,16 @@
+# Copyright 2022 TIER IV, INC. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """Helper functions for calling common used commands in subprocess."""
 
 
@@ -155,6 +168,11 @@ def get_attr_from_dev(
     """Use lsblk to query dev attr value."""
     _args = ["-in", "-o", attr_n, str(dev)]
     return _lsblk(_args, timeout=timeout, raise_exception=raise_exception)
+
+
+get_dev_fsuuid = functools.partial(get_attr_from_dev, attr_n="UUID")
+get_dev_partuuid = functools.partial(get_attr_from_dev, attr_n="PARTUUID")
+get_dev_fslabel = functools.partial(get_attr_from_dev, attr_n="LABEL")
 
 
 def gen_uuid_str(uuid: str) -> str:
