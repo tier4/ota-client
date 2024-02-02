0aa96f64b91e4c9168461948ada8374e8ef6265c
Bodong YANG <bodong.yang@tier4.jp>
2024-02-02 09:21:09 +0000

    minor docs cleanup

--- otaclient/configs/ecu_info.py
+++ otaclient/configs/ecu_info.py
@@ -11,20 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-r"""ECU metadatas definition.
-
-Version 1 scheme example:
-    format_vesrion: 1
-    ecu_id: "autoware"
-    ip_addr: "0.0.0.0"
-    bootloader: "grub"
-    secondaries:
-        - ecu_id: "p1"
-            ip_addr: "0.0.0.0"
-    available_ecu_ids:
-        - "autoware"
-        - "p1
-"""
+"""ECU metadatas definition and parsing logic."""
 
 
 from __future__ import annotations
--- otaclient/configs/proxy_info.py
+++ otaclient/configs/proxy_info.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""proxy_info.yaml parsing."""
+"""proxy_info.yaml definition and parsing logic."""
 
 
 from __future__ import annotations
