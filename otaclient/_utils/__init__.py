6158b5994f9c99896be29247cc735d4f40ca094f
Bodong Yang <86948717+Bodong-Yang@users.noreply.github.com>
2024-03-05 11:14:13 +0900

    refactor: backport new persist file handling implementation to v3.6.x (#276)
    This PR backports the new persist file handling implementation from main branch to replace the old copy-tree implementation.
    See #268 for more details.

--- otaclient/_utils/__init__.py
+++ otaclient/_utils/__init__.py
@@ -14,6 +14,7 @@
 
 
 from __future__ import annotations
+import os
 from math import ceil
 from pathlib import Path
 from typing import Any, Callable, Optional, TypeVar
@@ -66,3 +67,21 @@ def get_file_size(
     swapfile_fpath = Path(swapfile_fpath)
     if swapfile_fpath.is_file():
         return ceil(swapfile_fpath.stat().st_size / _multiplier[units])
+
+
+def replace_root(path: str | Path, old_root: str | Path, new_root: str | Path) -> str:
+    """Replace a <path> relative to <old_root> to <new_root>.
+
+    For example, if path="/abc", old_root="/", new_root="/new_root",
+    then we will have "/new_root/abc".
+    """
+    # normalize all the input args
+    path = os.path.normpath(path)
+    old_root = os.path.normpath(old_root)
+    new_root = os.path.normpath(new_root)
+
+    if not (old_root.startswith("/") and new_root.startswith("/")):
+        raise ValueError(f"{old_root=} and/or {new_root=} is not valid root")
+    if os.path.commonpath([path, old_root]) != old_root:
+        raise ValueError(f"{old_root=} is not the root of {path=}")
+    return os.path.join(new_root, os.path.relpath(path, old_root))
--- otaclient/_utils/linux.py
+++ otaclient/_utils/linux.py
@@ -18,6 +18,11 @@ from pathlib import Path
 from subprocess import check_call
 
 
+#
+# ------ swapfile handling ------ #
+#
+
+
 def create_swapfile(
     swapfile_fpath: str | Path, size_in_MiB: int, *, timeout=900
 ) -> Path:
@@ -63,3 +68,85 @@ def create_swapfile(
     check_call(["mkswap", str(swapfile_fpath)], timeout=timeout)
 
     return swapfile_fpath
+
+
+#
+# ------ gid/uid mapping ------ #
+#
+
+_SPLITTER = ":"
+
+
+class ParsedPasswd:
+    """Parse passwd and store name/uid mapping.
+
+    Example passwd entry line:
+    nobody:x:65534:65534:nobody:/nonexistent:/usr/sbin/nologin
+
+    Attrs:
+        _by_name (dict[str, int]): name:uid mapping.
+        _by_uid (dict[int, str]): uid:name mapping.
+    """
+
+    __slots__ = ["_by_name", "_by_uid"]
+
+    def __init__(self, passwd_fpath: str | Path) -> None:
+        self._by_name: dict[str, int] = {}
+        try:
+            with open(passwd_fpath, "r") as f:
+                for line in f:
+                    _raw_list = line.strip().split(_SPLITTER)
+                    _name, _uid = _raw_list[0], int(_raw_list[2])
+                    self._by_name[_name] = _uid
+            self._by_uid = {v: k for k, v in self._by_name.items()}
+        except Exception as e:
+            raise ValueError(f"invalid or missing {passwd_fpath=}: {e!r}")
+
+
+class ParsedGroup:
+    """Parse group and store name/gid mapping.
+
+    Example group entry line:
+    nogroup:x:65534:
+
+    Attrs:
+        _by_name (dict[str, int]): name:gid mapping.
+        _by_gid (dict[int, str]): gid:name mapping.
+    """
+
+    __slots__ = ["_by_name", "_by_gid"]
+
+    def __init__(self, group_fpath: str | Path) -> None:
+        self._by_name: dict[str, int] = {}
+        try:
+            with open(group_fpath, "r") as f:
+                for line in f:
+                    _raw_list = line.strip().split(_SPLITTER)
+                    self._by_name[_raw_list[0]] = int(_raw_list[2])
+            self._by_gid = {v: k for k, v in self._by_name.items()}
+        except Exception as e:
+            raise ValueError(f"invalid or missing {group_fpath=}: {e!r}")
+
+
+def map_uid_by_pwnam(*, src_db: ParsedPasswd, dst_db: ParsedPasswd, uid: int) -> int:
+    """Perform src_uid -> src_name -> dst_name -> dst_uid mapping.
+
+    Raises:
+        ValueError on failed mapping.
+    """
+    try:
+        return dst_db._by_name[src_db._by_uid[uid]]
+    except KeyError:
+        raise ValueError(f"failed to find mapping for {uid}")
+
+
+def map_gid_by_grpnam(*, src_db: ParsedGroup, dst_db: ParsedGroup, gid: int) -> int:
+    """Perform src_gid -> src_name -> dst_name -> dst_gid mapping.
+
+    Raises:
+        ValueError on failed mapping.
+    """
+    try:
+        return dst_db._by_name[src_db._by_gid[gid]]
+    except KeyError:
+        raise ValueError(f"failed to find mapping for {gid}")
