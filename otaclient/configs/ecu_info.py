c1e09549709197bb85dd1f57498327f86f46e235
Bodong Yang <bodong.yang@tier4.jp>
2024-03-27 07:03:05 +0000

    backport_v3.6.x: backport PR#270

---
+++ otaclient/configs/ecu_info.py
@@ -0,0 +1,139 @@
+# Copyright 2022 TIER IV, INC. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""ECU metadatas definition and parsing logic."""
+
+
+from __future__ import annotations
+import logging
+import yaml
+import warnings
+from enum import Enum
+from pathlib import Path
+from pydantic import AfterValidator, BeforeValidator, Field
+from typing import List
+from typing_extensions import Annotated
+
+from otaclient._utils.typing import StrOrPath, gen_strenum_validator
+from otaclient.configs._common import (
+    BaseFixedConfig,
+    NetworkPort,
+    IPAddressAny,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class BootloaderType(str, Enum):
+    """Bootloaders that supported by otaclient.
+
+    auto_detect: (DEPRECATED) at runtime detecting what bootloader is in use in this ECU.
+    grub: generic x86_64 platform with grub.
+    cboot: ADLink rqx-580, rqx-58g, with BSP 32.5.x.
+        (theoretically other Nvidia jetson xavier devices using cboot are also supported)
+    rpi_boot: raspberry pi 4 with eeprom version newer than 2020-10-28(with tryboot support).
+    """
+
+    AUTO_DETECT = "auto_detect"
+    GRUB = "grub"
+    CBOOT = "cboot"
+    RPI_BOOT = "rpi_boot"
+
+    @staticmethod
+    def deprecation_validator(value: BootloaderType) -> BootloaderType:
+        if value == BootloaderType.AUTO_DETECT:
+            _warning_msg = (
+                "bootloader field in ecu_info.yaml is not set or set to auto_detect(DEPRECATED), "
+                "runtime bootloader type detection is UNRELIABLE and bootloader field SHOULD be "
+                "set in ecu_info.yaml"
+            )
+            warnings.warn(_warning_msg, DeprecationWarning, stacklevel=2)
+            logger.warning(_warning_msg)
+        return value
+
+
+class ECUContact(BaseFixedConfig):
+    ecu_id: str
+    ip_addr: IPAddressAny
+    # NOTE(20240327): set the default as literal for now, in the future
+    #   this will be service_cfg.CLIENT_CALL_PORT.
+    port: NetworkPort = 50051
+
+
+class ECUInfo(BaseFixedConfig):
+    """ECU info configuration.
+
+    Attributes:
+        format_version: the ecu_info.yaml scheme version, current is 1.
+        ecu_id: the unique ID of this ECU.
+        ip_addr: the IP address OTA servicer listening on, default is <service_config.DEFAULT_SERVER_ADDRESS>.
+        bootloader: the bootloader type of this ECU.
+        available_ecu_ids: a list of ECU IDs that should be involved in OTA campaign.
+        secondaries: a list of ECUContact objects for sub ECUs.
+    """
+
+    format_version: int = 1
+    ecu_id: str
+    # NOTE(20240327): set the default as literal for now, in the future
+    #   when app_configs are fully backported this will be replaced by
+    #   service_cfg.DEFAULT_SERVER_ADDRESS
+    ip_addr: IPAddressAny = "127.0.0.1"
+    bootloader: Annotated[
+        BootloaderType,
+        BeforeValidator(gen_strenum_validator(BootloaderType)),
+        AfterValidator(BootloaderType.deprecation_validator),
+    ] = BootloaderType.AUTO_DETECT
+    available_ecu_ids: List[str] = Field(default_factory=list)
+    secondaries: List[ECUContact] = Field(default_factory=list)
+
+    def get_available_ecu_ids(self) -> list[str]:
+        """
+        NOTE: this method should be used instead of directly accessing the
+              available_ecu_ids attrs for backward compatibility reason.
+        """
+        # onetime fix, if no availabe_ecu_id is specified,
+        # add my_ecu_id into the list
+        if len(self.available_ecu_ids) == 0:
+            return [self.ecu_id]
+        return self.available_ecu_ids.copy()
+
+
+# NOTE: this is backward compatibility for old x1 that doesn't have
+#       ecu_info.yaml installed.
+DEFAULT_ECU_INFO = ECUInfo(
+    format_version=1,  # current version is 1
+    ecu_id="autoware",  # should be unique for each ECU in vehicle
+)
+
+
+def parse_ecu_info(ecu_info_file: StrOrPath) -> ECUInfo:
+    try:
+        _raw_yaml_str = Path(ecu_info_file).read_text()
+    except FileNotFoundError as e:
+        logger.warning(f"{ecu_info_file=} not found: {e!r}")
+        logger.warning(f"use default ecu_info: {DEFAULT_ECU_INFO}")
+        return DEFAULT_ECU_INFO
+
+    try:
+        loaded_ecu_info = yaml.safe_load(_raw_yaml_str)
+        assert isinstance(loaded_ecu_info, dict), "not a valid yaml file"
+        return ECUInfo.model_validate(loaded_ecu_info, strict=True)
+    except Exception as e:
+        logger.warning(f"{ecu_info_file=} is invalid: {e!r}\n{_raw_yaml_str=}")
+        logger.warning(f"use default ecu_info: {DEFAULT_ECU_INFO}")
+        return DEFAULT_ECU_INFO
+
+
+# NOTE(20240327): set the default as literal for now,
+#   in the future this will be app_cfg.ECU_INFO_FPATH
+ecu_info = parse_ecu_info(ecu_info_file="/boot/ota/ecu_info.yaml")
---
+++ otaclient/configs/proxy_info.py
@@ -0,0 +1,154 @@
+# Copyright 2022 TIER IV, INC. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""proxy_info.yaml definition and parsing logic."""
+
+
+from __future__ import annotations
+import logging
+import yaml
+import warnings
+from functools import cached_property
+from typing import Any, ClassVar
+from pathlib import Path
+from pydantic import AliasChoices, Field
+
+from otaclient._utils.typing import StrOrPath
+from otaclient.configs._common import (
+    BaseFixedConfig,
+    IPAddressAny,
+    HTTPURLAny,
+    NetworkPort,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class ProxyInfo(BaseFixedConfig):
+    """OTA-proxy configuration.
+
+    NOTE 1(20221220): when proxy_info.yaml is missing/not a valid yaml,
+                      a pre_defined proxy_info.yaml as follow will be used.
+
+    Attributes:
+        format_version: the proxy_info.yaml scheme version, current is 1.
+        enable_local_ota_proxy: whether to launch a local ota_proxy server.
+        enable_local_ota_proxy_cache: enable cache mechanism on ota-proxy.
+        local_ota_proxy_listen_addr: ipaddr ota_proxy listens on.
+        local_ota_proxy_listen_port: port ota_proxy used.
+        upper_ota_proxy: the URL of upper OTA proxy used by local ota_proxy server
+            or otaclient(proxy chain).
+        logging_server: the URL of AWS IoT otaclient logs upload server.
+    """
+
+    format_version: int = 1
+    # NOTE(20221219): the default values for the following settings
+    #                 now align with v2.5.4
+    upper_ota_proxy: HTTPURLAny = Field(default="", validate_default=False)
+    enable_local_ota_proxy: bool = Field(
+        default=False,
+        # NOTE(20240126): "enable_ota_proxy" is superseded by "enable_local_ota_proxy".
+        validation_alias=AliasChoices(
+            "enable_local_ota_proxy",
+            "enable_ota_proxy",
+        ),
+    )
+    # NOTE(20240327): set the default as literal for now, in the future
+    #   this will be app_cfg.OTA_PROXY_LISTEN_ADDRESS and app_cfg.OTA_PROXY_LISTEN_PORT.
+    local_ota_proxy_listen_addr: IPAddressAny = "0.0.0.0"
+    local_ota_proxy_listen_port: NetworkPort = 8082
+    # NOTE: this field not presented in v2.5.4,
+    #       for current implementation, it should be default to True.
+    #       This field doesn't take effect if enable_local_ota_proxy is False
+    enable_local_ota_proxy_cache: bool = True
+
+    # NOTE(20240201): check ota_client_log_server_port var in autoware_ecu_setup
+    #                 ansible configurations.
+    LOGGING_SERVER_PORT: ClassVar[int] = 8083
+    # NOTE: when logging_server is not configured, it implicitly means the logging server
+    #       is located at localhost.
+    #       check roles/ota_client/templates/run.sh.j2 in ecu_setup repo.
+    logging_server: HTTPURLAny = f"http://127.0.0.1:{LOGGING_SERVER_PORT}"
+
+    def get_proxy_for_local_ota(self) -> str | None:
+        """Tell local otaclient which proxy to use(or not use any)."""
+        if self.enable_local_ota_proxy:
+            # if local otaproxy is enabled, local otaclient also uses it
+            return f"http://{self.local_ota_proxy_listen_addr}:{self.local_ota_proxy_listen_port}"
+        elif self.upper_ota_proxy:
+            # else we directly use the upper proxy
+            return self.upper_ota_proxy
+        # default not using proxy
+
+    @cached_property
+    def gateway_otaproxy(self) -> bool:
+        """Whether this local otaproxy is a gateway otaproxy.
+
+        Evidence is if no upper_ota_proxy, then this otaproxy should act as a gateway.
+        NOTE(20240202): this replaces the previous user-configurable gateway field in
+                        the proxy_info.yaml.
+        """
+        return not bool(self.upper_ota_proxy)
+
+
+# deprecated field definition
+# <deprecated_old_name> -> <new_field_name>
+_deprecated_field: dict[str, str] = {"enable_ota_proxy": "enable_local_ota_proxy"}
+
+
+def _deprecation_check(_in: dict[str, Any]) -> None:
+    """
+    NOTE: in the future if pydantic support deprecated field annotated, use that
+          mechanism instead of this function.
+    """
+    for old_fname, _ in _in.items():
+        if new_fname := _deprecated_field.get(old_fname):
+            warnings.warn(
+                f"option field '{old_fname}' is superseded by '{new_fname}', "
+                f"and the support for '{old_fname}' option might be dropped in the future. "
+                f"please use '{new_fname}' in proxy_info.yaml instead.",
+                DeprecationWarning,
+                stacklevel=2,
+            )
+
+
+# NOTE: this default is for backward compatible with old device
+#       that doesn't have proxy_info.yaml installed.
+DEFAULT_PROXY_INFO = ProxyInfo(
+    format_version=1,
+    enable_local_ota_proxy=True,
+)
+
+
+def parse_proxy_info(proxy_info_file: StrOrPath) -> ProxyInfo:
+    try:
+        _raw_yaml_str = Path(proxy_info_file).read_text()
+    except FileNotFoundError as e:
+        logger.warning(f"{proxy_info_file=} not found: {e!r}")
+        logger.warning(f"use default proxy_info: {DEFAULT_PROXY_INFO}")
+        return DEFAULT_PROXY_INFO
+
+    try:
+        loaded_proxy_info = yaml.safe_load(_raw_yaml_str)
+        assert isinstance(loaded_proxy_info, dict), "not a valid yaml file"
+        _deprecation_check(loaded_proxy_info)
+        return ProxyInfo.model_validate(loaded_proxy_info, strict=True)
+    except Exception as e:
+        logger.warning(f"{proxy_info_file=} is invalid: {e!r}\n{_raw_yaml_str=}")
+        logger.warning(f"use default proxy_info: {DEFAULT_PROXY_INFO}")
+        return DEFAULT_PROXY_INFO
+
+
+# NOTE(20240327): set the default as literal for now,
+#   in the future this will be app_cfg.PROXY_INFO_FPATH
+proxy_info = parse_proxy_info("/boot/ota/proxy_info.yaml")
