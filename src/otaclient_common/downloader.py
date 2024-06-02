d8202dcbba1cd79c7bdc8a0b4de932bc34a8022b
bodong.yang <bodong.yang@tier4.jp>
2024-06-02 15:33:57 +0000

    minor fix downloader

--- src/otaclient_common/downloader.py
+++ src/otaclient_common/downloader.py
@@ -53,9 +53,8 @@ from urllib3.util.retry import Retry
 
 from ota_proxy import OTAFileCacheControl
 from otaclient._utils import copy_callable_typehint
-
-from ..otaclient.app.configs import config as cfg
-from .common import wait_with_backoff
+from otaclient.app.configs import config as cfg
+from otaclient_common.common import wait_with_backoff
 
 logger = logging.getLogger(__name__)
 
