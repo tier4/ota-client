47b48ddb5b25c1ae9b0460f9c849f83977b2c241
bodong.yang <bodong.yang@tier4.jp>
2024-06-02 15:09:03 +0000

    re-arrange test_otaclient

--- tests/test_ota_proxy/test_ota_proxy_server.py
+++ tests/test_ota_proxy/test_ota_proxy_server.py
@@ -28,8 +28,8 @@ import pytest
 import uvicorn
 
 from ota_proxy.utils import url_based_hash
-from otaclient.app.ota_metadata import parse_regulars_from_txt
-from otaclient.app.proto.wrapper import RegularInf
+from ota_metadata.legacy.parser import parse_regulars_from_txt
+from ota_metadata.legacy.types import RegularInf
 from tests.conftest import ThreadpoolExecutorFixtureMixin, cfg
 
 logger = logging.getLogger(__name__)
