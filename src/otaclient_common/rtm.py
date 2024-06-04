528fed140815252886d5707e040be88eeda2dcf4
bodong.yang <bodong.yang@tier4.jp>
2024-06-04 15:33:01 +0000

    finish up

--- src/otaclient_common/rtm.py
+++ src/otaclient_common/rtm.py
@@ -1,3 +1,18 @@
+# Copyright 2022 TIER IV, INC. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 from __future__ import annotations
 
 import itertools
@@ -25,6 +40,14 @@ class ThreadPoolExecutorWithRetry(ThreadPoolExecutor):
         thread_name_prefix: str = "",
         max_total_retry: Optional[int] = None,
     ) -> None:
+        """Initialize a ThreadPoolExecutorWithRetry instance.
+
+        Args:
+            max_concurrent (int, optional): How many tasks should be kept in the memory. Defaults to 128.
+            max_workers (Optional[int], optional): Max number of worker threads in the pool. Defaults to None.
+            thread_name_prefix (str, optional): Defaults to "".
+            max_total_retry (Optional[int], optional): Max total retry counts before abort. Defaults to None.
+        """
         self.max_total_retry = max_total_retry
 
         self._start_lock, self._started = threading.Lock(), False
@@ -63,8 +86,6 @@ class ThreadPoolExecutorWithRetry(ThreadPoolExecutor):
 
         return _task
 
-    # APIs
-
     @property
     def executor_interrupted(self) -> bool:
         return self._executor_interrupted
@@ -72,6 +93,19 @@ class ThreadPoolExecutorWithRetry(ThreadPoolExecutor):
     def ensure_tasks(
         self, func: Callable[[T], RT], iterable: Iterable[T]
     ) -> Generator[Future[RT], None, None]:
+        """Ensure all the items in <iterable> are processed by <func> in the pool.
+
+        Args:
+            func (Callable[[T], RT]): The function to take the item from <iterable>.
+            iterable (Iterable[T]): The iterable of items to be processed by <func>.
+
+        Raises:
+            ValueError: If the pool is shutdown or broken, or this method has already
+                being called once.
+
+        Yields:
+            The Future instance of each processed tasks.
+        """
         with self._start_lock:
             if self._started:
                 raise ValueError("ensure_tasks cannot be started more than once")
