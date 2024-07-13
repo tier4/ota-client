e4e5a3a045f5ac0473e995d1130004bdbde1dca0
bodong.yang <bodong.yang@tier4.jp>
2024-07-13 04:26:24 +0000

    implement configs._paths

---
+++ src/otaclient/configs/_paths.py
@@ -0,0 +1,79 @@
+# Copyright 2022 TIER IV, INC. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Path consts used by otaclient."""
+
+
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Any, Literal
+
+from . import _consts
+
+
+class _StaticPathConsts:
+    """Paths that are static and rooted at /."""
+
+    OTACLIENT_PID_FILE = "/run/otaclient.pid"
+    OTACLIENT_RUN_DIR = "/run/otaclient"
+    OTACLIENT_MOUNT_SPACE = "/run/otaclient/mnt"
+    OTACLIENT_INSTALLATION_DPATH = "/opt/ota/client"
+    ACTIVE_SLOT_MOUNT = "/run/otaclient/mnt/active_slot"
+    STANDY_SLOT_MOUNT = "/run/otaclient/mnt/standby_slot"
+
+    OTA_IMAGE_META_FOLDER = "/opt/ota/image-meta"
+    OTA_TMP_STORE = "/.ota-tmp"
+    """OTA temporary storage at standby slot during OTA."""
+
+
+class _DynamicPathConsts:
+    """Paths that will be dynamically re-rooted to <HOST_ROOTFS>."""
+
+    _HOST_ROOTFS: Literal["/"] | str = "/"
+
+    # ------ common system paths ------ #
+    ETC_DPATH = "/etc"
+    BOOT_MP = "/boot"
+
+    # ------ otaclient installation ------ #
+    OTACLIENT_INSTALLATION_DPATH = _StaticPathConsts.OTACLIENT_INSTALLATION_DPATH
+    OTACLIENT_CERTS_DPATH = f"{OTACLIENT_INSTALLATION_DPATH}/certs"
+    OTA_IMAGE_META_FOLDER = "/opt/ota/image-meta"
+
+    # ------ otaclient configuration dir ------ #
+    OTACLIENT_CONFIGS_DPATH = f"{BOOT_MP}/ota"
+    ECU_INFO_FPATH = f"{OTACLIENT_CONFIGS_DPATH}/{_consts.ECU_INFO_FNAME}"
+    PROXY_INFO_FPATH = f"{OTACLIENT_CONFIGS_DPATH}/{_consts.PROXY_INFO_FNAME}"
+
+    # ------ system files used/checked/updated by otaclient ------ #
+    PASSWD_FPATH = f"{ETC_DPATH}/passwd"
+    GROUP_FPATH = f"{ETC_DPATH}/passwd"
+    FSTAB_FPATH = f"{ETC_DPATH}/fstab"
+
+    def __init__(self, host_rootfs: Literal["/"] | str = "/") -> None:
+        self._HOST_ROOTFS = host_rootfs
+
+    def __getattribute__(self, name: str) -> str | Any:
+        attr_value = super().__getattribute__(name)
+        if name.startswith("_") or self._HOST_ROOTFS == "/":
+            return attr_value
+
+        # dynamically update the root according to HOST_ROOTFS value.
+        attr_value = Path(attr_value).relative_to("/")
+        return str(self._HOST_ROOTFS / attr_value)
+
+
+def _extract_path_attrs(obj: Any) -> dict[str, str]:
+    attrns = filter(lambda x: not x.startswith("_"), dir(obj))
+    return {k: getattr(obj, k) for k in attrns if isinstance(getattr(obj, k), str)}
