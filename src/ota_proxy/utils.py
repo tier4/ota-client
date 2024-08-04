b7ea2f70de6e134cddbd788b566c461c6fbc0fae
bodong.yang <bodong.yang@tier4.jp>
2024-08-04 13:29:18 +0000

    otaproxy.utils: remove unused

--- src/ota_proxy/utils.py
+++ src/ota_proxy/utils.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import asyncio
 from concurrent.futures import Executor
 from hashlib import sha256
 from os import PathLike
@@ -9,28 +8,6 @@ from typing import AsyncIterator
 import aiofiles
 
 from .config import config as cfg
-
-
-def get_backoff(n: int, factor: float, _max: float) -> float:
-    return min(_max, factor * (2 ** (n - 1)))
-
-
-async def wait_with_backoff(
-    _retry_cnt: int, *, _backoff_factor: float, _backoff_max: float
-) -> bool:
-    """
-    Returns:
-        A bool indicates whether upper caller should keep retry.
-    """
-    _timeout = get_backoff(
-        _retry_cnt,
-        _backoff_factor,
-        _backoff_max,
-    )
-    if _timeout <= _backoff_max:
-        await asyncio.sleep(_timeout)
-        return True
-    return False
 
 
 async def read_file(fpath: PathLike, *, executor: Executor) -> AsyncIterator[bytes]:
