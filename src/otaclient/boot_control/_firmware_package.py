e60c0d1bd163dcf1d55d51516442bb2873748b6a
Bodong Yang <86948717+Bodong-Yang@users.noreply.github.com>
2024-08-08 20:45:21 +0900

    feature: introduce firmware package control for NVIDIA Jetson device (#376)
    This PR introduces the implementation of firmware package control specification. This specification defines how the firmware package folder's layout, the metadata files format and the firmware update trigger mechanism.
    Currently this specification is only for managing NVIDIA devices' firmware update package and firmware update.

---
+++ src/otaclient/boot_control/_firmware_package.py
@@ -0,0 +1,188 @@
+# Copyright 2022 TIER IV, INC. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Implementation of firmware update package management specification.
+
+This specification defines how the firmware update packages are described by metadata.
+    otaclient can look at these metafiles to discover available firmware update packages.
+    Also, it provides a mechanism to control when otaclient should trigger the firmware update.
+    Currently this module is only for NVIDIA Jetson device with UEFI Capsule update support.
+
+A typical firmware package folder layout, assuming the firmware dir is /opt/ota/firmware:
+```
+/opt/ota/firmware
+├── BOOTAA64.efi
+├── bl_only_payload.Cap
+├── firmware_dir_layout
+└── firmware_manifest.yaml
+```
+
+If firmware update is needed, a file name `firmware_update.yaml` needed to be prepared under the
+    firmware package dir, then the folder layout will become:
+```
+/opt/ota/firmware
+├── BOOTAA64.efi
+├── bl_only_payload.Cap
+├── firmware_dir_layout
+├── firmware_manifest.yaml
+└── firmware_update.yaml
+```
+"""
+
+
+from __future__ import annotations
+
+import re
+from enum import Enum
+from typing import Any, List, Literal
+
+from pydantic import BaseModel, BeforeValidator, GetCoreSchemaHandler
+from pydantic_core import CoreSchema, core_schema
+from typing_extensions import Annotated
+
+from otaclient_common.typing import gen_strenum_validator
+
+
+class PayloadType(str, Enum):
+    UEFI_CAPSULE = "UEFI-CAPSULE"
+    UEFI_BOOT_APP = "UEFI-BOOT-APP"
+
+
+DIGEST_PATTERN = re.compile(r"^(?P<algorithm>[\w\-+ ]+):(?P<digest>[a-zA-Z0-9]+)$")
+
+
+def _pydantic_str_schema(
+    cls, source_type: Any, handler: GetCoreSchemaHandler
+) -> CoreSchema:
+    """Pydantic schema adapter for str."""
+    return core_schema.no_info_after_validator_function(cls, handler(str))
+
+
+class DigestValue(str):
+    """Implementation of digest value schema <algorithm>:<hexstr>."""
+
+    def __init__(self, _in: str) -> None:
+        _in = _in.strip()
+        if not (ma := DIGEST_PATTERN.match(_in)):
+            raise ValueError(f"invalid digest value: {_in}")
+        self.algorithm = ma.group("algorithm")
+        self.digest = ma.group("digest")
+
+    __get_pydantic_core_schema__ = classmethod(_pydantic_str_schema)
+
+
+class NVIDIAFirmwareCompat(BaseModel):
+    board_id: str
+    board_sku: str
+    board_rev: str
+    fab_id: str
+    board_conf: str
+
+    def check_compat(self, _tnspec: str) -> bool:
+        return all(
+            _tnspec.find(getattr(self, field_name)) >= 0
+            for field_name in self.model_fields
+        )
+
+
+class NVIDIAUEFIFirmwareSpec(BaseModel):
+    # NOTE: let the jetson-uefi module parse the bsp_version
+    bsp_version: str
+    firmware_compat: NVIDIAFirmwareCompat
+
+
+class PayloadFileLocation(str):
+    """Specifying the payload file location.
+
+    It supports file URL or digest value.
+
+    If the location is specified by file URL, it means the payload is stored
+        as a regular file on the system image.
+    If the location is specified by a string with schame "<algorithm>:<hexstr>",
+        it means the file is stored in the blob storage of OTA image.
+    """
+
+    location_type: Literal["blob", "file"]
+    location_path: str | DigestValue
+
+    def __init__(self, _in: str) -> None:
+        if _in.startswith("file://"):
+            self.location_type = "file"
+            self.location_path = _in.replace("file://", "", 1)
+        else:
+            self.location_type = "blob"
+            self.location_path = DigestValue(_in)
+
+    __get_pydantic_core_schema__ = classmethod(_pydantic_str_schema)
+
+
+class FirmwarePackage(BaseModel):
+    """Metadata of a firmware update package payload."""
+
+    payload_name: str
+    file_location: Annotated[PayloadFileLocation, BeforeValidator(PayloadFileLocation)]
+    type: Annotated[PayloadType, BeforeValidator(gen_strenum_validator(PayloadType))]
+    digest: DigestValue
+
+
+class HardwareType(str, Enum):
+    """
+    Currently we only support NVIDIA Jetson device.
+    """
+
+    nvidia_jetson = "nvidia_jetson"
+
+
+class FirmwareManifest(BaseModel):
+    """Implementation of firmware manifest yaml file.
+
+    It contains the basic information of the target hardware, and the metadata of
+        all available firmware update packages.
+    """
+
+    format_version: Literal[1] = 1
+    hardware_type: Annotated[
+        HardwareType, BeforeValidator(gen_strenum_validator(HardwareType))
+    ]
+    hardware_series: str
+    hardware_model: str
+    firmware_spec: NVIDIAUEFIFirmwareSpec
+    firmware_packages: List[FirmwarePackage]
+
+    def check_compat(self, _tnspec: str) -> bool:
+        """Check if an input compat string is compatible with this compat spec.
+
+        Compat string example:
+            2888-400-0004-M.0-1-2-jetson-xavier-rqx580-
+        """
+        return self.firmware_spec.firmware_compat.check_compat(_tnspec)
+
+    def get_firmware_packages(
+        self, update_request: FirmwareUpdateRequest
+    ) -> list[FirmwarePackage]:
+        """Get a list of firmware update packages according to update_request."""
+        return [
+            _payload
+            for _payload in self.firmware_packages
+            if _payload.payload_name in update_request.firmware_list
+        ]
+
+
+class FirmwareUpdateRequest(BaseModel):
+    """Implementation of firmware update request.
+
+    It contains a list of firmware to be used to update the target device.
+    """
+
+    format_version: Literal[1] = 1
+    firmware_list: List[str]
---
+++ tests/test_otaclient/test_boot_control/test_firmware_package.py
@@ -0,0 +1,211 @@
+# Copyright 2022 TIER IV, INC. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from __future__ import annotations
+
+import pytest
+import yaml
+
+from otaclient.boot_control._firmware_package import (
+    DigestValue,
+    FirmwareManifest,
+    FirmwarePackage,
+    FirmwareUpdateRequest,
+    HardwareType,
+    NVIDIAFirmwareCompat,
+    NVIDIAUEFIFirmwareSpec,
+    PayloadFileLocation,
+    PayloadType,
+)
+
+
+@pytest.mark.parametrize(
+    "_in, _expected",
+    (
+        (
+            "sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+            [
+                "sha256",
+                "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+            ],
+        ),
+        (
+            "sha1:da39a3ee5e6b4b0d3255bfef95601890afd80709",
+            [
+                "sha1",
+                "da39a3ee5e6b4b0d3255bfef95601890afd80709",
+            ],
+        ),
+    ),
+)
+def test_digest_value_parsing(_in, _expected):
+    _parsed = DigestValue(_in)
+    assert _parsed.algorithm == _expected[0]
+    assert _parsed.digest == _expected[1]
+
+
+@pytest.mark.parametrize(
+    "_in, _expected",
+    (
+        (
+            "file:///opt/ota/firmware/BOOTAA64.efi",
+            ["file", "/opt/ota/firmware/BOOTAA64.efi"],
+        ),
+        (
+            _in := "sha256:32baa6f7e96661d50fb78e5d7149763e3a0fe70c51c37c6bea92c3c27cd2472d",
+            [
+                "blob",
+                DigestValue(_in),
+            ],
+        ),
+    ),
+)
+def test_payload_file_location(_in, _expected):
+    _parsed = PayloadFileLocation(_in)
+    assert _parsed.location_type == _expected[0]
+    assert _parsed.location_path == _expected[1]
+
+
+@pytest.mark.parametrize(
+    "_spec, _compat_str, _expected",
+    (
+        # rqx58g compat against rqx58g compat str
+        (
+            NVIDIAFirmwareCompat(
+                board_id="2888",
+                board_sku="0004",
+                board_rev="M.0",
+                fab_id="400",
+                board_conf="jetson-xavier-rqx580",
+            ),
+            "2888-400-0004-M.0-1-2-jetson-xavier-rqx580-",
+            True,
+        ),
+        # rqx580 compat against rqx58g compat str
+        (
+            NVIDIAFirmwareCompat(
+                board_id="2888",
+                board_sku="0004",
+                board_rev="L.0",
+                fab_id="400",
+                board_conf="jetson-xavier-rqx580",
+            ),
+            "2888-400-0004-M.0-1-2-jetson-xavier-rqx580-",
+            False,
+        ),
+    ),
+)
+def test_check_compat(_spec, _compat_str, _expected):
+    assert _spec.check_compat(_compat_str) is _expected
+
+
+EXAMPLE_FIRMWARE_MANIFEST = """\
+format_version: 1
+hardware_type: nvidia_jetson
+hardware_series: ADLINK RQX
+hardware_model: rqx580
+firmware_spec:
+  bsp_version: r35.4.1
+  firmware_compat:
+    board_id: "2888"
+    board_sku: "0004"
+    board_rev: L.0
+    fab_id: "400"
+    board_conf: jetson-xavier-rqx580
+firmware_packages:
+  - payload_name: bl_only_payload.Cap
+    file_location: file:///opt/ota/firmware/bl_only_payload.Cap
+    type: UEFI-CAPSULE
+    digest: "sha256:32baa6f7e96661d50fb78e5d7149763e3a0fe70c51c37c6bea92c3c27cd2472d"
+  - payload_name: BOOTAA64.efi
+    file_location: file:///opt/ota/firmware/BOOTAA64.efi
+    type: UEFI-BOOT-APP
+    digest: "sha256:ac17457772666351154a5952e3b87851a6398da2afcf3a38bedfc0925760bb0e"
+"""
+
+EXAMPLE_FIRMWARE_MANIFEST_PARSED = FirmwareManifest(
+    format_version=1,
+    hardware_type=HardwareType("nvidia_jetson"),
+    hardware_series="ADLINK RQX",
+    hardware_model="rqx580",
+    firmware_spec=NVIDIAUEFIFirmwareSpec(
+        bsp_version="r35.4.1",
+        firmware_compat=NVIDIAFirmwareCompat(
+            board_id="2888",
+            board_sku="0004",
+            board_rev="L.0",
+            fab_id="400",
+            board_conf="jetson-xavier-rqx580",
+        ),
+    ),
+    firmware_packages=[
+        FirmwarePackage(
+            payload_name="bl_only_payload.Cap",
+            file_location=PayloadFileLocation(
+                "file:///opt/ota/firmware/bl_only_payload.Cap"
+            ),
+            type=PayloadType("UEFI-CAPSULE"),
+            digest=DigestValue(
+                "sha256:32baa6f7e96661d50fb78e5d7149763e3a0fe70c51c37c6bea92c3c27cd2472d"
+            ),
+        ),
+        FirmwarePackage(
+            payload_name="BOOTAA64.efi",
+            file_location=PayloadFileLocation("file:///opt/ota/firmware/BOOTAA64.efi"),
+            type=PayloadType("UEFI-BOOT-APP"),
+            digest=DigestValue(
+                "sha256:ac17457772666351154a5952e3b87851a6398da2afcf3a38bedfc0925760bb0e"
+            ),
+        ),
+    ],
+)
+
+
+@pytest.mark.parametrize(
+    "_in, _expected",
+    (
+        (
+            EXAMPLE_FIRMWARE_MANIFEST,
+            EXAMPLE_FIRMWARE_MANIFEST_PARSED,
+        ),
+    ),
+)
+def test_firmware_manifest_parsing(_in, _expected):
+    assert FirmwareManifest.model_validate(yaml.safe_load(_in)) == _expected
+
+
+EXAMPLE_FIRMWARE_UPDATE_REQUEST = """\
+format_version: 1
+firmware_list:
+  - bl_only_payload.Cap
+  - BOOTAA64.efi
+"""
+
+EXAMPLE_FIRMWARE_UPDATE_REQUEST_PARSED = FirmwareUpdateRequest(
+    format_version=1, firmware_list=["bl_only_payload.Cap", "BOOTAA64.efi"]
+)
+
+
+@pytest.mark.parametrize(
+    "_in, _expected",
+    (
+        (
+            EXAMPLE_FIRMWARE_UPDATE_REQUEST,
+            EXAMPLE_FIRMWARE_UPDATE_REQUEST_PARSED,
+        ),
+    ),
+)
+def test_firmware_update_request_parsing(_in, _expected):
+    assert FirmwareUpdateRequest.model_validate(yaml.safe_load(_in)) == _expected
