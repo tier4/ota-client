f48958bc033a5435c57b0ab55bdc30f09af3ca1a
bodong.yang <bodong.yang@tier4.jp>
2024-08-09 10:38:40 +0900

    implement hatch custom build hook to build proto generated code

---
+++ proto/hatch_build.py
@@ -0,0 +1,102 @@
+# Copyright 2022 TIER IV, INC. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Dynamically generate protobuf python code."""
+
+
+from __future__ import annotations
+
+import os
+import shutil
+import subprocess
+import sys
+from pathlib import Path
+from typing import Any
+
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+
+
+def _protoc_compile(
+    proto_file: str,
+    *,
+    extra_imports: list[str] | None,
+    output_dir: str | Path,
+    work_dir: str | Path,
+):
+    # fmt: off
+    cmd = [
+        sys.executable, "-m", "grpc_tools.protoc",
+        f"--python_out={output_dir}",
+        f"--pyi_out={output_dir}",
+        f"--grpc_python_out={output_dir}", 
+    ]
+    # fmt: on
+    if isinstance(extra_imports, list):
+        for _import in extra_imports:
+            cmd.append(f"-I{_import}")
+
+    cmd.append(proto_file)
+    print(f"call protoc with: {' '.join(cmd)}")
+    subprocess.check_call(cmd, cwd=work_dir)
+
+
+def _get_built_pb2_module_name(proto_file: str) -> str:
+    """
+    built module name will be the proto_file name + "_pb2".
+
+    For example, from "abc.proto" we will get module named "abc_pb2".
+    """
+    proto_fname = os.path.basename(proto_file)
+    _split = os.path.splitext(proto_fname)
+    return f"{_split[0]}_pb2"
+
+
+class CustomBuildHook(BuildHookInterface):
+    """
+    Configs:
+        proto_file: file to generate python code against.
+        extra_imports: a list of paths for proto imports.
+        output_dir: where to put the built python code to.
+    """
+
+    def initialize(self, version: str, build_data: dict[str, Any]) -> None:
+        config = self.config
+
+        # ------ parse config ------ #
+        output_dir = config["output_dir"]
+        proto_file = config["proto_file"]
+        extra_imports = config.get("extra_imports", [])
+
+        # ------ validate config ------ #
+        if not isinstance(extra_imports, list):
+            raise ValueError(
+                f"expect extra_imports to be a list, get {type(extra_imports)}"
+            )
+
+        if not os.path.exists(proto_file):
+            raise FileNotFoundError(f"{proto_file=} not found, abort")
+
+        # let the folder where proto file exists become the work_dir
+        work_dir = os.path.dirname(proto_file)
+        if not work_dir:
+            work_dir = "."
+        # always place the folder where proto_file exists
+        extra_imports.append(work_dir)
+
+        # ------ compile proto file ------ #
+        _protoc_compile(
+            proto_file,
+            extra_imports=extra_imports,
+            output_dir=output_dir,
+            work_dir=work_dir,
+        )
--- proto/src/otaclient_pb2/v2/__init__.py
+++
@@ -1,4 +0,0 @@
-__version__ = version = "2.1.0"
-__version_tuple__ = version_tuple = (2, 1, 0)
-
-__all__ = ["version", "__version__", "version_tuple", "__version_tuple__"]
