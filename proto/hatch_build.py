785978c99e85fcda1fbfb2db670f1f9f2dfca95a
bodong.yang <bodong.yang@tier4.jp>
2024-08-09 11:48:04 +0900

    support building multiple version of API

--- proto/hatch_build.py
+++ proto/hatch_build.py
@@ -17,6 +17,7 @@
 from __future__ import annotations
 
 import os
+import shutil
 import string
 import subprocess
 import sys
@@ -40,110 +41,93 @@ INIT_TEMPLATE = """\
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-import sys
-
-from . import ${pb2_module_name} as pb2
-
 __version__ = version = "${api_version}"
 __version_tuple__ = version_tuple = tuple(version.split("."))
 
 __all__ = ["version", "__version__", "version_tuple", "__version_tuple__"]
-
-# also place the pb2 module directly under the global namespace, this is required by
-#   pb2_grpc module as pb2_grpc module imports pb2 moduel without any prefix.
-sys.modules["${pb2_module_name}"] = pb2
 """
+OUTPUT_BASE = "src"
 
 
 def _protoc_compile(
     proto_file: str,
+    output_base: str,
+    output_package: str,
     *,
     extra_imports: list[str] | None,
-    output_dir: str | Path,
     work_dir: str | Path,
 ):
+    # copy the proto_file to the output_dir to retain the proper package import layout
+    output_package_dir = Path(output_base) / output_package
+    shutil.copy(proto_file, output_package_dir)
+    _proto_file = output_package_dir / os.path.basename(proto_file)
+
     # fmt: off
     cmd = [
         sys.executable, "-m", "grpc_tools.protoc",
-        f"--python_out={output_dir}",
-        f"--pyi_out={output_dir}",
-        f"--grpc_python_out={output_dir}", 
+        f"--python_out={output_base}",
+        f"--pyi_out={output_base}",
+        f"--grpc_python_out={output_base}", 
     ]
     # fmt: on
     if isinstance(extra_imports, list):
         for _import in extra_imports:
             cmd.append(f"-I{_import}")
+    # also place the output_base as import path
+    cmd.append(f"-I{output_base}")
 
-    cmd.append(proto_file)
+    cmd.append(str(_proto_file))
     print(f"call protoc with: {' '.join(cmd)}")
     subprocess.check_call(cmd, cwd=work_dir)
 
 
-def _get_built_pb2_module_name(proto_file: str) -> str:
-    """
-    built module name will be the proto_file name + "_pb2".
-
-    For example, from "abc.proto" we will get module named "abc_pb2".
-    """
-    proto_fname = os.path.basename(proto_file)
-    _split = os.path.splitext(proto_fname)
-    return f"{_split[0]}_pb2"
-
-
-def _generate__init__(api_version: str, proto_file: str, *, output_dir: str | Path):
-    pb2_module_name = _get_built_pb2_module_name(proto_file)
+def _generate__init__(api_version: str, *, output_dir: str | Path):
     template = string.Template(INIT_TEMPLATE)
     init_file = Path(output_dir) / "__init__.py"
-    init_file.write_text(
-        template.substitute(
-            api_version=api_version,
-            pb2_module_name=pb2_module_name,
-        )
-    )
+    init_file.write_text(template.substitute(api_version=api_version))
 
 
 class CustomBuildHook(BuildHookInterface):
     """
     Configs:
-        proto_file: file to generate python code against.
-        extra_imports: a list of paths for proto imports.
-        output_dir: where to put the built python code to.
         api_version: the API version of otaclient service API.
     """
 
     def initialize(self, version: str, build_data: dict[str, Any]) -> None:
-        config = self.config
+        for config in self.config["proto_builds"]:
+            # ------ parse config ------ #
+            proto_file = config["proto_file"]
+            print(f"build protoc python code for {proto_file} ...")
 
-        # ------ parse config ------ #
-        output_dir = config["output_dir"]
-        proto_file = config["proto_file"]
-        extra_imports = config.get("extra_imports", [])
-        api_version = config.get("api_version", "0.0.0")
+            output_package = config["output_package"]
+            extra_imports = config.get("extra_imports", [])
+            api_version = config.get("api_version", "0.0.0")
 
-        # ------ validate config ------ #
-        if not isinstance(extra_imports, list):
-            raise ValueError(
-                f"expect extra_imports to be a list, get {type(extra_imports)}"
+            # ------ load consts ------ #
+            output_base = OUTPUT_BASE
+
+            # ------ validate config ------ #
+            if not isinstance(extra_imports, list):
+                raise ValueError(
+                    f"expect extra_imports to be a list, get {type(extra_imports)}"
+                )
+
+            if not os.path.exists(proto_file):
+                raise FileNotFoundError(f"{proto_file=} not found, abort")
+
+            # let the folder where proto file exists become the work_dir
+            work_dir = os.path.dirname(proto_file)
+            if not work_dir:
+                work_dir = "."
+
+            # ------ compile proto file ------ #
+            _protoc_compile(
+                proto_file,
+                extra_imports=extra_imports,
+                output_base=output_base,
+                output_package=output_package,
+                work_dir=work_dir,
             )
-
-        if not os.path.exists(proto_file):
-            raise FileNotFoundError(f"{proto_file=} not found, abort")
-
-        # let the folder where proto file exists become the work_dir
-        work_dir = os.path.dirname(proto_file)
-        if not work_dir:
-            work_dir = "."
-        # always place the folder where proto_file exists
-        extra_imports.append(work_dir)
-
-        # ------ compile proto file ------ #
-        _protoc_compile(
-            proto_file,
-            extra_imports=extra_imports,
-            output_dir=output_dir,
-            work_dir=work_dir,
-        )
-        _generate__init__(
-            api_version=api_version, proto_file=proto_file, output_dir=output_dir
-        )
+            _generate__init__(
+                api_version=api_version, output_dir=Path(output_base) / output_package
+            )
