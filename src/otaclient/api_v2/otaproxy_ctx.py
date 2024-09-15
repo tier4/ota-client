80c0bc5910860c77f75faec6889421d7b1f40157
bodong.yang <bodong.yang@tier4.jp>
2024-09-15 16:19:22 +0000

    minor cleanup

--- src/otaclient/api_v2/otaproxy_ctx.py
+++ src/otaclient/api_v2/otaproxy_ctx.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Control the otaproxy startup/shutdown according to child ECUs' status."""
 
 
 from __future__ import annotations
