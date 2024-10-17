cda96660b4dec989a946a05f0b8c68d32c87dde4
bodong.yang <bodong.yang@tier4.jp>
2024-10-17 06:16:57 +0000

    ota_metadata: implement file_table

---
+++ src/ota_metadata/_file_table/tables.py
@@ -0,0 +1,133 @@
+# Copyright 2022 TIER IV, INC. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from __future__ import annotations
+
+from typing import Optional
+
+from pydantic import SkipValidation
+from simple_sqlite3_orm import ConstrainRepr, TableSpec, TypeAffinityRepr
+from typing_extensions import Annotated
+
+
+class DirectoriesTable(TableSpec):
+    path: Annotated[
+        str,
+        TypeAffinityRepr(str),
+        ConstrainRepr("PRIMARY KEY"),
+        SkipValidation,
+    ]
+    mode: Annotated[
+        int,
+        TypeAffinityRepr(int),
+        ConstrainRepr("NOT NULL"),
+        SkipValidation,
+    ]
+    uid: Annotated[
+        int,
+        TypeAffinityRepr(int),
+        ConstrainRepr("NOT NULL"),
+        SkipValidation,
+    ]
+    gid: Annotated[
+        int,
+        TypeAffinityRepr(int),
+        ConstrainRepr("NOT NULL"),
+        SkipValidation,
+    ]
+
+
+class SymlinksTable(TableSpec):
+    path: Annotated[
+        str,
+        TypeAffinityRepr(str),
+        ConstrainRepr("PRIMARY KEY"),
+        SkipValidation,
+    ]
+    uid: Annotated[
+        int,
+        TypeAffinityRepr(int),
+        ConstrainRepr("NOT NULL"),
+        SkipValidation,
+    ]
+    gid: Annotated[
+        int,
+        TypeAffinityRepr(int),
+        ConstrainRepr("NOT NULL"),
+        SkipValidation,
+    ]
+    target: Annotated[
+        str,
+        TypeAffinityRepr(str),
+        ConstrainRepr("NOT NULL"),
+        SkipValidation,
+    ]
+
+
+class RegularFilesTable(TableSpec):
+    path: Annotated[
+        str,
+        TypeAffinityRepr(str),
+        ConstrainRepr("PRIMARY KEY"),
+        SkipValidation,
+    ]
+    mode: Annotated[
+        int,
+        TypeAffinityRepr(int),
+        ConstrainRepr("NOT NULL"),
+        SkipValidation,
+    ]
+    uid: Annotated[
+        int,
+        TypeAffinityRepr(int),
+        ConstrainRepr("NOT NULL"),
+        SkipValidation,
+    ]
+    gid: Annotated[
+        int,
+        TypeAffinityRepr(int),
+        ConstrainRepr("NOT NULL"),
+        SkipValidation,
+    ]
+    size: Annotated[
+        int,
+        TypeAffinityRepr(int),
+        ConstrainRepr("NOT NULL"),
+        SkipValidation,
+    ]
+
+    # schema: <algh> || ":" || <digest>
+    digest: Annotated[
+        Optional[bytes],
+        TypeAffinityRepr(bytes),
+        SkipValidation,
+    ] = None
+
+    nlink: Annotated[
+        Optional[int],
+        TypeAffinityRepr(int),
+        SkipValidation,
+    ] = None
+    inode: Annotated[
+        Optional[int],
+        TypeAffinityRepr(int),
+        SkipValidation,
+    ] = None
+
+    contents: Annotated[
+        Optional[bytes],
+        TypeAffinityRepr(bytes),
+        SkipValidation,
+    ] = None
