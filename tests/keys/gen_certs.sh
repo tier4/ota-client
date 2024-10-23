d32749e012253bc36ad4852e8c22e1c3ff7eac9d
bodong.yang <bodong.yang@tier4.jp>
2024-10-23 02:05:48 +0000

    update tests/gen_certs.sh to correctly create x509v3 sign cert

--- tests/keys/gen_certs.sh
+++ tests/keys/gen_certs.sh
@@ -4,12 +4,14 @@
 
 set -eux
 
+CA_CHAIN_PREFIX=${1:-test}
+
 # Root CA:
 openssl ecparam -out root.key -name prime256v1 -genkey
 openssl req -new -x509 \
     -days $((365 * 10 + 5)) \
     -key root.key \
-    -out root.pem \
+    -out ${CA_CHAIN_PREFIX}.root.pem \
     -sha256 \
     -subj "/C=JP/ST=Tokyo/O=Tier4/CN=root.tier4.jp"
 
@@ -30,14 +32,23 @@ basicConstraints = critical, CA:true, pathlen:0
 openssl x509 -req \
     -days $((365 * 10 + 5)) \
     -in interm.csr \
-    -CA root.pem \
+    -CA ${CA_CHAIN_PREFIX}.root.pem \
     -CAkey root.key \
-    -out interm.pem \
+    -out ${CA_CHAIN_PREFIX}.interm.pem \
     -sha256 -CAcreateserial \
     -extfile <(echo "${CA_INTERM_EXT}") \
     -extensions v3_intermediate_ca
 
-# End user
+# Sign cert
+SIGN_CERT_EXT="
+[ sign_cert ]
+subjectKeyIdentifier = hash
+authorityKeyIdentifier = keyid:always,issuer
+keyUsage = critical, digitalSignature
+extendedKeyUsage = codeSigning
+basicConstraints = critical, CA:FALSE
+"
+
 openssl ecparam -out sign.key -name prime256v1 -genkey
 openssl req -new \
     -key sign.key \
@@ -48,9 +59,11 @@ openssl req -new \
 openssl x509 -req \
     -days 365 \
     -in sign.csr \
-    -CA interm.pem \
+    -CA ${CA_CHAIN_PREFIX}.interm.pem \
     -CAkey interm.key \
     -out sign.pem \
-    -sha256 -CAcreateserial
+    -sha256 -CAcreateserial \
+    -extfile <(echo "${SIGN_CERT_EXT}") \
+    -extensions sign_cert
 
 rm -f root.key interm.key interm.csr sign.csr *.srl
