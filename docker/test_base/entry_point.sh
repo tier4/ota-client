57e55ad99e96ddb874dc4ed61fbff5f3154aafd4
Bodong Yang <86948717+Bodong-Yang@users.noreply.github.com>
2024-10-24 19:47:55 +0900

    feat!: pre-load CA certification chains on otaclient starts up, reject OTA if no CA certs are installed (#408)
    This PR resolves an issue that comes from very old version of otaclient(since v1.0.0), which results in OTA image sign cert verification being skipped when no valid CA cert chains can be imported by otaclient. Also the logic of loading and managing the CA cert chains are split away from ota_metadata.legacy package and becomes a standalone module in ota_metadata.utils.cert_store.
    
    BREAKING CHANGE: Now otaclient will explicitly reject OTA when otaclient is not installed with valid CA cert chains. Also otaclient now only loads the CA cert chains once at startup. Re-configuring CA chains now requires otaclient restart.

--- docker/test_base/entry_point.sh
+++ docker/test_base/entry_point.sh
@@ -4,17 +4,9 @@ set -eu
 TEST_ROOT=/test_root
 OTACLIENT_SRC=/otaclient_src
 OUTPUT_DIR="${OUTPUT_DIR:-/test_result}"
-CERTS_DIR="${CERTS_DIR:-/certs}"
 
 # copy the source code as source is read-only
 cp -R "${OTACLIENT_SRC}" "${TEST_ROOT}"
-
-# copy the certs generated in the docker image to otaclient dir
-echo "setup certificates for testing..."
-cd ${TEST_ROOT}
-mkdir -p ./certs
-cp -av ${CERTS_DIR}/root.pem ./certs/1.root.pem
-cp -av ${CERTS_DIR}/interm.pem ./certs/1.interm.pem
 
 # exec the input params
 echo "execute test with coverage"
