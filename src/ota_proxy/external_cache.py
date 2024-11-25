8da3a33d502917e3b99bb6b1cf449f07484db22c
bodong.yang <bodong.yang@tier4.jp>
2024-11-25 03:19:42 +0000

    otaproxy: implement external cache helper module

---
+++ src/ota_proxy/external_cache.py
@@ -0,0 +1,64 @@
+# Copyright 2022 TIER IV, INC. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Implementation of mounting/umounting external cache."""
+
+
+from __future__ import annotations
+
+import logging
+
+from ota_proxy.config import config
+from otaclient_common import cmdhelper
+from otaclient_common.typing import StrOrPath
+
+logger = logging.getLogger(__name__)
+
+
+def mount_external_cache(
+    mnt_point: StrOrPath, *, cache_dev_fslabel: str = config.EXTERNAL_CACHE_DEV_FSLABEL
+) -> StrOrPath | None:
+    _cache_dev = cmdhelper.get_dev_by_token(
+        "LABEL",
+        cache_dev_fslabel,
+        raise_exception=False,
+    )
+    if not _cache_dev:
+        return
+
+    if len(_cache_dev) > 1:
+        logger.warning(
+            f"multiple external cache storage device found, use the first one: {_cache_dev[0]}"
+        )
+    _cache_dev = _cache_dev[0]
+    logger.info(f"external cache dev detected at {_cache_dev}")
+
+    cmdhelper.ensure_mointpoint(mnt_point, ignore_error=True)
+    try:
+        cmdhelper.ensure_mount(
+            target=_cache_dev,
+            mnt_point=mnt_point,
+            mount_func=cmdhelper.mount_ro,
+            raise_exception=True,
+            max_retry=3,
+        )
+        return mnt_point
+    except Exception as e:
+        logger.warning(f"failed to mount external cache: {e!r}")
+
+
+def umount_external_cache(mnt_point: StrOrPath) -> None:
+    try:
+        cmdhelper.ensure_umount(mnt_point, ignore_error=False)
+    except Exception as e:
+        logger.warning(f"failed to umount external cache {mnt_point=}: {e!r}")
