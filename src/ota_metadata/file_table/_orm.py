b1417170551c1804cd8417e54d315c71000ef967
bodong.yang <bodong.yang@tier4.jp>
2024-12-09 11:07:44 +0000

    finish up file_table

--- src/ota_metadata/file_table/_orm.py
+++ src/ota_metadata/file_table/_orm.py
@@ -16,37 +16,30 @@
 from __future__ import annotations
 
 from sqlite3 import Connection
-from typing import Callable, Literal
+from typing import Literal
 
-from simple_sqlite3_orm import ORMBase, ORMThreadPoolBase
+from simple_sqlite3_orm import ORMBase
 
-from ._table import FileSystemTable
+from ._table import FileTableNonRegularFiles, FileTableRegularFiles
 
 
-class FileSystemTableORM(ORMBase[FileSystemTable]):
+class FileTableRegularFilesORM(ORMBase[FileTableRegularFiles]):
     def __init__(
         self,
         con: Connection,
         schema_name: str | None | Literal["temp"] = None,
     ) -> None:
         super().__init__(
-            con, table_name=FileSystemTable.table_name, schema_name=schema_name
+            con, table_name=FileTableRegularFiles.table_name, schema_name=schema_name
         )
 
 
-class FileSystemTableORMThreadPool(ORMThreadPoolBase[FileSystemTable]):
+class FileTableNonRegularFilesORM(ORMBase[FileTableNonRegularFiles]):
     def __init__(
         self,
-        schema_name: str | None = None,
-        *,
-        con_factory: Callable[[], Connection],
-        number_of_cons: int,
-        thread_name_prefix: str = "",
+        con: Connection,
+        schema_name: str | None | Literal["temp"] = None,
     ) -> None:
         super().__init__(
-            FileSystemTable.table_name,
-            schema_name,
-            con_factory=con_factory,
-            number_of_cons=number_of_cons,
-            thread_name_prefix=thread_name_prefix,
+            con, table_name=FileTableNonRegularFiles.table_name, schema_name=schema_name
         )
