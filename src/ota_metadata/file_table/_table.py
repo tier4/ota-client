9317dbdcd53d2be05475b046b8b9f474f503da63
bodong.yang <bodong.yang@tier4.jp>
2024-12-09 11:04:46 +0000

    re-design file_table, now split file_table to ft_regular and ft_non_regular

--- src/ota_metadata/file_table/_table.py
+++ src/ota_metadata/file_table/_table.py
@@ -19,38 +19,23 @@ import os
 import shutil
 import stat
 from pathlib import Path
-from threading import Lock
 from typing import ClassVar, Literal, Optional
 
-from pydantic import SkipValidation
+from pydantic import BaseModel, SkipValidation
 from simple_sqlite3_orm import ConstrainRepr, TableSpec, TypeAffinityRepr
 from typing_extensions import Annotated
+
+from otaclient_common.typing import StrOrPath
 
 from ._types import InodeTable, Xattr
 
 CANONICAL_ROOT = "/"
 
-
-class HardlinkRegister:
-    def __init__(self) -> None:
-        self._lock = Lock()
-        self._inode_first_path_map: dict[int, str] = {}
-
-    def get_first_copy_fpath(self, inode: int, fpath: str) -> tuple[bool, str]:
-        """For a specific hardlink group, return the first copy of this group.
-
-        Returns:
-            A tuple of bool and str. bool indicates whether the caller is the first copy,
-                str is the fpath of the first copy in the group.
-        """
-        with self._lock:
-            first_copy = self._inode_first_path_map.setdefault(inode, fpath)
-            return first_copy == fpath, first_copy
+PrepareMethod = Literal["move", "hardlink", "copy"]
 
 
-class FileSystemTable(TableSpec):
+class FileTableBase(BaseModel):
     schema_ver: ClassVar[Literal[1]] = 1
-    table_name: ClassVar[Literal["file_table"]] = "file_table"
 
     path: Annotated[
         str,
@@ -84,30 +69,18 @@ class FileSystemTable(TableSpec):
     It contains a dict of xattr names and xattr values.
     """
 
-    digest: Annotated[
-        Optional[bytes],
-        TypeAffinityRepr(bytes),
-        SkipValidation,
-    ] = None
-    """If not None, the digest of this regular file."""
-
     contents: Annotated[
         Optional[bytes],
         TypeAffinityRepr(bytes),
         SkipValidation,
     ] = None
-    """The contents of the file.
+    """The contents of the file. Currently only used by symlink.
     
-    When is regular, <contents> is the file's contents.
     When is symlink, <contents> is the symlink target.
-    When is char, <contents> is a comma separate pair of int,
-        which is the the major and minor dev num.
-        In most cases, it should be 0,0, which is the whiteout file
-        of overlayfs.
     """
 
-    def _set_xattr(self, _target: Path) -> None:
-        """NOTE: this method always not following symlink."""
+    def _set_xattr(self, _target: StrOrPath) -> None:
+        """NOTE: this method always don't follow symlink."""
         if xattrs := self.xattrs:
             for k, v in xattrs.items():
                 os.setxattr(
@@ -117,63 +90,94 @@ class FileSystemTable(TableSpec):
                     follow_symlinks=False,
                 )
 
-    def copy_to_target(
-        self,
-        target_mnt: Path,
-        *,
-        resource_dir: Path,
-        ctx: HardlinkRegister,
+    def _set_perm(self, _target: StrOrPath) -> None:
+        """NOTE: this method always don't follow symlink."""
+        inode_table = self.inode
+        # NOTE: changing mode of symlink is not needed and uneffective, and on some platform
+        #   changing mode of symlink will even result in exception raised.
+        if not stat.S_ISLNK(inode_table.mode):
+            os.chmod(_target, mode=inode_table.mode, follow_symlinks=False)
+        os.chown(
+            _target, uid=inode_table.uid, gid=inode_table.gid, follow_symlinks=False
+        )
+
+
+class FileTableRegularFiles(TableSpec, FileTableBase):
+    """DB table for regular file entries."""
+
+    table_name: ClassVar[str] = "ft_regular"
+
+    digest: Annotated[
+        bytes,
+        TypeAffinityRepr(bytes),
+        SkipValidation,
+    ]
+
+    def prepare_target(
+        self, _rs: StrOrPath, *, target_mnt: StrOrPath, prepare_method: PrepareMethod
     ) -> None:
         _canonical_path = Path(self.path)
-        _relative_path = target_mnt / _canonical_path.relative_to(CANONICAL_ROOT)
+        _target_on_mnt = Path(target_mnt) / _canonical_path.relative_to(CANONICAL_ROOT)
+
+        if prepare_method == "copy":
+            shutil.copy(_rs, _target_on_mnt)
+            self._set_perm(_target_on_mnt)
+            self._set_xattr(_target_on_mnt)
+
+        elif prepare_method == "hardlink":
+            # NOTE: os.link will make dst a hardlink to src.
+            os.link(_rs, _target_on_mnt)
+            # NOTE: although we actually don't need to set_perm and set_xattr everytime
+            #   to file paths point to the same inode, for simplicity here we just
+            #   do it everytime.
+            self._set_perm(_target_on_mnt)
+            self._set_xattr(_target_on_mnt)
+
+        elif prepare_method == "move":
+            shutil.move(str(_rs), _target_on_mnt)
+            self._set_perm(_target_on_mnt)
+            self._set_xattr(_target_on_mnt)
+
+
+class FileTableNonRegularFiles(TableSpec, FileTableBase):
+    """DB table for non-regular file entries.
+
+    This includes:
+    1. directory.
+    2. symlink.
+    3. chardev file.
+
+    NOTE that support for chardev file is only for overlayfs' whiteout file,
+        so only device num as 0,0 will be allowed.
+    """
+
+    table_name: ClassVar[str] = "ft_non_regular"
+
+    def prepare_target(self, *, target_mnt: StrOrPath) -> None:
+        _canonical_path = Path(self.path)
+        _target_on_mnt = Path(target_mnt) / _canonical_path.relative_to(CANONICAL_ROOT)
 
         inode_table = self.inode
         _mode = inode_table.mode
-        _uid, _gid = inode_table.uid, inode_table.gid
-
         if stat.S_ISDIR(_mode):
-            _relative_path.mkdir(exist_ok=True, parents=True)
-            os.chmod(_relative_path, mode=_mode, follow_symlinks=False)
-            os.chown(_relative_path, uid=_uid, gid=_gid, follow_symlinks=False)
-            self._set_xattr(_relative_path)
+            _target_on_mnt.mkdir(exist_ok=True, parents=True)
+            self._set_perm(_target_on_mnt)
+            self._set_xattr(_target_on_mnt)
+            return
 
-        elif stat.S_ISREG(_mode) and self.digest:
-            _resource = resource_dir / self.digest.hex()
+        if stat.S_ISLNK(_mode):
+            assert (_symlink_target_raw := self.contents), f"invalid entry {self}"
+            _symlink_target = _symlink_target_raw.decode()
+            _target_on_mnt.symlink_to(_symlink_target)
+            self._set_perm(_target_on_mnt)
+            self._set_xattr(_target_on_mnt)
+            return
 
-            # hardlinked file, identify hardlink group by original inode number
-            if inode := inode_table.inode:
-                _first_one, _first_copy = ctx.get_first_copy_fpath(inode, self.path)
-
-                if _first_one:
-                    shutil.copy(_resource, _relative_path)
-                    os.chmod(_relative_path, mode=_mode, follow_symlinks=False)
-                    os.chown(_relative_path, uid=_uid, gid=_gid, follow_symlinks=False)
-                    self._set_xattr(_relative_path)
-                else:
-                    os.link(_first_copy, _relative_path)
-            # normal regular file with nlink==1
-            else:
-                shutil.copy(_resource, _relative_path)
-                os.chmod(_relative_path, mode=_mode, follow_symlinks=False)
-                os.chown(_relative_path, uid=_uid, gid=_gid, follow_symlinks=False)
-                self._set_xattr(_relative_path)
-
-        elif stat.S_ISLNK(_mode) and self.contents:
-            _symlink_target = self.contents.decode()
-            _relative_path.symlink_to(_symlink_target)
-            # NOTE: changing mode of symlink is not needed and uneffective
-            os.chown(_relative_path, uid=_uid, gid=_gid, follow_symlinks=False)
-            self._set_xattr(_relative_path)
-
-        elif stat.S_ISCHR(_mode):
-            # NOTE(20241206): although we have the major/minor recorded in contents field,
-            #   we always force to only use 0,0 as device num, which is used as whiteout
-            #   file for the overlayfs.
+        if stat.S_ISCHR(_mode):
             _device_num = os.makedev(0, 0)
-            os.mknod(_relative_path, mode=_mode, device=_device_num)
-            os.chmod(_relative_path, mode=_mode, follow_symlinks=False)
-            os.chmod(_relative_path, mode=_mode, follow_symlinks=False)
-            self._set_xattr(_relative_path)
+            os.mknod(_target_on_mnt, mode=_mode, device=_device_num)
+            self._set_perm(_target_on_mnt)
+            self._set_xattr(_target_on_mnt)
+            return
 
-        else:
-            raise ValueError(f"invalid entry: {self}")
+        raise ValueError(f"invalid entry {self}")
