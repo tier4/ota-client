7073ce4043f0c73248f26404e327403ac63db347
bodong.yang <bodong.yang@tier4.jp>
2024-12-17 09:50:56 +0000

    file_table.types: still do the serialization and validation

--- src/ota_metadata/file_table/_types.py
+++ src/ota_metadata/file_table/_types.py
@@ -18,6 +18,8 @@ from __future__ import annotations
 from typing import Generator, NamedTuple, Optional
 
 from msgpack import Unpacker, packb
+from pydantic import PlainSerializer, PlainValidator
+from typing_extensions import Annotated
 
 FILE_ENTRY_MAX_SIZE = 1024**2  # 1MiB
 
@@ -35,30 +37,32 @@ class FileEntryAttrs(NamedTuple):
         if self.xattrs:
             yield from self.xattrs.items()
 
-    def pack(self) -> bytes:
-        return pack_entry_attrs(self)
-
     @classmethod
-    def unpack(cls, _in: bytes) -> "FileEntryAttrs":
-        return parse_packed_entry_attrs(_in)
+    def _validator(cls, _in: bytes | FileEntryAttrs) -> FileEntryAttrs:
+        if isinstance(_in, FileEntryAttrs):
+            return _in
+
+        _unpacker = Unpacker(max_buffer_size=FILE_ENTRY_MAX_SIZE)
+        _unpacker.feed(_in)  # feed all the data into the internal buffer
+
+        # get exactly one list from buffer.
+        # NOTE that msgpack only has two container types when unpacking: list and dict.
+        _obj = _unpacker.unpack()
+        if not isinstance(_obj, list):
+            raise TypeError(f"expect unpack to a list, get {type(_obj)=}")
+        return cls(*_obj)
+
+    def _serializer(self: FileEntryAttrs) -> bytes:
+        try:
+            if _res := packb(self, buf_size=FILE_ENTRY_MAX_SIZE):
+                return _res
+            raise ValueError("nothing is packed")
+        except Exception as e:
+            raise ValueError(f"failed to pack {self}: {e!r}") from e
 
 
-def parse_packed_entry_attrs(_in: bytes) -> FileEntryAttrs:
-    _unpacker = Unpacker(max_buffer_size=FILE_ENTRY_MAX_SIZE)
-    _unpacker.feed(_in)  # feed all the data into the internal buffer
-
-    # get exactly one list from buffer.
-    # NOTE that msgpack only has two container types when unpacking: list and dict.
-    _obj = _unpacker.unpack()
-    if not isinstance(_obj, list):
-        raise TypeError(f"expect unpack to a list, get {type(_obj)=}")
-    return FileEntryAttrs(*_obj)
-
-
-def pack_entry_attrs(_in: FileEntryAttrs) -> bytes:
-    try:
-        if _res := packb(_in, buf_size=FILE_ENTRY_MAX_SIZE):
-            return _res
-        raise ValueError("nothing is packed")
-    except Exception as e:
-        raise ValueError(f"failed to pack {_in}: {e!r}") from e
+EntryAttrsType = Annotated[
+    FileEntryAttrs,
+    PlainValidator(FileEntryAttrs._validator),
+    PlainSerializer(FileEntryAttrs._serializer),
+]
