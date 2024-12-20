42e190661da79c970386dd8e4f4dad7ca2feb343
Bodong Yang <86948717+Bodong-Yang@users.noreply.github.com>
2024-12-20 10:55:04 +0900

    feat: use file_table and resource_table as sqlite3 db (#460)
    This PR introduces file_table as sqlite3 db with three tables, naming as follow:
    1. `ft_regular`: table contains all regular file entries.
    2. `ft_non_regular`: table contains all non-regular file entries, including symlinks and chardev(with 0,0 devnum).
    3. `ft_dir`: table contains all directory entries.
    
    Also a new resource_table sqlite3 db with one table is introduced:
    1. `rs_table`: table contains all the unique file entries(by sha256digest) and the necessary metadata of the corresponding resource.
    
    The [simple-sqlite3-orm](https://github.com/pga2rn/simple-sqlite3-orm) python package(which is developed and maintained by me also) is used to implement the table definitions and ORMs.
    
    For each tables definition(in `file_table._table` module), helper methods to prepare the entries onto the target(standby slot mount point during OTA) are implemented. 
    Comparing to previous implementation, xattrs and chardev files(required by supporting storing docker images on system image) are supported now.

---
+++ src/ota_metadata/legacy/rs_table.py
@@ -0,0 +1,100 @@
+# Copyright 2022 TIER IV, INC. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Resource table implementation for legacy OTA image."""
+
+
+from __future__ import annotations
+
+import random
+from typing import Any, ClassVar, Generator, Literal, Optional
+
+from pydantic import SkipValidation
+from simple_sqlite3_orm import (
+    ConstrainRepr,
+    ORMBase,
+    ORMThreadPoolBase,
+    TableSpec,
+    TypeAffinityRepr,
+)
+from typing_extensions import Annotated, Self
+
+RSTABLE_NAME = "rs_table"
+
+
+class ResourceTable(TableSpec):
+    schema_ver: ClassVar[Literal[1]] = 1
+
+    digest: Annotated[
+        bytes,
+        TypeAffinityRepr(bytes),
+        ConstrainRepr("PRIMARY KEY"),
+        SkipValidation,
+    ]
+    """sha256 digest of the original file."""
+
+    path: Annotated[
+        Optional[str],
+        TypeAffinityRepr(str),
+        SkipValidation,
+    ] = None
+    """NOTE: only for resource without zstd compression."""
+
+    original_size: Annotated[
+        int,
+        TypeAffinityRepr(int),
+        ConstrainRepr("NOT NULL"),
+        SkipValidation,
+    ]
+    """The size of the plain uncompressed resource."""
+
+    compression_alg: Annotated[
+        Optional[str],
+        TypeAffinityRepr(str),
+        SkipValidation,
+    ] = None
+    """The compression algorthim used to compressed the resource.
+
+    NOTE that this field should be None if <contents> is not None.
+    """
+
+    def __eq__(self, other: Any | Self) -> bool:
+        return isinstance(other, self.__class__) and self.digest == other.digest
+
+    def __hash__(self) -> int:
+        return hash(self.digest)
+
+
+class RSTORM(ORMBase[ResourceTable]):
+
+    _orm_table_name = RSTABLE_NAME
+
+    def iter_all_with_shuffle(self, *, batch_size: int) -> Generator[ResourceTable]:
+        """Iter all entries with seek method by rowid, shuffle each batch before yield.
+
+        NOTE: the target table must has rowid defined!
+        """
+        _this_batch = []
+        for _entry in self.orm_select_all_with_pagination(batch_size=batch_size):
+            _this_batch.append(_entry)
+            if len(_this_batch) >= batch_size:
+                random.shuffle(_this_batch)
+                yield from _this_batch
+                _this_batch = []
+        random.shuffle(_this_batch)
+        yield from _this_batch
+
+
+class RSTableORMThreadPool(ORMThreadPoolBase[ResourceTable]):
+
+    _orm_table_name = RSTABLE_NAME
