6016151b846c3e0138d725165f889abd1d6ae593
bodong.yang <bodong.yang@tier4.jp>
2025-02-05 07:10:19 +0000

    file_table: implement iter_common_by_digest

--- src/ota_metadata/file_table/_orm.py
+++ src/ota_metadata/file_table/_orm.py
@@ -15,6 +15,9 @@
 
 from __future__ import annotations
 
+from functools import partial
+from typing import Generator
+
 from simple_sqlite3_orm import ORMBase, ORMThreadPoolBase
 
 from ._table import (
@@ -31,6 +34,35 @@ FT_DIR_TABLE_NAME = "ft_dir"
 class FileTableRegularORM(ORMBase[FileTableRegularFiles]):
 
     _orm_table_name = FT_REGULAR_TABLE_NAME
+
+    def iter_common_by_digest(self, other_db: str) -> Generator[FileTableRegularFiles]:
+        """Yield entries in this ft which digest presented on <other_db>.ft_table.
+
+        This is for assisting faster delta_calculation without full disk scan.
+        """
+
+        attached_db_schema = "base"
+        stmt = """\
+        SELECT d2.*
+        FROM base.ft_regular AS d2
+        INNER JOIN (
+            SELECT digest
+            FROM main.ft_regular
+            GROUP BY digest
+        ) AS d1 ON d2.digest = d1.digest;
+        """
+        orm_conn = self.orm_con
+
+        orm_conn.execute(f"ATTACH {other_db} AS {attached_db_schema};")
+        try:
+            with orm_conn:
+                cur = orm_conn.execute(stmt)
+                cur.row_factory = partial(
+                    self.orm_table_spec.table_row_factory2, validation=False
+                )
+                yield from cur
+        finally:
+            orm_conn.execute(f"DETACH DATABASE {attached_db_schema};")
 
 
 class FileTableRegularORMPool(ORMThreadPoolBase[FileTableRegularFiles]):
