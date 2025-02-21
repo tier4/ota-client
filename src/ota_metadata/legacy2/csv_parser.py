f2b6ce90509542b7163f45d56764eb39c45a68d9
bodong.yang <bodong.yang@tier4.jp>
2025-02-21 08:35:48 +0000

    update csv_parser accordingly

--- src/ota_metadata/legacy2/csv_parser.py
+++ src/ota_metadata/legacy2/csv_parser.py
@@ -19,6 +19,7 @@ from __future__ import annotations
 import logging
 import re
 import stat
+from typing import NamedTuple
 
 from ota_metadata.file_table import (
     FileEntryAttrs,
@@ -29,6 +30,8 @@ from ota_metadata.file_table import (
     FileTableRegularFiles,
     FileTableRegularORM,
 )
+from ota_metadata.file_table._orm import FileTableResourceORM
+from ota_metadata.file_table._table import FileTableResource
 from otaclient_common._typing import StrOrPath
 
 from .rs_table import ResourceTable, ResourceTableORM
@@ -165,7 +168,7 @@ REGINF_CSV_PATTERN = re.compile(
 )
 
 
-def parse_regulars_csv_line(line: str) -> tuple[FileTableRegularFiles, ResourceTable]:
+def parse_regular_csv_line(line: str) -> re.Match:
     """Regular file entry's CSV pattern.
 
     Note that <compressed_alg> only has `zst` now.
@@ -178,7 +181,25 @@ def parse_regulars_csv_line(line: str) -> tuple[FileTableRegularFiles, ResourceT
     """
     _ma = REGINF_CSV_PATTERN.match(line.strip())
     assert _ma is not None, f"matching reg_inf failed for {line}"
+    return _ma
 
+
+class DigestSize(NamedTuple):
+    digest: bytes
+    size: int
+
+
+def parser_create_file_table_rs_entry(_ma: re.Match) -> DigestSize:
+    sha256hash = bytes.fromhex(_ma.group("hash"))
+    size = 0
+    if _size := _ma.group("size"):
+        size = int(_size)
+    return DigestSize(sha256hash, size)
+
+
+def parse_create_file_table_row(
+    _ma: re.Match, resource_id: int
+) -> tuple[FileTableRegularFiles, ResourceTable]:
     # NOTE: the ota-metadata generator strips away the file type bits.
     mode = int(_ma.group("mode"), 8) | stat.S_IFREG
     uid = int(_ma.group("uid"))
@@ -207,7 +228,7 @@ def parse_regulars_csv_line(line: str) -> tuple[FileTableRegularFiles, ResourceT
             size=size,
             inode=inode,
         ),
-        digest=sha256hash,
+        resource_id=resource_id,
     )
 
     # NOTE: in rev4 of OTA image metadata we add compression support, and compressed version of resource
@@ -227,25 +248,36 @@ def parse_regulars_csv_line(line: str) -> tuple[FileTableRegularFiles, ResourceT
         compression_alg=compressed_alg,
         original_size=size or 0,
     )
-
     return _new, _new_rs
 
 
 def parse_regulars_from_csv_file(
     _fpath: StrOrPath,
     _orm: FileTableRegularORM,
+    _orm_ft_resource: FileTableResourceORM,
     _orm_rs: ResourceTableORM,
 ) -> int:
     """Compatibility to the plaintext regulars.txt."""
+    digest_resources: dict[DigestSize, int] = {}
+
     _batch, _batch_rs, _batch_cnt = [], [], 0
     with open(_fpath, "r") as f:
-        _idx = 0
-        for _idx, line in enumerate(f, start=1):
-            _new, _new_rs = parse_regulars_csv_line(line)
+        regular_file_entry_count = 0
+        for regular_file_entry_count, line in enumerate(f, start=1):
+            _line_match = parse_regular_csv_line(line)
+            _digest_size = parser_create_file_table_rs_entry(_line_match)
+            _resource_id = digest_resources.setdefault(
+                _digest_size, len(digest_resources)
+            )
+
+            _new, _new_rs = parse_create_file_table_row(_line_match, _resource_id)
+
             _batch.append(_new)
             _batch_rs.append(_new_rs)
 
-            if (_this_batch := _idx // ENTRIES_PROCESS_BATCH_SIZE) > _batch_cnt:
+            if (
+                _this_batch := regular_file_entry_count // ENTRIES_PROCESS_BATCH_SIZE
+            ) > _batch_cnt:
                 _batch_cnt = _this_batch
                 _orm.orm_insert_entries(_batch)
                 # NOTE: ignore entries with same digest
@@ -255,7 +287,16 @@ def parse_regulars_from_csv_file(
         _orm.orm_insert_entries(_batch)
         _orm_rs.orm_insert_entries(_batch_rs, or_option="ignore")
 
-        return _idx
+    # prepare the ft_resource
+    _orm_ft_resource.orm_insert_entries(
+        (
+            FileTableResource(
+                resource_id=_rs_id, digest=_digest_size.digest, size=_digest_size.size
+            )
+            for _digest_size, _rs_id in digest_resources.items()
+        )
+    )
+    return regular_file_entry_count
 
 
 #
