f6884ae25df3fef7804cd815fb8f7e790a5de50e
bodong.yang <bodong.yang@tier4.jp>
2025-02-21 07:06:28 +0000

    update file_table schema, add ft_resource; use ft_resource to optimize the iter_common_by_digest

--- src/ota_metadata/file_table/_orm.py
+++ src/ota_metadata/file_table/_orm.py
@@ -17,10 +17,11 @@ from __future__ import annotations
 
 import logging
 import sqlite3
-from typing import Generator
+from typing import Any, Generator, NamedTuple
 
 from simple_sqlite3_orm import CreateIndexParams, ORMBase, ORMThreadPoolBase
 from simple_sqlite3_orm.utils import wrap_value
+from typing_extensions import Self
 
 from otaclient_common import EMPTY_FILE_SHA256_BYTE
 
@@ -28,6 +29,7 @@ from ._table import (
     FileTableDirectories,
     FileTableNonRegularFiles,
     FileTableRegularFiles,
+    FileTableResource,
 )
 
 logger = logging.getLogger(__name__)
@@ -35,7 +37,19 @@ logger = logging.getLogger(__name__)
 FT_REGULAR_TABLE_NAME = "ft_regular"
 FT_NON_REGULAR_TABLE_NAME = "ft_non_regular"
 FT_DIR_TABLE_NAME = "ft_dir"
+FT_RESOURCE_TABLE_NAME = "ft_resource"
 MAX_ENTRIES_PER_DIGEST = 10
+
+
+class FileEntryToScan(NamedTuple):
+
+    path: str
+    digest: bytes
+    size: int
+
+    @classmethod
+    def row_factory(cls, _cursor, _row: tuple[Any, ...] | Any) -> Self:
+        return cls(*_row)
 
 
 class FileTableRegularORM(ORMBase[FileTableRegularFiles]):
@@ -47,7 +61,7 @@ class FileTableRegularORM(ORMBase[FileTableRegularFiles]):
 
     def iter_common_by_digest(
         self, other_db: str, *, max_entries_per_digest: int = MAX_ENTRIES_PER_DIGEST
-    ) -> Generator[FileTableRegularFiles]:
+    ) -> Generator[FileEntryToScan]:
         """Yield entries from <other_db>.ft_table which digest presented in this ft.
 
         This is for assisting faster delta_calculation without full disk scan.
@@ -60,28 +74,40 @@ class FileTableRegularORM(ORMBase[FileTableRegularFiles]):
             )
 
             stmt = f"""\
-            SELECT d2.*
-            FROM base.{FT_REGULAR_TABLE_NAME} AS d2
-            INNER JOIN (
-                SELECT digest
-                FROM main.{FT_REGULAR_TABLE_NAME}
+            WITH common_digests AS (
+                SELECT db1.digest
+                FROM {FT_RESOURCE_TABLE_NAME} AS db1
+                INNER JOIN base.{FT_RESOURCE_TABLE_NAME} AS db2 USING(digest)
                 WHERE digest != {wrap_value(EMPTY_FILE_SHA256_BYTE)}
-                GROUP BY digest
-            ) AS d1 USING(digest) ORDER BY digest;
+            )
+            SELECT {FT_REGULAR_TABLE_NAME}.path, {FT_RESOURCE_TABLE_NAME}.digest, {FT_RESOURCE_TABLE_NAME}.size
+            FROM {FT_REGULAR_TABLE_NAME}
+            JOIN {FT_RESOURCE_TABLE_NAME} ON {FT_REGULAR_TABLE_NAME}.resource_id = {FT_RESOURCE_TABLE_NAME}.resource_id
+            JOIN common_digests ON {FT_RESOURCE_TABLE_NAME}.digest = common_digests.digest
+            ORDER BY {FT_RESOURCE_TABLE_NAME}.digest;
             """
         else:
             stmt = f"""\
-            WITH ranked AS (
-                SELECT d2.*, ROW_NUMBER() OVER (PARTITION BY d2.digest) AS rown
-                FROM base.{FT_REGULAR_TABLE_NAME} AS d2
-                INNER JOIN (
-                    SELECT digest
-                    FROM main.{FT_REGULAR_TABLE_NAME}
-                    WHERE digest != {wrap_value(EMPTY_FILE_SHA256_BYTE)}
-                    GROUP BY digest
-                ) AS d1 USING(digest) ORDER BY digest
+            WITH common_digests AS (
+                SELECT db1.digest
+                FROM {FT_RESOURCE_TABLE_NAME} AS db1
+                INNER JOIN base.{FT_RESOURCE_TABLE_NAME} AS db2 USING(digest)
+                WHERE digest != {wrap_value(EMPTY_FILE_SHA256_BYTE)}
+            ), ranked_results AS (
+                SELECT 
+                    {FT_REGULAR_TABLE_NAME}.path, 
+                    {FT_RESOURCE_TABLE_NAME}.digest, 
+                    {FT_RESOURCE_TABLE_NAME}.size,
+                    ROW_NUMBER() OVER (PARTITION BY {FT_RESOURCE_TABLE_NAME}.digest ORDER BY {FT_RESOURCE_TABLE_NAME}.path) AS row_num
+                FROM {FT_REGULAR_TABLE_NAME}
+                JOIN {FT_RESOURCE_TABLE_NAME} USING(resource_id)
+                JOIN common_digests USING(digest)
             )
-            SELECT * FROM ranked WHERE rown <= {max_entries_per_digest};
+
+            SELECT path, digest, size
+            FROM ranked_results
+            WHERE row_num <= {max_entries_per_digest}
+            ORDER BY digest;
             """
         orm_conn = self.orm_con
 
@@ -89,7 +115,7 @@ class FileTableRegularORM(ORMBase[FileTableRegularFiles]):
         try:
             with orm_conn:
                 cur = orm_conn.execute(stmt)
-                cur.row_factory = self.orm_table_spec.table_row_factory2
+                cur.row_factory = FileEntryToScan.row_factory
                 yield from cur
         finally:
             orm_conn.execute(f"DETACH DATABASE {attached_db_schema};")
@@ -113,3 +139,11 @@ class FileTableDirORM(ORMBase[FileTableDirectories]):
 class FileTableDirORMPool(ORMThreadPoolBase[FileTableDirectories]):
 
     orm_bootstrap_table_name = FT_DIR_TABLE_NAME
+
+
+class FileTableResourceORM(ORMBase[FileTableResource]):
+
+    orm_bootstrap_table_name = FT_RESOURCE_TABLE_NAME
+    orm_bootstrap_indexes_params = [
+        CreateIndexParams(index_name="digest_index", index_cols=("digest",))
+    ]
