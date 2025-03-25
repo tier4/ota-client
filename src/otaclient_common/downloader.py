9ccc7388311d63f21a8e6fb19d0dd3134931901d
Keisuke Nakata <keisuke.nakata@tier4.jp>
2025-03-25 17:40:28 +0900

    temp

--- src/otaclient_common/downloader.py
+++ src/otaclient_common/downloader.py
@@ -401,7 +401,11 @@ class Downloader:
         err_count, downloaded_file_size, traffic_on_wire = 0, 0, 0
 
         with self._session.get(
-            prepared_url, stream=True, headers=prepared_headers, timeout=timeout
+            prepared_url,
+            stream=True,
+            headers=prepared_headers,
+            timeout=timeout,
+            verify=False,
         ) as resp, open(dst, "wb") as dst_fp:
             resp.raise_for_status()
 
