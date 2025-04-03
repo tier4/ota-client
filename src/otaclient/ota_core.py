b69c20e703cf395e4395630ebfaa0ce62788785f
Keisuke Nakata <keisuke.nakata@tier4.jp>
2025-04-03 10:13:12 +0900

    fix: reverted unnecessary changes in ota_core

--- src/otaclient/ota_core.py
+++ src/otaclient/ota_core.py
@@ -59,7 +59,6 @@ from otaclient._status_monitor import (
     UpdateProgressReport,
 )
 from otaclient._types import (
-    ClientUpdateRequestV2,
     FailureType,
     IPCRequest,
     IPCResEnum,
@@ -963,14 +962,6 @@ class OTAClient:
                 failure_type=e.failure_type,
             )
 
-    def client_update(self, request: ClientUpdateRequestV2) -> None:
-        """
-        NOTE that client update API will not raise any exceptions. The failure information
-            is available via status API.
-        """
-        # TODO: implement core part later
-        pass
-
     def rollback(self, request: RollbackRequestV2) -> None:
         self._live_ota_status = OTAStatus.ROLLBACKING
         new_session_id = request.session_id
@@ -1035,24 +1026,6 @@ class OTAClient:
                     name="ota_update_executor",
                 )
                 _update_thread.start()
-
-                resp_queue.put_nowait(
-                    IPCResponse(
-                        res=IPCResEnum.ACCEPT,
-                        session_id=request.session_id,
-                    )
-                )
-                _allow_request_after = _now + HOLD_REQ_HANDLING_ON_ACK_REQUEST
-
-            elif isinstance(request, ClientUpdateRequestV2):
-
-                _client_update_thread = threading.Thread(
-                    target=self.client_update,
-                    args=[request],
-                    daemon=True,
-                    name="ota_client_update_executor",
-                )
-                _client_update_thread.start()
 
                 resp_queue.put_nowait(
                     IPCResponse(
