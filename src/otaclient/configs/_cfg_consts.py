4d8293ff9fa051208d30f36558db770906064ad8
Keisuke Nakata <keisuke.nakata@tier4.jp>
2025-05-07 20:53:45 +0900

    fix

--- src/otaclient/configs/_cfg_consts.py
+++ src/otaclient/configs/_cfg_consts.py
@@ -55,8 +55,6 @@ class Consts:
     STANDBY_SLOT_MNT = "/run/otaclient/mnt/standby_slot"
     # mount point for downloaded otaclient
     DYNAMIC_CLIENT_MNT = "/run/otaclient/mnt/dynamic_otaclient"
-    # mount point for original rootfs
-    ORIGINAL_ROOT_MNT = "/run/otaclient/mnt/dynamic_otaclient/original_root"
 
     OTA_TMP_STORE = "/.ota-tmp"
     """tmp store for local copy, located at standby slot."""
