0bb32787a12577fb5088be032f300991d741cc2e
Keisuke Nakata <keisuke.nakata@tier4.jp>
2025-05-19 23:03:19 +0900

    tmp

--- src/otaclient/main.py
+++ src/otaclient/main.py
@@ -30,13 +30,11 @@ import threading
 import time
 from functools import partial
 
-import psutil
-
 from otaclient import __version__
 from otaclient._types import ClientUpdateControlFlags, MultipleECUStatusFlags
 from otaclient._utils import SharedOTAClientStatusReader, SharedOTAClientStatusWriter
 from otaclient.configs.cfg import cfg, ecu_info, proxy_info
-from otaclient_common import _env, cmdhelper
+from otaclient_common import _env
 from otaclient_common.common import subprocess_call
 from otaclient_common.linux import subprocess_popen_wrapper
 
@@ -99,6 +97,9 @@ def _dynamic_client_shutdown() -> None:
         # in dynamic client environment, do not shutdown the dynamic client
         return
 
+    # umount paths related to dynamic client
+    subprocess_call(f"umount -R {cfg.DYNAMIC_CLIENT_MNT}", raise_exception=False)
+
     # kill the dynamic client process if it is running
     global _dynamic_client_p
     if _dynamic_client_p and _dynamic_client_p.poll() is None:
@@ -108,31 +109,6 @@ def _dynamic_client_shutdown() -> None:
             print(f"failed to kill dynamic client process group: {e}")
         _dynamic_client_p.wait()
         _dynamic_client_p = None
-
-    mnt_base = cfg.DYNAMIC_CLIENT_MNT
-    _cmd = ["fuser", "-k", "-m", mnt_base]
-    try:
-        subprocess_call(_cmd, raise_exception=False)
-        time.sleep(1)  # Give processes time to exit
-    except Exception as e:
-        logger.warning(f"Failed to kill processes using mount: {e}")
-
-    # umount paths related to dynamic client from the longest to the shortest
-    mounts = [p.mountpoint for p in psutil.disk_partitions(all=True)]
-    targets = sorted(
-        [mnt for mnt in mounts if mnt.startswith(mnt_base)],
-        key=len,
-        reverse=True,
-    )
-    for mnt in targets:
-        try:
-            cmdhelper.ensure_umount(
-                mnt,
-                ignore_error=False,
-                max_retry=1,
-            )
-        except Exception:
-            subprocess_call(f"umount -l {mnt}", raise_exception=False)
 
     logger.info("dynamic client shutdown completed.")
 
