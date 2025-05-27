40e8cb8ec30973a3b2463ea9451efdd7ee71e8c3
bodong.yang <bodong.yang@tier4.jp>
2025-05-27 03:01:42 +0000

    fix test docker base

--- docker/test_base/entry_point.sh
+++ docker/test_base/entry_point.sh
@@ -11,6 +11,6 @@ cp -R "${OTACLIENT_SRC}" "${TEST_ROOT}"
 # exec the input params
 echo "execute test with coverage"
 cd ${TEST_ROOT}
-uv run --python python3 --no-managed-python pytest --junit-xml=${OUTPUT_DIR}/pytest.xml ${@:-}
+uv run --python python3 --no-managed-python coverage run -m pytest --junit-xml=${OUTPUT_DIR}/pytest.xml ${@:-}
 uv run --python python3 --no-managed-python coverage combine
 uv run --python python3 --no-managed-python coverage xml -o ${OUTPUT_DIR}/coverage.xml
