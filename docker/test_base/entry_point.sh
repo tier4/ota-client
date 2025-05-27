5f534536e36b502e3b7d4f4c84b6839a7fd53290
Bodong Yang <86948717+Bodong-Yang@users.noreply.github.com>
2025-05-27 16:18:17 +0900

    chore(dev): add devcontainer config, introduce to use uv for deps management with lock file (#530)
    This PR introduces a reproducible python dev environment for otaclient development with the following features:
    
    1. introduce out-of-box ready-to-use devcontainer environment (defined at `.devcontainer/devcontainer.json`): 
        a. docker-in-docker: enables running containerized pytest.
        b. python3.8 installation.
        c. uv with virtual env setup.
        d. Basic VSCode extensions for python development:
            - Basic Python extension: ms-python.python, ms-python.vscode-pylance, ms-python.debugpy
            -  Ruff extension for python lint and format.
            - Yaml file support.
    
    2. introduce uv (https://docs.astral.sh/uv/) managed reproducible python virtual environment with lock file.
    
    
    Other changes:
    
    1. pyproject.toml: update to follow pep639, related to license related fields.
    2. pyproject.toml: update to follow pep735, move the dev dependencies into dependency-groups.dev.
    3. pyproject.toml: fix pyright configuration, add exclude rules to exclude virtual envs and temporary tests folder.
    4. test docker image: use uv to setup virtual environment.

--- docker/test_base/entry_point.sh
+++ docker/test_base/entry_point.sh
@@ -11,7 +11,6 @@ cp -R "${OTACLIENT_SRC}" "${TEST_ROOT}"
 # exec the input params
 echo "execute test with coverage"
 cd ${TEST_ROOT}
-hatch env create dev
-hatch run dev:coverage run -m pytest --junit-xml=${OUTPUT_DIR}/pytest.xml ${@:-}
-hatch run dev:coverage combine
-hatch run dev:coverage xml -o ${OUTPUT_DIR}/coverage.xml
+uv run --python python3 --no-managed-python coverage run -m pytest --junit-xml=${OUTPUT_DIR}/pytest.xml ${@:-}
+uv run --python python3 --no-managed-python coverage combine
+uv run --python python3 --no-managed-python coverage xml -o ${OUTPUT_DIR}/coverage.xml
