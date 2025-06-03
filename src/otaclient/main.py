09d14be92fef8b3e6118a120fd1dea6766b62bd9
Keisuke Nakata <keisuke.nakata@tier4.jp>
2025-06-03 10:49:00 +0900

    temp

--- src/otaclient/main.py
+++ src/otaclient/main.py
@@ -16,22 +16,14 @@
 
 from __future__ import annotations
 
-import atexit
 import logging
-import multiprocessing as mp
 import multiprocessing.context as mp_ctx
 import multiprocessing.shared_memory as mp_shm
 import os
-import secrets
-import signal
 import sys
-import threading
 import time
-from functools import partial
 
 from otaclient import __version__
-from otaclient._types import ClientUpdateControlFlags, MultipleECUStatusFlags
-from otaclient._utils import SharedOTAClientStatusReader, SharedOTAClientStatusWriter
 
 logger = logging.getLogger(__name__)
 
@@ -52,43 +44,13 @@ _shm: mp_shm.SharedMemory | None = None
 
 
 def _on_shutdown(sys_exit: bool = False) -> None:  # pragma: no cover
-    global _ota_core_p, _grpc_server_p, _shm
-    if _ota_core_p:
-        _ota_core_p.terminate()
-        _ota_core_p.join()
-        _ota_core_p = None
-
-    if _grpc_server_p:
-        _grpc_server_p.terminate()
-        _grpc_server_p.join()
-        _grpc_server_p = None
-
-    if _shm:
-        _shm.close()
-        _shm.unlink()
-        _shm = None
-
-    if sys_exit:
-        sys.exit(1)
-
-
-def _signal_handler(signal_value, _) -> None:  # pragma: no cover
-    print(f"otaclient receives {signal_value=}, shutting down ...")
-    # NOTE: the daemon_process needs to exit also.
-    _on_shutdown(sys_exit=True)
+    pass
 
 
 def main() -> None:  # pragma: no cover
     from otaclient._logging import configure_logging
-    from otaclient._otaproxy_ctx import (
-        otaproxy_control_thread,
-        otaproxy_on_global_shutdown,
-    )
-    from otaclient._utils import check_other_otaclient, create_otaclient_rundir
     from otaclient.client_package import OTAClientPackagePrepareter
     from otaclient.configs.cfg import cfg, ecu_info, proxy_info
-    from otaclient.grpc.api_v2.main import grpc_server_process
-    from otaclient.ota_core import ota_core_process
     from otaclient_common import _env
 
     # configure logging before any code being executed
@@ -107,6 +69,7 @@ def main() -> None:  # pragma: no cover
 
     if _env.is_dynamic_client_preparing() and not _env.is_dynamic_client_running():
         try:
+            logger.info("in dynamic client preparation process ...")
             OTAClientPackagePrepareter().mount_client_package()
 
             running_env = os.environ.copy()
@@ -121,126 +84,19 @@ def main() -> None:  # pragma: no cover
             logger.exception(f"Failed during dynamic client preparation: {e}")
             return _on_shutdown(sys_exit=True)
 
-    check_other_otaclient(cfg.OTACLIENT_PID_FILE)
-    create_otaclient_rundir(cfg.RUN_DIR)
-
-    #
-    # ------ start each processes ------ #
-    #
-    global _ota_core_p, _grpc_server_p, _shm
-
-    # NOTE: if the atexit hook is triggered by signal received,
-    #   first the signal handler will be executed, and then atexit hook.
-    #   At the time atexit hook is executed, the _ota_core_p, _grpc_server_p
-    #   and _shm are set to None by signal handler.
-    atexit.register(_on_shutdown)
-    signal.signal(signal.SIGTERM, _signal_handler)
-    signal.signal(signal.SIGINT, _signal_handler)
-
-    mp_ctx = mp.get_context("spawn")
-    _shm = mp_shm.SharedMemory(size=STATUS_SHM_SIZE, create=True)
-    _key = secrets.token_bytes(SHM_HMAC_KEY_LEN)
-
-    # shared queues and flags
-    local_otaclient_op_queue = mp_ctx.Queue()
-    local_otaclient_resp_queue = mp_ctx.Queue()
-    ecu_status_flags = MultipleECUStatusFlags(
-        any_child_ecu_in_update=mp_ctx.Event(),
-        any_requires_network=mp_ctx.Event(),
-        all_success=mp_ctx.Event(),
-    )
-    client_update_control_flags = ClientUpdateControlFlags(
-        notify_data_ready_event=mp_ctx.Event(),
-        request_shutdown_event=mp_ctx.Event(),
-    )
-
-    _ota_core_p = mp_ctx.Process(
-        target=partial(
-            ota_core_process,
-            shm_writer_factory=partial(
-                SharedOTAClientStatusWriter, name=_shm.name, key=_key
-            ),
-            ecu_status_flags=ecu_status_flags,
-            op_queue=local_otaclient_op_queue,
-            resp_queue=local_otaclient_resp_queue,
-            max_traceback_size=MAX_TRACEBACK_SIZE,
-            client_update_control_flags=client_update_control_flags,
-        ),
-        name="otaclient_ota_core",
-    )
-    _ota_core_p.start()
-
-    _grpc_server_p = mp_ctx.Process(
-        target=partial(
-            grpc_server_process,
-            shm_reader_factory=partial(
-                SharedOTAClientStatusReader, name=_shm.name, key=_key
-            ),
-            op_queue=local_otaclient_op_queue,
-            resp_queue=local_otaclient_resp_queue,
-            ecu_status_flags=ecu_status_flags,
-        ),
-        name="otaclient_api_server",
-    )
-    _grpc_server_p.start()
-
-    del _key
-
-    # ------ setup main process ------ #
-
-    _otaproxy_control_t = None
-    if proxy_info.enable_local_ota_proxy:
-        _otaproxy_control_t = threading.Thread(
-            target=partial(otaproxy_control_thread, ecu_status_flags),
-            daemon=True,
-            name="otaclient_otaproxy_control_t",
-        )
-        _otaproxy_control_t.start()
-
     while True:
         time.sleep(HEALTH_CHECK_INTERVAL)
-
-        if not _ota_core_p.is_alive():
-            logger.error(
-                "ota_core process is dead! "
-                f"otaclient will exit in {SHUTDOWN_AFTER_CORE_EXIT}seconds ..."
-            )
-            time.sleep(SHUTDOWN_AFTER_CORE_EXIT)
-            return _on_shutdown(sys_exit=True)
-
-        if not _grpc_server_p.is_alive():
-            logger.error(
-                f"ota API server is dead, whole otaclient will exit in {SHUTDOWN_AFTER_API_SERVER_EXIT}seconds ..."
-            )
-            time.sleep(SHUTDOWN_AFTER_API_SERVER_EXIT)
-            return _on_shutdown(sys_exit=True)
-
         # launch the dynamic client preparation process
-        if client_update_control_flags.notify_data_ready_event.is_set():
-            try:
-                # kill ota proxy thread if it is running
-                if _otaproxy_control_t and _otaproxy_control_t.is_alive():
-                    logger.info("killing otaproxy control thread ...")
-                    otaproxy_on_global_shutdown()
-                    _otaproxy_control_t.join()
-                # kill other resources except main process
-                logger.info("on main shutdown...")
-                _on_shutdown(sys_exit=False)
-
-                logger.info("execve for dynamic client preparation ...")
-                # Create a copy of the current environment and modify it
-                preparing_env = os.environ.copy()
-                preparing_env[cfg.PREPARING_DOWNLOADED_DYNAMIC_OTA_CLIENT] = "yes"
-                # Execute with the modified environment
-                os.execve(
-                    path=sys.executable,
-                    argv=[sys.executable, __file__],
-                    env=preparing_env,
-                )
-            except Exception as e:
-                logger.exception(f"Failed during dynamic client preparation: {e}")
-                return _on_shutdown(sys_exit=True)
-
-        # shutdown request
-        if client_update_control_flags.request_shutdown_event.is_set():
+        try:
+            logger.info("preparing dynamic client package ...")
+            preparing_env = os.environ.copy()
+            preparing_env[cfg.PREPARING_DOWNLOADED_DYNAMIC_OTA_CLIENT] = "yes"
+            # Execute with the modified environment
+            os.execve(
+                path=sys.executable,
+                argv=[sys.executable, __file__],
+                env=preparing_env,
+            )
+        except Exception as e:
+            logger.exception(f"Failed during dynamic client preparation: {e}")
             return _on_shutdown(sys_exit=True)
