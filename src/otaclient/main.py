5a2f21b9515adfd464418c57b2d9539c7d2416a1
Keisuke Nakata <keisuke.nakata@tier4.jp>
2025-06-03 11:21:21 +0900

    test

--- src/otaclient/main.py
+++ src/otaclient/main.py
@@ -17,72 +17,28 @@
 from __future__ import annotations
 
 import logging
-import multiprocessing.context as mp_ctx
-import multiprocessing.shared_memory as mp_shm
 import os
 import sys
 import time
 
-from otaclient import __version__
-
 logger = logging.getLogger(__name__)
 
-HEALTH_CHECK_INTERVAL = 6  # seconds
-# NOTE: the reason to let daemon_process exits after 16 seconds of ota_core dead
-#   is to allow grpc API server to respond to the status API calls with up-to-date
-#   failure information from ota_core.
-SHUTDOWN_AFTER_CORE_EXIT = 16  # seconds
-SHUTDOWN_AFTER_API_SERVER_EXIT = 3  # seconds
-
-STATUS_SHM_SIZE = 4096  # bytes
-MAX_TRACEBACK_SIZE = 2048  # bytes
-SHM_HMAC_KEY_LEN = 64  # bytes
-
-_ota_core_p: mp_ctx.SpawnProcess | None = None
-_grpc_server_p: mp_ctx.SpawnProcess | None = None
-_shm: mp_shm.SharedMemory | None = None
-
-
-def _on_shutdown(sys_exit: bool = False) -> None:  # pragma: no cover
-    pass
+HEALTH_CHECK_INTERVAL = 2  # seconds
 
 
 def main() -> None:  # pragma: no cover
     from otaclient._logging import configure_logging
-    from otaclient.client_package import OTAClientPackagePrepareter
-    from otaclient.configs.cfg import cfg, ecu_info, proxy_info
-    from otaclient_common import _env
 
     # configure logging before any code being executed
     configure_logging()
 
     logger.info("started")
-    logger.info(f"otaclient version: {__version__}")
-    logger.info(f"ecu_info.yaml: \n{ecu_info}")
-    logger.info(f"proxy_info.yaml: \n{proxy_info}")
-    logger.info(
-        f"env.preparing_downloaded_dynamic_ota_client: {os.getenv(cfg.PREPARING_DOWNLOADED_DYNAMIC_OTA_CLIENT)}"
-    )
-    logger.info(
-        f"env.running_downloaded_dynamic_ota_client: {os.getenv(cfg.RUNNING_DOWNLOADED_DYNAMIC_OTA_CLIENT)}"
-    )
+    logger.info(f"env.preparing_downloaded_dynamic_ota_client: {os.getenv('hoge')}")
 
-    if _env.is_dynamic_client_preparing() and not _env.is_dynamic_client_running():
-        try:
-            logger.info("in dynamic client preparation process ...")
-            OTAClientPackagePrepareter().mount_client_package()
-
-            running_env = os.environ.copy()
-            del running_env[cfg.PREPARING_DOWNLOADED_DYNAMIC_OTA_CLIENT]
-            running_env[cfg.RUNNING_DOWNLOADED_DYNAMIC_OTA_CLIENT] = "yes"
-            os.execve(
-                path=sys.executable,
-                argv=[sys.executable, __file__],
-                env=running_env,
-            )
-        except Exception as e:
-            logger.exception(f"Failed during dynamic client preparation: {e}")
-            return _on_shutdown(sys_exit=True)
+    if os.getenv("hoge") == "yes":
+        logger.info("in dynamic client preparation process ...")
+        while True:
+            time.sleep(HEALTH_CHECK_INTERVAL)
 
     while True:
         time.sleep(HEALTH_CHECK_INTERVAL)
@@ -90,13 +46,12 @@ def main() -> None:  # pragma: no cover
         try:
             logger.info("preparing dynamic client package ...")
             preparing_env = os.environ.copy()
-            preparing_env[cfg.PREPARING_DOWNLOADED_DYNAMIC_OTA_CLIENT] = "yes"
+            preparing_env["hoge"] = "yes"
             # Execute with the modified environment
             os.execve(
                 path=sys.executable,
-                argv=[sys.executable, __file__],
+                argv=[sys.executable, os.path.abspath(__file__)],
                 env=preparing_env,
             )
         except Exception as e:
             logger.exception(f"Failed during dynamic client preparation: {e}")
-            return _on_shutdown(sys_exit=True)
