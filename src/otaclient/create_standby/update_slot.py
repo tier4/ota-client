ad12ea36e350df60ccbc607636db224229182ec1
bodong.yang <bodong.yang@tier4.jp>
2025-06-03 17:07:55 +0000

    update_slot: dramatically simplify

--- src/otaclient/create_standby/update_slot.py
+++ src/otaclient/create_standby/update_slot.py
@@ -17,8 +17,6 @@ from __future__ import annotations
 
 import logging
 import shutil
-import threading
-import time
 from pathlib import Path
 from queue import Queue
 
@@ -29,14 +27,11 @@ from ota_metadata.file_table.utils import (
     prepare_regular,
 )
 from ota_metadata.legacy2.metadata import OTAMetadata
-from otaclient._status_monitor import StatusReport, UpdateProgressReport
+from otaclient._status_monitor import StatusReport
 from otaclient.configs.cfg import cfg
 from otaclient.create_standby.delta_gen import (
-    PROCESS_FILES_REPORT_BATCH,
-    PROCESS_FILES_REPORT_INTERVAL,
     UpdateStandbySlotFailed,
 )
-from otaclient_common import replace_root
 from otaclient_common._logging import BurstSuppressFilter
 
 logger = logging.getLogger(__name__)
@@ -71,170 +66,66 @@ class UpdateStandbySlot:
         self._standby_slot_mp = Path(standby_slot_mount_point)
         self._resource_dir = Path(resource_dir)
 
-        self._file_process_interrupted = threading.Event()
-        self.file_process_max_failure = file_process_max_failure
-
-    def _process_file_groups_thread_worker(self):
-        """files group process worker."""
-        _failure_count = 0
-
-        _next_commit_before, _batch_cnt = 0, 0
-        _merged_payload = UpdateProgressReport(
-            operation=UpdateProgressReport.Type.APPLY_DELTA
-        )
-
-        _total_cnt = 0
-        while _input := self._que.get():
-            digest, entries = _input
-            # NOTE: the very first entry in the group must be prepared by local copy or
-            #   download from remote, which both cases are recorded previously, so we minus one
-            #   entry when calculating the processed_files_num and processed_files_size.
-            _total_cnt += 1
-            processed_files_num = len(entries) - 1
-            _merged_payload.processed_file_num += processed_files_num
-            _merged_payload.processed_file_size += processed_files_num * (
-                entries[0]["size"] or 0
-            )
-
-            resource_file = self._resource_dir / digest.hex()
-            hardlinked: dict[int, str] = {}
-
-            try:
-                first_entry = entries.pop()
-                prepare_regular(
-                    first_entry,
-                    _rs=resource_file,
-                    target_mnt=self._standby_slot_mp,
-                    prepare_method="hardlink",
-                )
-                if first_entry["links_count"] is not None:
-                    hardlinked[first_entry["inode_id"]] = first_entry["path"]
-
-                for entry in entries:
-                    if entry["links_count"] is not None:
-                        _inode_id = entry["inode_id"]
-                        if _first_hardlinked_canon := hardlinked.get(_inode_id):
-                            prepare_regular(
-                                entry,
-                                _rs=replace_root(
-                                    _first_hardlinked_canon,
-                                    cfg.CANONICAL_ROOT,
-                                    self._standby_slot_mp,
-                                ),
-                                target_mnt=self._standby_slot_mp,
-                                prepare_method="hardlink",
-                            )
-                        else:
-                            prepare_regular(
-                                entry,
-                                resource_file,
-                                target_mnt=self._standby_slot_mp,
-                                prepare_method="copy",
-                            )
-                            hardlinked[_inode_id] = entry["path"]
-                    else:
-                        prepare_regular(
-                            entry,
-                            resource_file,
-                            target_mnt=self._standby_slot_mp,
-                            prepare_method="copy",
-                        )
-
-                _now = int(time.time())
-                if (
-                    _this_batch := _total_cnt // PROCESS_FILES_REPORT_BATCH
-                ) > _batch_cnt or _now > _next_commit_before:
-                    _next_commit_before = _now + PROCESS_FILES_REPORT_INTERVAL
-                    _batch_cnt = _this_batch
-
-                    self._status_report_queue.put_nowait(
-                        StatusReport(
-                            payload=_merged_payload,
-                            session_id=self.session_id,
-                        )
-                    )
-                    _merged_payload = UpdateProgressReport(
-                        operation=UpdateProgressReport.Type.APPLY_DELTA
-                    )
-
-                # reset failure count at success process
-                _failure_count = 0
-            except BaseException as e:  # NOSONAR
-                _failure_count += 1
-                burst_suppressed_logger.exception(f"failed to process {_input}: {e!r}")
-
-                if _failure_count > self.file_process_max_failure:
-                    logger.error(
-                        f"file process worker reaches maximum failure count({self.file_process_max_failure})!"
-                        f"last failure: {e!r}"
-                    )
-                    self._file_process_interrupted.set()
-                    self._que.put_nowait(None)  # stop other workers
-                    return  # and then directly exit
-
-                time.sleep(0.1)  # put the failure item back to que
-                self._que.put_nowait(_input)
-
-        # commit left-over items that cannot fill the batch
-        self._status_report_queue.put_nowait(
-            StatusReport(
-                payload=_merged_payload,
-                session_id=self.session_id,
-            )
-        )
-        # wait up other threads
-        self._que.put_nowait(None)
-
     def _process_regular_file_entries(self) -> None:
         """Yield a group of regular file entries which have the same digest each time.
 
         NOTE: it depends on the regular file table is sorted by digest!
         """
         logger.info("start to process regular file entries ...")
-        _workers: list[threading.Thread] = []
-        for _ in range(cfg.MAX_PROCESS_FILE_THREAD):
-            _t = threading.Thread(target=self._process_file_groups_thread_worker)
-            _t.start()
-            _workers.append(_t)
+        hardlink_group: dict[int, Path] = {}
+        cur_digest: bytes = b""
+        cur_resource: Path = Path()
 
         try:
-            cur_digest_group: list[RegularFileTypedDict] = []
-            cur_digest: bytes = b""
             for _entry in self._ota_metadata.iter_regular_entries():
-                if self._file_process_interrupted.is_set():
-                    return
-
                 _this_digest = _entry["digest"]
-                if not cur_digest:
+                _inode_id = _entry["inode_id"]
+                _is_hardlinked = _entry["links_count"] is not None
+
+                # we always prepare the first copy by moving
+                if _this_digest != cur_digest:
                     cur_digest = _this_digest
-                    cur_digest_group.append(_entry)
+                    hardlink_group.clear()
+                    cur_resource = prepare_regular(
+                        _entry,
+                        _rs=self._resource_dir / cur_digest.hex(),
+                        target_mnt=self._standby_slot_mp,
+                        prepare_method="move",
+                    )
+
+                    if _is_hardlinked:
+                        hardlink_group[_inode_id] = cur_resource
                     continue
 
-                if _this_digest != cur_digest:
-                    self._que.put_nowait((cur_digest, cur_digest_group))
-
-                    cur_digest = _this_digest
-                    cur_digest_group = [_entry]
+                # not the first copy in this digest group
+                if _is_hardlinked:
+                    if _inode_id in hardlink_group:
+                        prepare_regular(
+                            _entry,
+                            _rs=hardlink_group[_inode_id],
+                            target_mnt=self._standby_slot_mp,
+                            prepare_method="hardlink",
+                        )
+                    else:
+                        hardlink_group[_inode_id] = prepare_regular(
+                            _entry,
+                            _rs=cur_resource,
+                            target_mnt=self._standby_slot_mp,
+                            prepare_method="copy",
+                        )
                 else:
-                    cur_digest_group.append(_entry)
-            # NOTE: remember to yield the last group
-            self._que.put_nowait((cur_digest, cur_digest_group))
-        except Exception as e:
-            logger.exception(f"itering file table database failed: {e!r}")
-            raise UpdateStandbySlotFailed(
-                f"itering file table database failed: {e!r}"
-            ) from e
-        finally:
-            self._que.put_nowait(None)
-            for _t in _workers:
-                _t.join()
+                    prepare_regular(
+                        _entry,
+                        cur_resource,
+                        target_mnt=self._standby_slot_mp,
+                        prepare_method="copy",
+                    )
 
-            # finally, check if any workers exit
-            if self._file_process_interrupted.is_set():
-                logger.error("not all workers finish work successfully")
-                raise UpdateStandbySlotFailed(
-                    "not all workers finish work successfully"
-                )
+        except Exception as e:
+            logger.exception(f"failed to update standby slot: {e!r}")
+            raise UpdateStandbySlotFailed(
+                f"failed to update standby slot: {e!r}"
+            ) from e
 
     def _process_dir_entries(self) -> None:
         logger.info("start to process directory entries ...")
