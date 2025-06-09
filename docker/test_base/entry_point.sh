73341562bd0f145f1bb3b63e3bfe4c192f8b0995
bodong.yang <bodong.yang@tier4.jp>
2025-06-09 01:25:04 +0000

    minor update to entry_point.sh

--- docker/test_base/entry_point.sh
+++ docker/test_base/entry_point.sh
@@ -8,11 +8,11 @@ OUTPUT_DIR="${OUTPUT_DIR:-/test_result}"
 mkdir -p ${TEST_ROOT}
 # source code needed to be rw, so copy it to the test_root
 cp -R ${OTACLIENT_SRC}/src ${TEST_ROOT}
+cp ${OTACLIENT_SRC}/uv.lock ${TEST_ROOT}
 # symlink all the other needed folders/files into test root
 ln -s ${OTACLIENT_SRC}/tests ${TEST_ROOT}
 ln -s ${OTACLIENT_SRC}/.git ${TEST_ROOT}
 ln -s ${OTACLIENT_SRC}/pyproject.toml ${TEST_ROOT}
-ln -s ${OTACLIENT_SRC}/uv.lock ${TEST_ROOT}
 ln -s ${OTACLIENT_SRC}/README.md ${TEST_ROOT}
 ln -s ${OTACLIENT_SRC}/LICENSE.md ${TEST_ROOT}
 
