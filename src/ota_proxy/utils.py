b9b11befd8ea8a57ac347a5f2b41f9acab98a55d
bodong.yang <bodong.yang@tier4.jp>
2025-06-14 16:05:40 +0000

    ota_proxy.utils: read_file: avoid caching with best efforts

--- src/ota_proxy/utils.py
+++ src/ota_proxy/utils.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import os
 from hashlib import sha256
 from os import PathLike
 from typing import AsyncIterator
@@ -11,9 +12,13 @@ from .config import config as cfg
 
 async def read_file(fpath: PathLike) -> AsyncIterator[bytes]:
     """Open and read a file asynchronously."""
-    async with await open_file(fpath, "rb") as f:
+    async with await open_file(fpath, "rb", buffering=0) as f:
+        fd = f.wrapped.fileno()
+        os.posix_fadvise(fd, 0, 0, os.POSIX_FADV_NOREUSE)
+        os.posix_fadvise(fd, 0, 0, os.POSIX_FADV_SEQUENTIAL)
         while data := await f.read(cfg.CHUNK_SIZE):
             yield data
+        os.posix_fadvise(fd, 0, 0, os.POSIX_FADV_DONTNEED)
 
 
 def url_based_hash(raw_url: str) -> str:
