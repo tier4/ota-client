c9aa4a6608d3b847f36ec444ad15f8ce01e29b6f
Keisuke Nakata <keisuke.nakata@tier4.jp>
2025-06-16 19:50:51 +0900

    fix: fix mount option in rw

--- src/otaclient_common/cmdhelper.py
+++ src/otaclient_common/cmdhelper.py
@@ -378,7 +378,7 @@ def bind_mount_rw(
     """Bind mount the <target> to <mount_point> read-write.
 
     This is implemented by calling:
-        mount -o bind,rw --make-private --make-unbindable <target> <mount_point>
+        mount -o bind,rw --make-private <target> <mount_point>
 
     Args:
         target (StrOrPath): target to be mounted.
@@ -390,7 +390,7 @@ def bind_mount_rw(
     cmd = [
         "mount",
         "-o", "bind,rw",
-        "--make-private", "--make-unbindable",
+        "--make-private",
         str(target),
         str(mount_point)
     ]
