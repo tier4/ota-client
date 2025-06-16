4f9509f166fd2306d8c1cfa935f30b5ffbbf7252
Keisuke Nakata <keisuke.nakata@tier4.jp>
2025-06-16 14:55:53 +0900

    fix: erase and prepare standby before choort

--- src/otaclient/boot_control/_jetson_uefi.py
+++ src/otaclient/boot_control/_jetson_uefi.py
@@ -976,14 +976,19 @@ class JetsonUEFIBootControl(BootControllerProtocol):
     def get_standby_slot_path(self) -> Path:  # pragma: no cover
         return self._mp_control.standby_slot_mount_point
 
+    def prepare_active_and_standby_slots(
+        self, *, base_mount_point: Path = None, erase_standby=False
+    ):
+        self._mp_control.prepare_standby_dev(erase_standby=erase_standby)
+        self._mp_control.mount_standby(base_mount_point=base_mount_point)
+        self._mp_control.mount_active(base_mount_point=base_mount_point)
+
     def pre_update(self, version: str, *, standby_as_ref: bool, erase_standby: bool):
         try:
             logger.info("jetson-uefi: pre-update ...")
             self._ota_status_control.pre_update_current()
 
-            self._mp_control.prepare_standby_dev(erase_standby=erase_standby)
-            self._mp_control.mount_standby()
-            self._mp_control.mount_active()
+            self.prepare_active_and_standby_slots(erase_standby=erase_standby)
 
             self._ota_status_control.pre_update_standby(version=version)
         except Exception as e:
