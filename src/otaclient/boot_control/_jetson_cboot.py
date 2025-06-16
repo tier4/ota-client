afadcf975fd39dd9113d78b7849da09d12c3519c
Keisuke Nakata <keisuke.nakata@tier4.jp>
2025-06-16 17:50:27 +0900

    fix: erase and mount both active/standby slots before chroot

--- src/otaclient/boot_control/_jetson_cboot.py
+++ src/otaclient/boot_control/_jetson_cboot.py
@@ -611,6 +611,13 @@ class JetsonCBootControl(BootControllerProtocol):
     def get_standby_slot_path(self) -> Path:  # pragma: no cover
         return self._mp_control.standby_slot_mount_point
 
+    def prepare_active_and_standby_slots(
+        self, *, base_mount_point: Optional[Path] = None, erase_standby=False
+    ):
+        self._mp_control.prepare_standby_dev(erase_standby=erase_standby)
+        self._mp_control.mount_standby(base_mount_point=base_mount_point)
+        self._mp_control.mount_active(base_mount_point=base_mount_point)
+
     def pre_update(self, version: str, *, standby_as_ref: bool, erase_standby: bool):
         try:
             logger.info("jetson-cboot: pre-update ...")
@@ -626,10 +633,7 @@ class JetsonCBootControl(BootControllerProtocol):
                 self._cboot_control.set_standby_rootfs_unbootable()
 
             # prepare standby slot dev
-            self._mp_control.prepare_standby_dev(erase_standby=erase_standby)
-            # mount slots
-            self._mp_control.mount_standby()
-            self._mp_control.mount_active()
+            self.prepare_active_and_standby_slots(erase_standby=erase_standby)
 
             # update standby slot's ota_status files
             self._ota_status_control.pre_update_standby(version=version)
