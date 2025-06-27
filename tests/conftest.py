d522d6844eada33103a8994faa2493b393e17570
bodong.yang <bodong.yang@tier4.jp>
2025-06-27 07:54:02 +0000

    cleanup unused

--- tests/conftest.py
+++ tests/conftest.py
@@ -147,41 +147,6 @@ def run_http_server_subprocess():
         _server_p.kill()
 
 
-# @pytest.fixture(autouse=True, scope="session")
-# def ota_image_ft_db() -> Path:
-#     ota_image_dir = Path(OTA_IMAGE_DIR)
-#     _ft_fpath = ota_image_dir / OTAMetadata.FSTABLE_DB
-#     _rs_fpath = ota_image_dir / OTAMetadata.RSTABLE_DB
-
-#     with closing(sqlite3.connect(_ft_fpath)) as fst_conn, closing(
-#         sqlite3.connect(_rs_fpath)
-#     ) as rst_conn:
-#         ft_regular_orm = FileTableRegularORM(fst_conn)
-#         ft_regular_orm.orm_bootstrap_db()
-#         ft_dir_orm = FileTableDirORM(fst_conn)
-#         ft_dir_orm.orm_bootstrap_db()
-#         ft_non_regular_orm = FileTableNonRegularORM(fst_conn)
-#         ft_non_regular_orm.orm_bootstrap_db()
-#         ft_resource_orm = FileTableResourceORM(fst_conn)
-#         ft_resource_orm.orm_bootstrap_db()
-
-#         rs_orm = ResourceTableORM(rst_conn)
-#         rs_orm.orm_bootstrap_db()
-
-#         csv_parser.parse_regulars_from_csv_file(
-#             _fpath=ota_image_dir / "regulars.txt",
-#             _orm=ft_regular_orm,
-#             _orm_ft_resource=ft_resource_orm,
-#             _orm_rs=rs_orm,
-#         )
-#         csv_parser.parse_dirs_from_csv_file(ota_image_dir / "dirs.txt", ft_dir_orm)
-#         csv_parser.parse_symlinks_from_csv_file(
-#             ota_image_dir / "symlinks.txt", ft_non_regular_orm
-#         )
-
-#     return _ft_fpath
-
-
 @pytest.fixture
 def ab_slots(tmp_path_factory: pytest.TempPathFactory) -> SlotMeta:
     """Prepare AB slots for the whole test session.
