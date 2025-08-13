e51c80a2dd7e5a2c7b98841fa63158cd4747c668
bodong.yang <bodong.yang@tier4.jp>
2025-08-13 07:45:58 +0000

    rollback_impl

---
+++ src/otaclient/ota_core2/_rollbacker.py
@@ -0,0 +1,37 @@
+# Copyright 2022 TIER IV, INC. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+import logging
+
+from otaclient import errors as ota_errors
+from otaclient.boot_control import BootControllerProtocol
+
+logger = logging.getLogger(__name__)
+
+
+class OTARollbackImpl:
+    def __init__(self, boot_controller: BootControllerProtocol) -> None:
+        self._boot_controller = boot_controller
+
+    def execute(self):
+        try:
+            self._boot_controller.pre_rollback()
+            self._boot_controller.post_rollback()
+            self._boot_controller.finalizing_rollback()
+        except ota_errors.OTAError as e:
+            logger.error(f"rollback failed: {e!r}")
+            self._boot_controller.on_operation_failure()
+            raise
