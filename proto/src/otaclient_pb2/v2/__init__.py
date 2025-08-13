e2cbf932a41f4c6a456b70f9825053b1169b997f
Timothy Lin <timothy.lin@tier4.jp>
2025-08-13 14:25:55 +0900

    fix version tuple

--- proto/src/otaclient_pb2/v2/__init__.py
+++ proto/src/otaclient_pb2/v2/__init__.py
@@ -13,6 +13,6 @@
 # limitations under the License.
 
 __version__ = version = "2.3.0"
-__version_tuple__ = version_tuple = (2, 2, 0)
+__version_tuple__ = version_tuple = (2, 3, 0)
 
 __all__ = ["version", "__version__", "version_tuple", "__version_tuple__"]
