ebcc951819b832b713da5f29538d8ad406f2d29c
Timothy Lin <timothy.lin@tier4.jp>
2025-08-20 18:47:11 +0900

    revert proto version changes

--- proto/src/otaclient_pb2/v2/__init__.py
+++ proto/src/otaclient_pb2/v2/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = version = "2.3.0"
-__version_tuple__ = version_tuple = (2, 3, 0)
+__version__ = version = "2.2.0"
+__version_tuple__ = version_tuple = (2, 2, 0)
 
 __all__ = ["version", "__version__", "version_tuple", "__version_tuple__"]
