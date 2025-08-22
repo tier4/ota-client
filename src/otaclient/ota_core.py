269a0590024f30a76e93ffbd41d6977831960f57
Timothy Lin <timothy.lin@tier4.jp>
2025-08-22 15:07:06 +0900

    remove critical zone flag updates

--- src/otaclient/ota_core.py
+++ src/otaclient/ota_core.py
@@ -861,37 +861,36 @@ class _OTAUpdater(_OTAUpdateOperator):
         """Post-update phase."""
         logger.info("enter post update phase...")
 
-        with self.critical_zone_flags:
-            _current_time = int(time.time())
-            self._status_report_queue.put_nowait(
-                StatusReport(
-                    payload=OTAUpdatePhaseChangeReport(
-                        new_update_phase=UpdatePhase.PROCESSING_POSTUPDATE,
-                        trigger_timestamp=_current_time,
-                    ),
-                    session_id=self.session_id,
-                )
+        _current_time = int(time.time())
+        self._status_report_queue.put_nowait(
+            StatusReport(
+                payload=OTAUpdatePhaseChangeReport(
+                    new_update_phase=UpdatePhase.PROCESSING_POSTUPDATE,
+                    trigger_timestamp=_current_time,
+                ),
+                session_id=self.session_id,
             )
-            self._metrics.post_update_start_timestamp = _current_time
+        )
+        self._metrics.post_update_start_timestamp = _current_time
 
-            # NOTE(20240219): move persist file handling here
-            self._process_persistents(self._ota_metadata)
+        # NOTE(20240219): move persist file handling here
+        self._process_persistents(self._ota_metadata)
 
-            # save the OTA metadata to the actual location after
-            #   standby slot rootfs updated
-            ota_metadata_save_dst = Path(
-                replace_root(
-                    cfg.IMAGE_META_DPATH,
-                    cfg.CANONICAL_ROOT,
-                    self._boot_controller.get_standby_slot_path(),
-                )
+        # save the OTA metadata to the actual location after
+        #   standby slot rootfs updated
+        ota_metadata_save_dst = Path(
+            replace_root(
+                cfg.IMAGE_META_DPATH,
+                cfg.CANONICAL_ROOT,
+                self._boot_controller.get_standby_slot_path(),
             )
-            ota_metadata_save_dst.mkdir(exist_ok=True, parents=True)
-            shutil.rmtree(ota_metadata_save_dst, ignore_errors=True)
-            shutil.move(self._ota_tmp_meta_on_standby, ota_metadata_save_dst)
+        )
+        ota_metadata_save_dst.mkdir(exist_ok=True, parents=True)
+        shutil.rmtree(ota_metadata_save_dst, ignore_errors=True)
+        shutil.move(self._ota_tmp_meta_on_standby, ota_metadata_save_dst)
 
-            self._preserve_client_squashfs()
-            self._boot_controller.post_update(self.update_version)
+        self._preserve_client_squashfs()
+        self._boot_controller.post_update(self.update_version)
 
     def _process_persistents(self, ota_metadata: OTAMetadata):
         logger.info("start persist files handling...")
@@ -953,43 +952,42 @@ class _OTAUpdater(_OTAUpdateOperator):
         """Finalize the OTA update."""
         logger.info("local update finished, wait on all subecs...")
 
-        with self.critical_zone_flags:
-            _current_finalizing_time = int(time.time())
-            self._status_report_queue.put_nowait(
-                StatusReport(
-                    payload=OTAUpdatePhaseChangeReport(
-                        new_update_phase=UpdatePhase.FINALIZING_UPDATE,
-                        trigger_timestamp=_current_finalizing_time,
-                    ),
-                    session_id=self.session_id,
-                )
+        _current_finalizing_time = int(time.time())
+        self._status_report_queue.put_nowait(
+            StatusReport(
+                payload=OTAUpdatePhaseChangeReport(
+                    new_update_phase=UpdatePhase.FINALIZING_UPDATE,
+                    trigger_timestamp=_current_finalizing_time,
+                ),
+                session_id=self.session_id,
             )
-            self._metrics.finalizing_update_start_timestamp = _current_finalizing_time
-            if proxy_info.enable_local_ota_proxy:
-                wait_and_log(
-                    check_flag=self.ecu_status_flags.any_child_ecu_in_update.is_set,
-                    check_for=False,
-                    message="permit reboot flag",
-                    log_func=logger.info,
-                )
-
-            _current_reboot_time = int(time.time())
-            self._metrics.reboot_start_timestamp = _current_reboot_time
-
-            # publish the metrics before rebooting
-            try:
-                if self._shm_metrics_reader:
-                    _shm_metrics = self._shm_metrics_reader.sync_msg()
-                    self._metrics.shm_merge(_shm_metrics)
-            except Exception as e:
-                logger.error(f"failed to merge metrics: {e!r}")
-            self._metrics.publish()
-
-            logger.info(f"device will reboot in {WAIT_BEFORE_REBOOT} seconds!")
-            time.sleep(WAIT_BEFORE_REBOOT)
-            self._boot_controller.finalizing_update(
-                chroot=_env.get_dynamic_client_chroot_path()
+        )
+        self._metrics.finalizing_update_start_timestamp = _current_finalizing_time
+        if proxy_info.enable_local_ota_proxy:
+            wait_and_log(
+                check_flag=self.ecu_status_flags.any_child_ecu_in_update.is_set,
+                check_for=False,
+                message="permit reboot flag",
+                log_func=logger.info,
             )
+
+        _current_reboot_time = int(time.time())
+        self._metrics.reboot_start_timestamp = _current_reboot_time
+
+        # publish the metrics before rebooting
+        try:
+            if self._shm_metrics_reader:
+                _shm_metrics = self._shm_metrics_reader.sync_msg()
+                self._metrics.shm_merge(_shm_metrics)
+        except Exception as e:
+            logger.error(f"failed to merge metrics: {e!r}")
+        self._metrics.publish()
+
+        logger.info(f"device will reboot in {WAIT_BEFORE_REBOOT} seconds!")
+        time.sleep(WAIT_BEFORE_REBOOT)
+        self._boot_controller.finalizing_update(
+            chroot=_env.get_dynamic_client_chroot_path()
+        )
 
     # API
 
