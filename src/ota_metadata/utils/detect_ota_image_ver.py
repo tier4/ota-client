679220d99dc510840952e89bb2f3c1fcde79b5ed
bodong.yang <bodong.yang@tier4.jp>
2025-09-12 05:38:53 +0000

    implement detect_ota_image_ver helper

---
+++ src/ota_metadata/utils/detect_ota_image_ver.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from http import HTTPStatus
+from urllib.parse import urlsplit
+
+from otaclient_common.downloader import DownloaderPool
+
+OTA_IMAGE_V1_HINT = "oci-layout"
+DOWNLOAD_TIMEOUT = 2  # hint file only takes 31bytes
+RETRY_TIMES = 3
+
+
+def check_if_ota_image_v1(base_url: str, *, downloader_pool: DownloaderPool) -> bool:
+    """Determine whether an OTA image host is OTA Image v1."""
+    _downloader = downloader_pool.get_instance()
+    try:
+        _hint_file_url = f"{base_url.rstrip('/')}/{OTA_IMAGE_V1_HINT}"
+        if _downloader._force_http:
+            _hint_file_url = urlsplit(_hint_file_url)._replace(scheme="http").geturl()
+
+        for _ in range(RETRY_TIMES):
+            resp = _downloader._session.get(_hint_file_url, timeout=DOWNLOAD_TIMEOUT)
+            if resp.status_code in [HTTPStatus.OK]:
+                return True
+        return False
+    finally:
+        downloader_pool.release_instance()
