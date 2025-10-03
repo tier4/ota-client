cf693d3793bbbc986fd74576a1030781b83725f1
bodong.yang <bodong.yang@tier4.jp>
2025-10-03 06:24:55 +0000

    minor fix up

--- src/otaclient/ota_core/_main.py
+++ src/otaclient/ota_core/_main.py
@@ -72,6 +72,7 @@ OP_CHECK_INTERVAL = 1  # second
 HOLD_REQ_HANDLING_ON_ACK_REQUEST = 16  # seconds
 HOLD_REQ_HANDLING_ON_ACK_CLIENT_UPDATE_REQUEST = 4  # seconds
 WAIT_FOR_OTAPROXY_ONLINE = 3 * 60  # 3mins
+WAIT_BEFORE_DYNAMIC_CLIENT_EXIT = 6  # seconds
 
 
 class OTAClient:
@@ -231,6 +232,7 @@ class OTAClient:
             # dynamic client is not running, no need to exit
             return
 
+        time.sleep(WAIT_BEFORE_DYNAMIC_CLIENT_EXIT)
         logger.info("exit from dynamic client...")
         self._client_update_control_flags.request_shutdown_event.set()
 
@@ -315,7 +317,6 @@ class OTAClient:
                 failure_reason=e.get_failure_reason(),
                 failure_type=e.failure_type,
             )
-            self._exit_from_dynamic_client()
         finally:
             shutil.rmtree(session_wd, ignore_errors=True)
             try:
@@ -325,6 +326,8 @@ class OTAClient:
             except Exception as e:
                 logger.error(f"failed to merge metrics: {e!r}")
             self._metrics.publish()
+
+            self._exit_from_dynamic_client()
 
     def client_update(self, request: ClientUpdateRequestV2) -> None:
         """
