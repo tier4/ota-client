b4f664461c31e421f5295c6b7d6800639f24c710
bodong.yang <bodong.yang@tier4.jp>
2025-10-07 02:47:02 +0000

    add e2e test for ota image v1 metadata

---
+++ tests/test_ota_metadata/test_ota_image_v1/test_e2e.py
@@ -0,0 +1,72 @@
+# Copyright 2022 TIER IV, INC. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+import logging
+import os
+from hashlib import sha256
+from pathlib import Path
+from queue import Queue
+from typing import Any, Iterable
+
+from pytest_mock import MockerFixture
+
+from otaclient._types import MultipleECUStatusFlags
+from otaclient._utils import SharedOTAClientMetricsReader
+from otaclient.metrics import OTAMetricsData
+from otaclient.ota_core._updater_base import OTAImageV1SupportMixin
+from otaclient_common.downloader import DownloaderPool
+from tests.conftest import cfg
+
+logger = logging.getLogger(__name__)
+
+
+def _iter_helper(_iter: Iterable[Any]):
+    _any = False
+    for _ in _iter:
+        if not _any:
+            _any = True
+    return _any
+
+
+def test_download_and_parse_metadata(tmp_path: Path, mocker: MockerFixture):
+    # ------ execution ------ #
+    # NOTE: directly bootstrap the mixin as we only check metadata downloading and parsing
+    ota_image_v1 = OTAImageV1SupportMixin(
+        version="dummy_version",
+        raw_url_base=cfg.OTA_IMAGE_V1_URL,
+        session_wd=tmp_path,
+        downloader_pool=DownloaderPool(instance_num=3, hash_func=sha256),
+        session_id=f"session_id_{os.urandom(2).hex()}",
+        ecu_status_flags=mocker.MagicMock(spec=MultipleECUStatusFlags),
+        status_report_queue=mocker.MagicMock(spec=Queue),
+        metrics=mocker.MagicMock(spec=OTAMetricsData),
+        shm_metrics_reader=mocker.MagicMock(spec=SharedOTAClientMetricsReader),
+    )  # type: ignore
+    ota_image_v1._process_metadata()
+
+    # ------ check result ------ #
+    ota_image_helper = ota_image_v1._ota_image_helper
+    assert (_image_index := ota_image_helper.image_index)
+    assert (_image_manifest := ota_image_helper.image_manifest)
+    assert (_image_config := ota_image_helper.image_config)
+    logger.info(str(ota_image_helper.image_index))
+    logger.info(str(ota_image_helper.image_manifest))
+    logger.info(str(ota_image_helper.image_config))
+
+    fst_helper = ota_image_helper.file_table_helper
+    _iter_helper(fst_helper.iter_dir_entries())
+    _iter_helper(fst_helper.iter_non_regular_entries())
+    _iter_helper(fst_helper.iter_regular_entries())
