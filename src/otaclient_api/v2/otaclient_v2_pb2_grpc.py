10f7ef39c0238d64820576b67be581419efe62c9
Timothy Lin <timothy.lin@tier4.jp>
2026-01-21 12:32:02 +0900

    fix imports

--- src/otaclient_api/v2/otaclient_v2_pb2_grpc.py
+++ src/otaclient_api/v2/otaclient_v2_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import otaclient_v2_pb2 as otaclient__v2__pb2
+from otaclient_api.v2 import otaclient_v2_pb2 as otaclient__v2__pb2
 
 GRPC_GENERATED_VERSION = '1.70.0'
 GRPC_VERSION = grpc.__version__
