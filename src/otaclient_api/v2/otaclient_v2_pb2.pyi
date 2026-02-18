803dbe58a9d1e6d4ebffa57f61be51c1ce0fb3c1
Timothy Lin <timothy.lin@tier4.jp>
2026-02-18 15:22:27 +0900

    fix protobuf

--- src/otaclient_api/v2/otaclient_v2_pb2.pyi
+++ src/otaclient_api/v2/otaclient_v2_pb2.pyi
@@ -1,25 +1,28 @@
+import datetime
+
 from google.protobuf import duration_pb2 as _duration_pb2
 from google.protobuf.internal import containers as _containers
 from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
+from collections.abc import Iterable as _Iterable, Mapping as _Mapping
+from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class FailureType(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
-    __slots__ = []
+    __slots__ = ()
     NO_FAILURE: _ClassVar[FailureType]
     RECOVERABLE: _ClassVar[FailureType]
     UNRECOVERABLE: _ClassVar[FailureType]
 
 class AbortFailureType(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
-    __slots__ = []
+    __slots__ = ()
     ABORT_NO_FAILURE: _ClassVar[AbortFailureType]
     ABORT_FAILURE: _ClassVar[AbortFailureType]
 
 class StatusOta(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
-    __slots__ = []
+    __slots__ = ()
     INITIALIZED: _ClassVar[StatusOta]
     SUCCESS: _ClassVar[StatusOta]
     FAILURE: _ClassVar[StatusOta]
@@ -31,7 +34,7 @@ class StatusOta(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
     ABORTED: _ClassVar[StatusOta]
 
 class StatusProgressPhase(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
-    __slots__ = []
+    __slots__ = ()
     INITIAL: _ClassVar[StatusProgressPhase]
     METADATA: _ClassVar[StatusProgressPhase]
     DIRECTORY: _ClassVar[StatusProgressPhase]
@@ -41,7 +44,7 @@ class StatusProgressPhase(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
     POST_PROCESSING: _ClassVar[StatusProgressPhase]
 
 class UpdatePhase(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
-    __slots__ = []
+    __slots__ = ()
     INITIALIZING: _ClassVar[UpdatePhase]
     PROCESSING_METADATA: _ClassVar[UpdatePhase]
     CALCULATING_DELTA: _ClassVar[UpdatePhase]
@@ -81,7 +84,7 @@ FINALIZING_UPDATE: UpdatePhase
 DOWNLOADING_OTA_CLIENT: UpdatePhase
 
 class UpdateRequestEcu(_message.Message):
-    __slots__ = ["ecu_id", "version", "url", "cookies"]
+    __slots__ = ("ecu_id", "version", "url", "cookies")
     ECU_ID_FIELD_NUMBER: _ClassVar[int]
     VERSION_FIELD_NUMBER: _ClassVar[int]
     URL_FIELD_NUMBER: _ClassVar[int]
@@ -93,7 +96,7 @@ class UpdateRequestEcu(_message.Message):
     def __init__(self, ecu_id: _Optional[str] = ..., version: _Optional[str] = ..., url: _Optional[str] = ..., cookies: _Optional[str] = ...) -> None: ...
 
 class UpdateRequest(_message.Message):
-    __slots__ = ["ecu", "request_id"]
+    __slots__ = ("ecu", "request_id")
     ECU_FIELD_NUMBER: _ClassVar[int]
     REQUEST_ID_FIELD_NUMBER: _ClassVar[int]
     ecu: _containers.RepeatedCompositeFieldContainer[UpdateRequestEcu]
@@ -101,7 +104,7 @@ class UpdateRequest(_message.Message):
     def __init__(self, ecu: _Optional[_Iterable[_Union[UpdateRequestEcu, _Mapping]]] = ..., request_id: _Optional[str] = ...) -> None: ...
 
 class UpdateResponseEcu(_message.Message):
-    __slots__ = ["ecu_id", "result", "message"]
+    __slots__ = ("ecu_id", "result", "message")
     ECU_ID_FIELD_NUMBER: _ClassVar[int]
     RESULT_FIELD_NUMBER: _ClassVar[int]
     MESSAGE_FIELD_NUMBER: _ClassVar[int]
@@ -111,19 +114,19 @@ class UpdateResponseEcu(_message.Message):
     def __init__(self, ecu_id: _Optional[str] = ..., result: _Optional[_Union[FailureType, str]] = ..., message: _Optional[str] = ...) -> None: ...
 
 class UpdateResponse(_message.Message):
-    __slots__ = ["ecu"]
+    __slots__ = ("ecu",)
     ECU_FIELD_NUMBER: _ClassVar[int]
     ecu: _containers.RepeatedCompositeFieldContainer[UpdateResponseEcu]
     def __init__(self, ecu: _Optional[_Iterable[_Union[UpdateResponseEcu, _Mapping]]] = ...) -> None: ...
 
 class AbortRequestEcu(_message.Message):
-    __slots__ = ["ecu_id"]
+    __slots__ = ("ecu_id",)
     ECU_ID_FIELD_NUMBER: _ClassVar[int]
     ecu_id: str
     def __init__(self, ecu_id: _Optional[str] = ...) -> None: ...
 
 class AbortRequest(_message.Message):
-    __slots__ = ["ecu", "request_id"]
+    __slots__ = ("ecu", "request_id")
     ECU_FIELD_NUMBER: _ClassVar[int]
     REQUEST_ID_FIELD_NUMBER: _ClassVar[int]
     ecu: _containers.RepeatedCompositeFieldContainer[AbortRequestEcu]
@@ -131,7 +134,7 @@ class AbortRequest(_message.Message):
     def __init__(self, ecu: _Optional[_Iterable[_Union[AbortRequestEcu, _Mapping]]] = ..., request_id: _Optional[str] = ...) -> None: ...
 
 class AbortResponseEcu(_message.Message):
-    __slots__ = ["ecu_id", "result", "message"]
+    __slots__ = ("ecu_id", "result", "message")
     ECU_ID_FIELD_NUMBER: _ClassVar[int]
     RESULT_FIELD_NUMBER: _ClassVar[int]
     MESSAGE_FIELD_NUMBER: _ClassVar[int]
@@ -141,19 +144,19 @@ class AbortResponseEcu(_message.Message):
     def __init__(self, ecu_id: _Optional[str] = ..., result: _Optional[_Union[AbortFailureType, str]] = ..., message: _Optional[str] = ...) -> None: ...
 
 class AbortResponse(_message.Message):
-    __slots__ = ["ecu"]
+    __slots__ = ("ecu",)
     ECU_FIELD_NUMBER: _ClassVar[int]
     ecu: _containers.RepeatedCompositeFieldContainer[AbortResponseEcu]
     def __init__(self, ecu: _Optional[_Iterable[_Union[AbortResponseEcu, _Mapping]]] = ...) -> None: ...
 
 class RollbackRequestEcu(_message.Message):
-    __slots__ = ["ecu_id"]
+    __slots__ = ("ecu_id",)
     ECU_ID_FIELD_NUMBER: _ClassVar[int]
     ecu_id: str
     def __init__(self, ecu_id: _Optional[str] = ...) -> None: ...
 
 class RollbackRequest(_message.Message):
-    __slots__ = ["ecu", "request_id"]
+    __slots__ = ("ecu", "request_id")
     ECU_FIELD_NUMBER: _ClassVar[int]
     REQUEST_ID_FIELD_NUMBER: _ClassVar[int]
     ecu: _containers.RepeatedCompositeFieldContainer[RollbackRequestEcu]
@@ -161,7 +164,7 @@ class RollbackRequest(_message.Message):
     def __init__(self, ecu: _Optional[_Iterable[_Union[RollbackRequestEcu, _Mapping]]] = ..., request_id: _Optional[str] = ...) -> None: ...
 
 class RollbackResponseEcu(_message.Message):
-    __slots__ = ["ecu_id", "result", "message"]
+    __slots__ = ("ecu_id", "result", "message")
     ECU_ID_FIELD_NUMBER: _ClassVar[int]
     RESULT_FIELD_NUMBER: _ClassVar[int]
     MESSAGE_FIELD_NUMBER: _ClassVar[int]
@@ -171,17 +174,17 @@ class RollbackResponseEcu(_message.Message):
     def __init__(self, ecu_id: _Optional[str] = ..., result: _Optional[_Union[FailureType, str]] = ..., message: _Optional[str] = ...) -> None: ...
 
 class RollbackResponse(_message.Message):
-    __slots__ = ["ecu"]
+    __slots__ = ("ecu",)
     ECU_FIELD_NUMBER: _ClassVar[int]
     ecu: _containers.RepeatedCompositeFieldContainer[RollbackResponseEcu]
     def __init__(self, ecu: _Optional[_Iterable[_Union[RollbackResponseEcu, _Mapping]]] = ...) -> None: ...
 
 class StatusRequest(_message.Message):
-    __slots__ = []
+    __slots__ = ()
     def __init__(self) -> None: ...
 
 class StatusProgress(_message.Message):
-    __slots__ = ["phase", "total_regular_files", "regular_files_processed", "files_processed_copy", "files_processed_link", "files_processed_download", "file_size_processed_copy", "file_size_processed_link", "file_size_processed_download", "elapsed_time_copy", "elapsed_time_link", "elapsed_time_download", "errors_download", "total_regular_file_size", "total_elapsed_time", "download_bytes"]
+    __slots__ = ("phase", "total_regular_files", "regular_files_processed", "files_processed_copy", "files_processed_link", "files_processed_download", "file_size_processed_copy", "file_size_processed_link", "file_size_processed_download", "elapsed_time_copy", "elapsed_time_link", "elapsed_time_download", "errors_download", "total_regular_file_size", "total_elapsed_time", "download_bytes")
     PHASE_FIELD_NUMBER: _ClassVar[int]
     TOTAL_REGULAR_FILES_FIELD_NUMBER: _ClassVar[int]
     REGULAR_FILES_PROCESSED_FIELD_NUMBER: _ClassVar[int]
@@ -214,10 +217,10 @@ class StatusProgress(_message.Message):
     total_regular_file_size: int
     total_elapsed_time: _duration_pb2.Duration
     download_bytes: int
-    def __init__(self, phase: _Optional[_Union[StatusProgressPhase, str]] = ..., total_regular_files: _Optional[int] = ..., regular_files_processed: _Optional[int] = ..., files_processed_copy: _Optional[int] = ..., files_processed_link: _Optional[int] = ..., files_processed_download: _Optional[int] = ..., file_size_processed_copy: _Optional[int] = ..., file_size_processed_link: _Optional[int] = ..., file_size_processed_download: _Optional[int] = ..., elapsed_time_copy: _Optional[_Union[_duration_pb2.Duration, _Mapping]] = ..., elapsed_time_link: _Optional[_Union[_duration_pb2.Duration, _Mapping]] = ..., elapsed_time_download: _Optional[_Union[_duration_pb2.Duration, _Mapping]] = ..., errors_download: _Optional[int] = ..., total_regular_file_size: _Optional[int] = ..., total_elapsed_time: _Optional[_Union[_duration_pb2.Duration, _Mapping]] = ..., download_bytes: _Optional[int] = ...) -> None: ...
+    def __init__(self, phase: _Optional[_Union[StatusProgressPhase, str]] = ..., total_regular_files: _Optional[int] = ..., regular_files_processed: _Optional[int] = ..., files_processed_copy: _Optional[int] = ..., files_processed_link: _Optional[int] = ..., files_processed_download: _Optional[int] = ..., file_size_processed_copy: _Optional[int] = ..., file_size_processed_link: _Optional[int] = ..., file_size_processed_download: _Optional[int] = ..., elapsed_time_copy: _Optional[_Union[datetime.timedelta, _duration_pb2.Duration, _Mapping]] = ..., elapsed_time_link: _Optional[_Union[datetime.timedelta, _duration_pb2.Duration, _Mapping]] = ..., elapsed_time_download: _Optional[_Union[datetime.timedelta, _duration_pb2.Duration, _Mapping]] = ..., errors_download: _Optional[int] = ..., total_regular_file_size: _Optional[int] = ..., total_elapsed_time: _Optional[_Union[datetime.timedelta, _duration_pb2.Duration, _Mapping]] = ..., download_bytes: _Optional[int] = ...) -> None: ...
 
 class Status(_message.Message):
-    __slots__ = ["status", "failure", "failure_reason", "version", "progress"]
+    __slots__ = ("status", "failure", "failure_reason", "version", "progress")
     STATUS_FIELD_NUMBER: _ClassVar[int]
     FAILURE_FIELD_NUMBER: _ClassVar[int]
     FAILURE_REASON_FIELD_NUMBER: _ClassVar[int]
@@ -231,7 +234,7 @@ class Status(_message.Message):
     def __init__(self, status: _Optional[_Union[StatusOta, str]] = ..., failure: _Optional[_Union[FailureType, str]] = ..., failure_reason: _Optional[str] = ..., version: _Optional[str] = ..., progress: _Optional[_Union[StatusProgress, _Mapping]] = ...) -> None: ...
 
 class StatusResponseEcu(_message.Message):
-    __slots__ = ["ecu_id", "result", "status"]
+    __slots__ = ("ecu_id", "result", "status")
     ECU_ID_FIELD_NUMBER: _ClassVar[int]
     RESULT_FIELD_NUMBER: _ClassVar[int]
     STATUS_FIELD_NUMBER: _ClassVar[int]
@@ -241,7 +244,7 @@ class StatusResponseEcu(_message.Message):
     def __init__(self, ecu_id: _Optional[str] = ..., result: _Optional[_Union[FailureType, str]] = ..., status: _Optional[_Union[Status, _Mapping]] = ...) -> None: ...
 
 class StatusResponse(_message.Message):
-    __slots__ = ["ecu", "available_ecu_ids", "ecu_v2"]
+    __slots__ = ("ecu", "available_ecu_ids", "ecu_v2")
     ECU_FIELD_NUMBER: _ClassVar[int]
     AVAILABLE_ECU_IDS_FIELD_NUMBER: _ClassVar[int]
     ECU_V2_FIELD_NUMBER: _ClassVar[int]
@@ -251,7 +254,7 @@ class StatusResponse(_message.Message):
     def __init__(self, ecu: _Optional[_Iterable[_Union[StatusResponseEcu, _Mapping]]] = ..., available_ecu_ids: _Optional[_Iterable[str]] = ..., ecu_v2: _Optional[_Iterable[_Union[StatusResponseEcuV2, _Mapping]]] = ...) -> None: ...
 
 class StatusResponseEcuV2(_message.Message):
-    __slots__ = ["ecu_id", "firmware_version", "otaclient_version", "ota_status", "failure_type", "failure_reason", "failure_traceback", "update_status"]
+    __slots__ = ("ecu_id", "firmware_version", "otaclient_version", "ota_status", "failure_type", "failure_reason", "failure_traceback", "update_status")
     ECU_ID_FIELD_NUMBER: _ClassVar[int]
     FIRMWARE_VERSION_FIELD_NUMBER: _ClassVar[int]
     OTACLIENT_VERSION_FIELD_NUMBER: _ClassVar[int]
@@ -271,7 +274,7 @@ class StatusResponseEcuV2(_message.Message):
     def __init__(self, ecu_id: _Optional[str] = ..., firmware_version: _Optional[str] = ..., otaclient_version: _Optional[str] = ..., ota_status: _Optional[_Union[StatusOta, str]] = ..., failure_type: _Optional[_Union[FailureType, str]] = ..., failure_reason: _Optional[str] = ..., failure_traceback: _Optional[str] = ..., update_status: _Optional[_Union[UpdateStatus, _Mapping]] = ...) -> None: ...
 
 class UpdateStatus(_message.Message):
-    __slots__ = ["update_firmware_version", "total_files_size_uncompressed", "total_files_num", "update_start_timestamp", "phase", "total_download_files_num", "total_download_files_size", "downloaded_files_num", "downloaded_bytes", "downloaded_files_size", "downloading_errors", "total_remove_files_num", "removed_files_num", "processed_files_num", "processed_files_size", "total_elapsed_time", "delta_generating_elapsed_time", "downloading_elapsed_time", "update_applying_elapsed_time"]
+    __slots__ = ("update_firmware_version", "total_files_size_uncompressed", "total_files_num", "update_start_timestamp", "phase", "total_download_files_num", "total_download_files_size", "downloaded_files_num", "downloaded_bytes", "downloaded_files_size", "downloading_errors", "total_remove_files_num", "removed_files_num", "processed_files_num", "processed_files_size", "total_elapsed_time", "delta_generating_elapsed_time", "downloading_elapsed_time", "update_applying_elapsed_time")
     UPDATE_FIRMWARE_VERSION_FIELD_NUMBER: _ClassVar[int]
     TOTAL_FILES_SIZE_UNCOMPRESSED_FIELD_NUMBER: _ClassVar[int]
     TOTAL_FILES_NUM_FIELD_NUMBER: _ClassVar[int]
@@ -310,4 +313,4 @@ class UpdateStatus(_message.Message):
     delta_generating_elapsed_time: _duration_pb2.Duration
     downloading_elapsed_time: _duration_pb2.Duration
     update_applying_elapsed_time: _duration_pb2.Duration
-    def __init__(self, update_firmware_version: _Optional[str] = ..., total_files_size_uncompressed: _Optional[int] = ..., total_files_num: _Optional[int] = ..., update_start_timestamp: _Optional[int] = ..., phase: _Optional[_Union[UpdatePhase, str]] = ..., total_download_files_num: _Optional[int] = ..., total_download_files_size: _Optional[int] = ..., downloaded_files_num: _Optional[int] = ..., downloaded_bytes: _Optional[int] = ..., downloaded_files_size: _Optional[int] = ..., downloading_errors: _Optional[int] = ..., total_remove_files_num: _Optional[int] = ..., removed_files_num: _Optional[int] = ..., processed_files_num: _Optional[int] = ..., processed_files_size: _Optional[int] = ..., total_elapsed_time: _Optional[_Union[_duration_pb2.Duration, _Mapping]] = ..., delta_generating_elapsed_time: _Optional[_Union[_duration_pb2.Duration, _Mapping]] = ..., downloading_elapsed_time: _Optional[_Union[_duration_pb2.Duration, _Mapping]] = ..., update_applying_elapsed_time: _Optional[_Union[_duration_pb2.Duration, _Mapping]] = ...) -> None: ...
+    def __init__(self, update_firmware_version: _Optional[str] = ..., total_files_size_uncompressed: _Optional[int] = ..., total_files_num: _Optional[int] = ..., update_start_timestamp: _Optional[int] = ..., phase: _Optional[_Union[UpdatePhase, str]] = ..., total_download_files_num: _Optional[int] = ..., total_download_files_size: _Optional[int] = ..., downloaded_files_num: _Optional[int] = ..., downloaded_bytes: _Optional[int] = ..., downloaded_files_size: _Optional[int] = ..., downloading_errors: _Optional[int] = ..., total_remove_files_num: _Optional[int] = ..., removed_files_num: _Optional[int] = ..., processed_files_num: _Optional[int] = ..., processed_files_size: _Optional[int] = ..., total_elapsed_time: _Optional[_Union[datetime.timedelta, _duration_pb2.Duration, _Mapping]] = ..., delta_generating_elapsed_time: _Optional[_Union[datetime.timedelta, _duration_pb2.Duration, _Mapping]] = ..., downloading_elapsed_time: _Optional[_Union[datetime.timedelta, _duration_pb2.Duration, _Mapping]] = ..., update_applying_elapsed_time: _Optional[_Union[datetime.timedelta, _duration_pb2.Duration, _Mapping]] = ...) -> None: ...
