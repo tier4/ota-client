c852ce4521f97200dc07a106f91adb3268551985
Timothy Lin <timothy.lin@tier4.jp>
2026-02-20 19:05:01 +0900

    revert gRPC generated version change

--- src/otaclient_api/v2/otaclient_v2_pb2_grpc.py
+++ src/otaclient_api/v2/otaclient_v2_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 from otaclient_api.v2 import otaclient_v2_pb2 as otaclient__api_dot_v2_dot_otaclient__v2__pb2
 
-GRPC_GENERATED_VERSION = '1.78.1'
+GRPC_GENERATED_VERSION = '1.78.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
