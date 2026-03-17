36230851baf5050a8cdf01c992230d3a977ea299
bodong.yang <bodong.yang@tier4.jp>
2026-03-18 08:48:35 +0900

    utils: add subprocess_call helper

--- tools/offline_ota_image_builder/src/offline_ota_image_builder/utils.py
+++ tools/offline_ota_image_builder/src/offline_ota_image_builder/utils.py
@@ -13,11 +13,12 @@
 # limitations under the License.
 """Shared utils among package."""
 
-
+import os
+import shlex
+import subprocess
+import sys
 from os import PathLike
-from typing import Union
-
-from typing_extensions import TypeAlias
+from typing import Optional, TypeAlias, Union
 
 StrPath: TypeAlias = Union[str, PathLike]
 
@@ -26,3 +27,54 @@ class InputImageProcessError(Exception): ...
 
 
 class ExportError(Exception): ...
+
+
+RUN_AS_PYINSTALLER_BUNDLE = getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS")
+
+
+def pyinstaller_aware_subprocess_run(args, *p_pargs, env=None, **p_kwargs):
+    """
+    See https://pyinstaller.org/en/stable/runtime-information.html#ld-library-path-libpath-considerations
+        for more details.
+    """
+    lp_key = "LD_LIBRARY_PATH"  # for GNU/Linux and *BSD.
+    lp_orig_key = f"{lp_key}_ORIG"
+
+    if env is None:
+        _parsed_env = dict(os.environ)
+    else:
+        _parsed_env = dict(env)
+
+    lp_orig = _parsed_env.pop(lp_orig_key, None)
+    if lp_orig is not None:
+        _parsed_env[lp_key] = lp_orig  # restore the original, unmodified value
+    else:
+        # This happens when LD_LIBRARY_PATH was not set.
+        # Remove the env var as a last resort:
+        _parsed_env.pop(lp_key, None)
+    return subprocess.run(args, *p_pargs, env=_parsed_env, **p_kwargs)
+
+
+def subprocess_run_wrapper(
+    cmd: str | list[str],
+    *,
+    check: bool,
+    check_output: bool,
+    env: Optional[dict[str, str]] = None,
+    timeout: Optional[float] = None,
+) -> subprocess.CompletedProcess[bytes]:
+    if isinstance(cmd, str):
+        cmd = shlex.split(cmd)
+
+    _run_func = subprocess.run
+    if RUN_AS_PYINSTALLER_BUNDLE:
+        _run_func = pyinstaller_aware_subprocess_run
+
+    return _run_func(
+        cmd,
+        check=check,
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE if check_output else None,
+        timeout=timeout,
+        env=env,
+    )  # type: ignore
