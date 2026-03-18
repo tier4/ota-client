859b90e5800761b7a61cfbbd0e01f400895e73fd
bodong.yang <bodong.yang@tier4.jp>
2026-03-18 09:23:20 +0900

    finish up v1 helper

--- tools/offline_ota_image_builder/src/offline_ota_image_builder/ota_metadata/v1.py
+++ tools/offline_ota_image_builder/src/offline_ota_image_builder/ota_metadata/v1.py
@@ -15,6 +15,7 @@
 from pathlib import Path
 
 from ota_image_libs._resource_filter import CompressFilter
+from ota_image_libs.v1.artifact.reader import OTAImageArtifactReader
 from ota_image_libs.v1.resource_table.schema import ResourceTableManifestTypedDict
 from ota_image_libs.v1.resource_table.utils import ResourceTableDBHelper
 
@@ -24,11 +25,25 @@ DB_CONN = 3
 class OTAImageBroken(Exception): ...
 
 
+class OTAImageHelper:
+    def __init__(self, _image_zip: Path) -> None:
+        self._image_helper = OTAImageArtifactReader(_image_zip)
+
+    def save_resource_table(self, _save_dst: Path) -> None:
+        _image_index = self._image_helper.parse_index()
+        self._image_helper.get_resource_table(_image_index, _save_dst)
+
+
 class ResourceTableHelper:
     def __init__(self, _rst_file: Path, *, db_conn: int = DB_CONN) -> None:
         _rst_db_helper = ResourceTableDBHelper(_rst_file)
-
         self._rst_orm_pool = _rst_db_helper.get_orm_pool(db_conn)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc, tb):
+        self._rst_orm_pool.orm_pool_shutdown()
 
     def check_blob_zstd_compressed(self, _digest_hex: str) -> bool:
         """Thread-safe helper to check whether a blob is zstd compressed."""
